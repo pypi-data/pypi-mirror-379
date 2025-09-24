62eeaef7464e2d09fad0cfe6da4920f29bd5c2fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 03:15:02 +0000

    Add package DesmosKiller/6.5/desmoskiller-6.5.tar.gz

---
+++ packages/DesmosKiller/desmoskiller-6.5.tar.gz/desmoskiller-6.5/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name="DesmosKiller",
+      version="6.5",
+      packages=["DesmosKiller"],
+      author="YusufA442",
+      author_email="yusuf365820@gmail.com",
+      license='Creative Commons Attribution-Noncommercial-Share Alike license',
+      long_description=open('readme.txt').read())
