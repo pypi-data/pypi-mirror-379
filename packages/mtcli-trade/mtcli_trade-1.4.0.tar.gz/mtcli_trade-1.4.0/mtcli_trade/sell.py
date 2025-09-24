f4d696532c19762edf5ac812ac3202e22d0bf362
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 14:04:09 +0000

    Add package mtcli-trade/1.4.0/mtcli_trade-1.4.0.tar.gz

---
+++ packages/mtcli-trade/mtcli_trade-1.4.0.tar.gz/mtcli_trade-1.4.0/mtcli_trade/sell.py
@@ -0,0 +1,62 @@
+"""Comando para executar venda a mercado ou pendente."""
+
+import click
+import MetaTrader5 as mt5
+from mtcli.conecta import conectar, shutdown
+from mtcli.logger import setup_logger
+from .risco import controlar_risco
+from .ordem import inicializar, criar_ordem, enviar_ordem
+from .conf import LOSS_LIMIT, STATUS_FILE
+
+log = setup_logger()
+
+
+@click.command()
+@click.option(
+    "--symbol", "-s", default="WINV25", help="Símbolo do ativo (default WINV25)."
+)
+@click.option(
+    "--lot",
+    type=float,
+    default=1.0,
+    help="Quantidade de contratos (default =1.0)",
+)
+@click.option(
+    "-sl", type=float, default=150, help="Stop loss (em pontos) (default 150)."
+)
+@click.option("-tp", type=float, default=300, help="Take profit (em pontos)")
+@click.option("--limit", "-l", is_flag=True, help="Envia ordem limit (sell limit)")
+@click.option("--preco", "-pr", type=float, default=None, help="Preço da ordem limit")
+def sell(symbol, lot, sl, tp, limit, preco):
+    """Venda a mercado ou pendente com sl e tp."""
+    conectar()
+
+    # ⚠ Verifica risco antes de enviar qualquer ordem
+    if controlar_risco(STATUS_FILE, LOSS_LIMIT):
+        click.echo("Ordem bloqueada: limite de prejuízo diário atingido.")
+        log.info("Envio de ordem bloqueado por risco.")
+        shutdown()
+        return
+
+    tick = inicializar(symbol)
+    if not tick:
+        return
+
+    if limit:
+        if preco is None:
+            click.echo("Para ordens pendentes, defina o --preco")
+            shutdown()
+            return
+        price = preco
+        order_type = mt5.ORDER_TYPE_SELL_LIMIT
+    else:
+        price = tick.bid
+        order_type = mt5.ORDER_TYPE_SELL
+
+    ordem = criar_ordem(symbol, lot, sl, tp, price, order_type, limit)
+    enviar_ordem(ordem, limit)
+    shutdown()
+
+
+if __name__ == "__main__":
+    sell()
