bf71ec9ec55018c2a1d125c54bef8a47639e46e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 14:05:18 +0000

    Add package async-lambda-unstable/0.5.4/async-lambda-unstable-0.5.4.tar.gz

---
+++ packages/async-lambda-unstable/async-lambda-unstable-0.5.4.tar.gz/async-lambda-unstable-0.5.4/async_lambda/config.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+
+class AsyncLambdaConfig:
+    name: str = "async-lambda"
+    runtime: str = "python3.10"
+    s3_payload_retention: Optional[int] = 30
+
+
+config = AsyncLambdaConfig()
+
+
+def config_set_name(name: str):
+    """
+    Sets the name for the project.
+    """
+    config.name = name
+
+
+def config_set_runtime(runtime: str):
+    """
+    Sets the runtime for the project.
+    """
+    config.runtime = runtime
+
+
+def config_set_s3_payload_retention(days: Optional[int]):
+    """
+    Sets the s3_payload_retention policy in days.
+    """
+    config.s3_payload_retention = days
