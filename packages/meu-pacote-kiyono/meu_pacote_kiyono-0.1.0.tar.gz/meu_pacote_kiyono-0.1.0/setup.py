845269b2a0555aff8f38ec8398fcfa0f5de2278a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 18:05:06 +0000

    Add package meu-pacote-kiyono/0.1.0/meu_pacote_kiyono-0.1.0.tar.gz

---
+++ packages/meu-pacote-kiyono/meu_pacote_kiyono-0.1.0.tar.gz/meu_pacote_kiyono-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='meu_pacote_kiyono',
+    version='0.1.0',
+    description='Meu pacote',
+    long_description='Meu pacote',
+    author='Marcos Kiyono',
+    author_email='kiyono.marcos@gmail.com',
+    python_requires='>=3.8,<4.0',
+    install_requires=['httpx'],
+)
