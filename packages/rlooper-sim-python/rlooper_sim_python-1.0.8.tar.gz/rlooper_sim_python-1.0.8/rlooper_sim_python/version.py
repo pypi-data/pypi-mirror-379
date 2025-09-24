5a44e22acd8f4b70f069e1ef0fc42b59c3e084b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 18:05:55 +0000

    Add package rlooper-sim-python/1.0.8/rlooper_sim_python-1.0.8.tar.gz

---
+++ packages/rlooper-sim-python/rlooper_sim_python-1.0.8.tar.gz/rlooper_sim_python-1.0.8/rlooper_sim_python/version.py
@@ -0,0 +1,13 @@
+# Rlooper Simulation Python - Version Information
+
+__version__ = "1.0.8"
+__release_date__ = "2025-09-24"
+__author__ = "srhartono"
+__description__ = "Rlooper with R-loop Peak Simulator (Python Implementation)"
+
+# Version history
+CHANGELOG = """
+## v1.0.5 (2025-09-23)
+- Fixed DAG not being made
+- Fixed summary report generation
+"""
