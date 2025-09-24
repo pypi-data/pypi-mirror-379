88f1207a21c7b0f98382965518f56f1cfc31229a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 17:05:11 +0000

    Add package maleo-infra/0.0.52/maleo_infra-0.0.52-py3-none-any.whl

---
+++ packages/maleo-infra/maleo_infra-0.0.52-py3-none-any.whl/maleo/infra/resource/enums.py
@@ -0,0 +1,14 @@
+from enum import StrEnum
+from maleo.types.string import ListOfStrings
+
+
+class Status(StrEnum):
+    LOW = "low"
+    NORMAL = "normal"
+    HIGH = "high"
+    CRITICAL = "critical"
+    OVERLOAD = "overload"
+
+    @classmethod
+    def choices(cls) -> ListOfStrings:
+        return [e.value for e in cls]
