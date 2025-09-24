71f4f93cf0bb499ab6d9e39b9bccfc2e0d7b9e04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 06:03:46 +0000

    Add package meu-pacote-leticia-matosc/0.1.0/meu_pacote_leticia_matosc-0.1.0.tar.gz

---
+++ packages/meu-pacote-leticia-matosc/meu_pacote_leticia_matosc-0.1.0.tar.gz/meu_pacote_leticia_matosc-0.1.0/README.md
@@ -0,0 +1,6 @@
+# meu_pacote
+
+Pacote simples criado como exemplo para curso de Python.
+
+Autora: Leticia Matos
+"# meu-pacote"  
---
+++ packages/meu-pacote-leticia-matosc/meu_pacote_leticia_matosc-0.1.0.tar.gz/meu_pacote_leticia_matosc-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="meu_pacote-leticia-matosc",
+    version="0.1.0",
+    author="Leticia Matos",
+    author_email="",
+    description="Um pacote simples de exemplo para curso de Python",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.7",
+)
