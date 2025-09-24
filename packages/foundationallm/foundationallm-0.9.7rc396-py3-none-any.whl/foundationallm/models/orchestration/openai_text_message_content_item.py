a6fd7ded619734a7fa64cdbc6c7123a1519da373
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 03:14:59 +0000

    Add package foundationallm/0.9.7rc396/foundationallm-0.9.7rc396-py3-none-any.whl

---
+++ packages/foundationallm/foundationallm-0.9.7rc396-py3-none-any.whl/foundationallm/models/orchestration/openai_text_message_content_item.py
@@ -0,0 +1,12 @@
+from pydantic import Field
+from typing import Optional, List, Literal
+from .message_content_item_base import MessageContentItemBase
+from .openai_file_path_message_content_item import OpenAIFilePathMessageContentItem
+from .message_content_item_types import MessageContentItemTypes
+
+class OpenAITextMessageContentItem(MessageContentItemBase):
+    """An OpenAI text message content item."""
+
+    type: Literal[MessageContentItemTypes.TEXT] = MessageContentItemTypes.TEXT
+    annotations: Optional[List[OpenAIFilePathMessageContentItem]] = Field(default_factory=list, alias="annotations")
+    value: Optional[str] = Field(None, alias="value")
