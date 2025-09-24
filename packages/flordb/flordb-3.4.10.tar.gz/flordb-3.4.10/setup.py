30e4e61d48e150afd1053da44277eedce131dc6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 05:05:03 +0000

    Add package flordb/3.4.10/flordb-3.4.10.tar.gz

---
+++ packages/flordb/flordb-3.4.10.tar.gz/flordb-3.4.10/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+import io
+
+with io.open("README.md", mode="r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="flordb",
+    version="3.4.10",
+    author="Rolando Garcia",
+    author_email="rogarcia@berkeley.edu",
+    description="Fast Low-Overhead Recovery",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ucbrise/flor",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "GitPython",
+        "cloudpickle",
+        "astunparse",
+        "pandas",
+        "bidict==0.21.3",
+        "apted",
+        "matplotlib",
+        "scikit-learn",
+        "numpy",
+        "tqdm",
+        "sh",
+        "ipython",
+        "ipykernel",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+)
