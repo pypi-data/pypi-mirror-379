85ffa8e182680819f79c098856d088554082a30d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 18:06:12 +0000

    Add package itk-numerics/6.0b1/itk_numerics-6.0b1-cp310-cp310-win_amd64.whl

---
+++ packages/itk-numerics/itk_numerics-6.0b1-cp310-cp310-win_amd64.whl/itk/Configuration/ITKImageCompose_snake_case.py
@@ -0,0 +1,1 @@
+snake_case_functions = ('compose_image_filter', 'join_series_image_filter', )
---
+++ packages/itk-numerics/itk_numerics-6.0b1-cp310-cp310-win_amd64.whl/itk/Configuration/ITKPath_snake_case.py
@@ -0,0 +1,1 @@
+snake_case_functions = ('contour_extractor2_d_image_filter', 'image_to_path_filter', 'path_source', 'path_to_image_filter', )
