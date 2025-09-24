298624134eb054f0b584d2be61d4bc41d21bbc8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 18:05:24 +0000

    Add package data-intelligence-mcp-server/0.0.33/data_intelligence_mcp_server-0.0.33-py3-none-any.whl

---
+++ packages/data-intelligence-mcp-server/data_intelligence_mcp_server-0.0.33-py3-none-any.whl/app/services/search_assets/tools/search_asset.py
@@ -0,0 +1,37 @@
+from typing import Any, List
+
+from ..models.search_asset import SearchAssetRequest, SearchAssetResponse
+
+from app.core.auth import get_access_token
+from app.core.registry import service_registry
+from app.core.settings import settings
+from app.services.constants import ASSISTANT_BASE_ENDPOINT
+from app.shared.exceptions.base import ExternalAPIError
+from app.shared.utils.helpers import is_none
+from app.shared.utils.http_client import get_http_client
+from app.shared.logging import LOGGER, auto_context
+
+@service_registry.tool(
+        name="search_assets_search_asset",
+        description="""Understand user's request about searching data assets and return list of retrieved assets.
+                       This function takes a user's search prompt as input and may take container type: project or catalog. Default container type to catalog.
+                       It then returns list of asset that has been found
+
+                       Args:
+                            request: is an object of SearchAssetRequest containing:
+                                search_prompt (str): The search prompt from the user about data assets potentially with additional searching details.
+                                container_type (str): The container type for which to search assets.
+                       Returns:
+                            List[SearchAssetResponse] : A list of SearchAssetResponse objects.
+
+                       """)
+@auto_context
+async def search_asset(search_prompt:str, auth_scope:str, ctx=None) -> List[SearchAssetResponse]:
+    auth_scope = "catalog"
+    return List[SearchAssetResponse]
+
+def _construct_search_asset(asset: Any):
+    search_asset = SearchAssetResponse.model_validate(asset)
+    if "<ui_base_url>" in search_asset.url:
+        search_asset.url = search_asset.url.replace("<ui_base_url>", f"{settings.ui_url}")
+    return search_asset
