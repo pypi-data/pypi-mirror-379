1527ba159ae2615504ad3835527946b03de129c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 18:05:48 +0000

    Add package itk-filtering/6.0b1/itk_filtering-6.0b1-cp310-cp310-macosx_15_0_arm64.whl

---
+++ packages/itk-filtering/itk_filtering-6.0b1-cp310-cp310-macosx_15_0_arm64.whl/itk/Configuration/ITKImageCompare_snake_case.py
@@ -0,0 +1,1 @@
+snake_case_functions = ('absolute_value_difference_image_filter', 'checker_board_image_filter', 'similarity_index_image_filter', 'squared_difference_image_filter', 'staple_image_filter', )
---
+++ packages/itk-filtering/itk_filtering-6.0b1-cp310-cp310-macosx_15_0_arm64.whl/itk/Configuration/ITKImageLabel_snake_case.py
@@ -0,0 +1,1 @@
+snake_case_functions = ('binary_contour_image_filter', 'change_label_image_filter', 'label_contour_image_filter', 'unary_functor_image_filter', )
