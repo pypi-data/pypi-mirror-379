48f10da284249c6c2e26eb055d01a534ec9e5be5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 17:05:11 +0000

    Add package tpu-pod-commander/0.1.7/tpu_pod_commander-0.1.7.tar.gz

---
+++ packages/tpu-pod-commander/tpu_pod_commander-0.1.7.tar.gz/tpu_pod_commander-0.1.7/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tpu_pod_commander',
+    version='0.1.7',
+    license='Apache-2.0',
+    description='TPU pod commander is a command line tool to manage cloud TPU pods.',
+    url='https://github.com/young-geng/tpu_pod_commander',
+    packages=find_packages(include=['tpu_pod_commander']),
+    python_requires=">=3.8",
+    install_requires=[
+        'absl-py',
+        'mlxu>=0.2.0',
+    ],
+    entry_points={
+        'console_scripts': [
+            'tpc = tpu_pod_commander.cli:run_cli',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+)
