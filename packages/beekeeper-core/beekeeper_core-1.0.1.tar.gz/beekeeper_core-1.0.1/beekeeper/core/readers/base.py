136d5fae018784089d7726b27289d6d7ff0b22a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 07:04:11 +0000

    Add package beekeeper-core/1.0.1/beekeeper_core-1.0.1.tar.gz

---
+++ packages/beekeeper-core/beekeeper_core-1.0.1.tar.gz/beekeeper_core-1.0.1/beekeeper/core/readers/base.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+from beekeeper.core.document import Document
+from pydantic.v1 import BaseModel
+
+
+class BaseReader(ABC, BaseModel):
+    """An interface for document reader."""
+
+    @classmethod
+    def class_name(cls) -> str:
+        return "BaseReader"
+
+    @abstractmethod
+    def load_data(self) -> List[Document]:
+        """Loads data."""
+
+    def load(self) -> List[Document]:
+        return self.load_data()
+
+    def lazy_load(self) -> List[Document]:
+        return self.load_data()
