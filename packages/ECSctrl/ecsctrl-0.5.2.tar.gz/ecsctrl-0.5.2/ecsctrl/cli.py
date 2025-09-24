ff4828242c4842e0d464a5ab43ac7890ef1e828a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 03:14:36 +0000

    Add package ECSctrl/0.5.2/ecsctrl-0.5.2.tar.gz

---
+++ packages/ECSctrl/ecsctrl-0.5.2.tar.gz/ecsctrl-0.5.2/ecsctrl/cli.py
@@ -0,0 +1,414 @@
+import os
+import re
+from email.policy import default
+
+import click
+
+from ecsctrl.loader import VarsLoader
+
+from .boto_client import BotoClient
+from .dump import generate_var_lut
+from .dump.secrets import dump_secrets, render_dumped_secrets
+from .service_updater import ServiceUpdater, TaskDefinitionServiceUpdater, WaitForUpdate
+from .yaml_converter import (
+    JOB_DEFINITION,
+    SECRETS,
+    SERVICE,
+    TASK_DEFINITION,
+    yaml_file_to_dict,
+)
+
+
+def check_var(ctx, param, value):
+    for v in value:
+        if not re.match("^[^=]+=.*$", v):
+            raise click.BadParameter(
+                f"'{v}'. Variable has to be in format variable=value"
+            )
+    return value
+
+
+# fmt: off
+@click.group()
+@click.option("--dry-run", is_flag=True, default=False, help="Do not call actual AWS API")
+@click.pass_context
+# fmt: on
+def cli(ctx, dry_run):
+    ctx.ensure_object(dict)
+    ctx.obj["dry_run"] = dry_run
+    ctx.obj["boto_client"] = BotoClient("ecs", dry_run=dry_run)
+
+
+@cli.group(name="task-definition")
+@click.pass_context
+def task_definition(ctx):
+    """Task definition management."""
+
+
+def common_options(fn):
+    # fmt: off
+    fn = click.option("--env-file", "-e", multiple=True, type=str, help="Path to env-style file with variables")(fn)
+    fn = click.option("--json-file", "-j", multiple=True, type=str, help="Path to json file with variable")(fn)
+    fn = click.option("--var", "-v", multiple=True, type=str, callback=check_var, help="Single variable in format name=value")(fn)
+    fn = click.option("--sys-env/--no-sys-env", is_flag=True, default=False, help="Uses system env as a source for template variables")(fn)
+    # fmt: on
+    return fn
+
+
+def wait_options(wait_for, many=False):
+    def wrapper(fn):
+        s = "s" if many else ""
+        # fmt: off
+        fn = click.option("--wait", "-w", is_flag=True, help=f"Waits for service{s} to finish {wait_for}")(fn)
+        fn = click.option("--wait-timeout", default=600, type=int, help=f"Custom timeout in seconds (defaults to 600s)")(fn)
+        # fmt: on
+        return fn
+
+    return wrapper
+
+
+# fmt: off
+@task_definition.command()
+@click.argument("spec-file", type=str)
+@common_options
+@click.option("--update-services-in-cluster", "-c", multiple=True, type=str, help="Updates all services deployed with this task in a particular cluster")
+@wait_options(wait_for="update", many=True)
+@click.pass_context
+# fmt: on
+def register(
+    ctx,
+    spec_file,
+    env_file,
+    json_file,
+    var,
+    sys_env,
+    update_services_in_cluster,
+    wait,
+    wait_timeout,
+):
+    """Register task definition."""
+
+    vars = VarsLoader(env_file, var, json_file, sys_env).load()
+    spec = yaml_file_to_dict(spec_file, vars, TASK_DEFINITION)
+    task_family = spec.get("family", "N/A")
+    click.echo(f"🗂 Registering task definition {task_family}.")
+    response = ctx.obj["boto_client"].call("register_task_definition", **spec)
+    task_definition_arn = response["taskDefinition"]["taskDefinitionArn"]
+    click.echo(f"\t✅ done, task definition arn: {task_definition_arn}.")
+
+    if update_services_in_cluster and not ctx.obj["dry_run"]:
+        updated_services = {}
+
+        for cluster_name in update_services_in_cluster:
+            updater = TaskDefinitionServiceUpdater(
+                ctx.obj["boto_client"], task_definition_arn, cluster_name
+            )
+            updated_services_in_cluster = updater.update()
+            updated_services[cluster_name] = updated_services_in_cluster
+
+        if wait:
+            waiter = WaitForUpdate(ctx.obj["boto_client"], updated_services)
+            waiter.timeout = wait_timeout
+            waiter.wait_for_all()
+
+
+@cli.group(name="batch-job-definition")
+@click.pass_context
+def batch_job_definition(ctx):
+    """Batch job definition management."""
+
+
+# fmt: off
+@batch_job_definition.command()
+@click.argument("spec-file", type=str)
+@common_options
+@click.pass_context
+# fmt: on
+def register(
+    ctx,
+    spec_file,
+    env_file,
+    json_file,
+    var,
+    sys_env,
+):
+    """Register AWS Batch job definition."""
+
+    vars = VarsLoader(env_file, var, json_file, sys_env).load()
+    spec = yaml_file_to_dict(spec_file, vars, JOB_DEFINITION)
+    job_definition_name = spec.get("jobDefinitionName", "N/A")
+    click.echo(f"🗂 Registering batch job definition {job_definition_name}.")
+    client = BotoClient("batch", dry_run=ctx.obj["boto_client"].dry_run)
+    response = client.call("register_job_definition", **spec)
+    job_definition_arn = response["jobDefinitionArn"]
+    click.echo(f"\t✅ done, job definition arn: {job_definition_arn}.")
+
+
+@cli.group(name="service")
+@click.pass_context
+def service(ctx):
+    """Service management."""
+
+
+@service.command()
+@click.argument("spec-file", type=str)
+@common_options
+@wait_options(wait_for="creation")
+@click.pass_context
+def create(
+    ctx,
+    spec_file,
+    env_file,
+    json_file,
+    var,
+    sys_env,
+    wait,
+    wait_timeout,
+):
+    """Create a new service."""
+
+    vars = VarsLoader(env_file, var, json_file, sys_env).load()
+    spec = yaml_file_to_dict(spec_file, vars, SERVICE)
+    service_name = spec.get("serviceName")
+    cluster_name = spec.get("cluster")
+    click.echo(f"🏸 Creating service {service_name}.")
+    response = ctx.obj["boto_client"].call("create_service", **spec)
+    service_arn = response["service"]["serviceArn"]
+    click.echo("\t✅ done.")
+
+    if wait:
+        waiter = WaitForUpdate(
+            ctx.obj["boto_client"],
+            {cluster_name: [(service_arn, service_name)]},
+        )
+        waiter.timeout = wait_timeout
+        waiter.wait_for_all()
+
+
+@service.command()
+@click.argument("spec-file", type=str)
+@common_options
+@wait_options(wait_for="update")
+@click.pass_context
+def update(
+    ctx,
+    spec_file,
+    env_file,
+    json_file,
+    var,
+    sys_env,
+    wait,
+    wait_timeout,
+):
+    """Update an existing service."""
+
+    vars = VarsLoader(env_file, var, json_file, sys_env).load()
+    spec = yaml_file_to_dict(spec_file, vars, SERVICE)
+    service_name = spec.get("serviceName")
+    cluster_name = spec.get("cluster")
+    click.echo(f"🏸 Updating service {service_name}.")
+    updater = ServiceUpdater()
+    spec = updater.make_update_payload(spec)
+    response = ctx.obj["boto_client"].call("update_service", **spec)
+    service_arn = response["service"]["serviceArn"]
+    click.echo("\t✅ done.")
+
+    if wait:
+        waiter = WaitForUpdate(
+            ctx.obj["boto_client"],
+            {cluster_name: [(service_arn, service_name)]},
+        )
+        waiter.timeout = wait_timeout
+        waiter.wait_for_all()
+
+
+@service.command("create-or-update")
+@click.argument("spec-file", type=str)
+@common_options
+@wait_options(wait_for="update")
+@click.pass_context
+def create_or_update(
+    ctx,
+    spec_file,
+    env_file,
+    json_file,
+    var,
+    sys_env,
+    wait,
+    wait_timeout,
+):
+    """Check if service exists and update it or create a new one."""
+
+    vars = VarsLoader(env_file, var, json_file, sys_env).load()
+    spec = yaml_file_to_dict(spec_file, vars, SERVICE)
+    service_name = spec.get("serviceName")
+    cluster_name = spec.get("cluster")
+
+    response = ctx.obj["boto_client"].call(
+        "describe_services",
+        cluster=spec["cluster"],
+        services=[service_name],
+    )
+    service_exists = len(response["services"]) > 0
+
+    if service_exists:
+        click.echo(f"🏸 Updating service {service_name}.")
+        updater = ServiceUpdater()
+        spec = updater.make_update_payload(spec)
+        response = ctx.obj["boto_client"].call("update_service", **spec)
+        click.echo("\t✅ done.")
+    else:
+        click.echo(f"🏸 Creating service {service_name}.")
+        response = ctx.obj["boto_client"].call("create_service", **spec)
+        click.echo("\t✅ done.")
+    service_arn = response["service"]["serviceArn"]
+
+    if wait:
+        waiter = WaitForUpdate(
+            ctx.obj["boto_client"],
+            {cluster_name: [(service_arn, service_name)]},
+        )
+        waiter.timeout = wait_timeout
+        waiter.wait_for_all()
+
+
+@cli.group(name="secrets")
+@click.pass_context
+def secrets(ctx):
+    """Secrets management."""
+
+
+@secrets.command()
+@click.argument("spec-file", type=str)
+@common_options
+@click.pass_context
+def store(
+    ctx,
+    spec_file,
+    env_file,
+    json_file,
+    var,
+    sys_env,
+):
+    """Store secrets is Parameter Store."""
+    vars = VarsLoader(env_file, var, json_file, sys_env).load()
+    spec = yaml_file_to_dict(spec_file, vars, SECRETS)
+    ssm = BotoClient("ssm", dry_run=ctx.obj["boto_client"].dry_run)
+
+    for secret_name, value in spec.items():
+        common_ssm_params = {
+            "Name": secret_name,
+            "Overwrite": True,
+            "Tier": "Intelligent-Tiering",
+        }
+
+        if isinstance(value, str):
+            ssm_params = {
+                **common_ssm_params,
+                "Value": value,
+                "Type": "SecureString",
+            }
+        else:
+            ssm_params = {
+                **common_ssm_params,
+                "Value": value["Value"],
+                "Type": value["Type"],
+            }
+
+        click.echo(f"🔑 Storing secret {secret_name}.")
+        response = ssm.call("put_parameter", **ssm_params)
+        click.echo(f"\t✅ done, parameter version: {response['Version']}")
+
+
+@secrets.command()
+@click.argument("spec-file", type=str)
+@click.option(
+    "--filter",
+    type=str,
+    default=None,
+    help="Export only secrets matching given regexp pattern",
+)
+@common_options
+@click.pass_context
+def dump(
+    ctx,
+    spec_file,
+    filter,
+    env_file,
+    json_file,
+    var,
+    sys_env,
+):
+    """Dump secrets from Parameter Store."""
+    vars = VarsLoader(env_file, var, json_file, sys_env).load()
+    var_lut = generate_var_lut(vars)
+    ssm = BotoClient("ssm", dry_run=ctx.obj["boto_client"].dry_run)
+    secrets = dump_secrets(ssm, filter)
+    render_dumped_secrets(click, secrets, var_lut, spec_file)
+
+
+@service.command()
+@click.argument("task-definition-spec-file", type=str)
+@click.argument("service-spec-file", type=str)
+@common_options
+@wait_options(wait_for="update")
+@click.pass_context
+def deploy(
+    ctx,
+    task_definition_spec_file,
+    service_spec_file,
+    env_file,
+    json_file,
+    var,
+    sys_env,
+    wait,
+    wait_timeout,
+):
+    """All-in-one - register task definition and create or update service."""
+
+    vars = VarsLoader(env_file, var, json_file, sys_env).load()
+    task_definition_spec = yaml_file_to_dict(
+        task_definition_spec_file, vars, TASK_DEFINITION
+    )
+    task_family = task_definition_spec.get("family", "N/A")
+    click.echo(f"🗂 Registering task definition {task_family}.")
+    response = ctx.obj["boto_client"].call(
+        "register_task_definition", **task_definition_spec
+    )
+    task_definition_arn = response["taskDefinition"]["taskDefinitionArn"]
+    click.echo(f"\t✅ done, task definition arn: {task_definition_arn}.")
+
+    service_spec = yaml_file_to_dict(service_spec_file, vars, SERVICE)
+    service_name = service_spec.get("serviceName")
+    cluster_name = service_spec.get("cluster")
+    service_spec["taskDefinition"] = task_definition_arn
+
+    response = ctx.obj["boto_client"].call(
+        "describe_services",
+        cluster=service_spec["cluster"],
+        services=[service_name],
+    )
+    existing_services = list(
+        filter(lambda s: s["status"] != "INACTIVE", response["services"])
+    )
+    service_exists = len(existing_services) > 0
+
+    if service_exists:
+        click.echo(f"🏸 Updating service {service_name}.")
+        updater = ServiceUpdater()
+        service_spec = updater.make_update_payload(service_spec)
+        response = ctx.obj["boto_client"].call("update_service", **service_spec)
+        click.echo("\t✅ done.")
+    else:
+        click.echo(f"🏸 Creating service {service_name}.")
+        response = ctx.obj["boto_client"].call("create_service", **service_spec)
+        click.echo("\t✅ done.")
+    service_arn = response["service"]["serviceArn"]
+
+    if wait:
+        waiter = WaitForUpdate(
+            ctx.obj["boto_client"],
+            {cluster_name: [(service_arn, service_name)]},
+        )
+        waiter.timeout = wait_timeout
+        waiter.wait_for_all()
---
+++ packages/ECSctrl/ecsctrl-0.5.2.tar.gz/ecsctrl-0.5.2/ecsctrl/service_updater.py
@@ -0,0 +1,260 @@
+import math
+import os
+import re
+import sys
+from datetime import datetime, timezone
+from time import sleep, time
+from typing import Dict, List
+
+import click
+
+
+class TaskDefinitionServiceUpdater:
+    def __init__(
+        self, boto_client, task_definition_arn: str, cluster_name: str
+    ) -> None:
+        self.boto_client = boto_client
+        self.task_definition_arn = task_definition_arn
+        self.cluster_name = cluster_name
+        self.task_definition_family = re.findall(
+            r".+\/(.+)\:\d+?", self.task_definition_arn
+        )
+
+    def update(self) -> List[str]:
+        services = self.find_services_to_update()
+        for service_arn, service_name in services:
+            click.echo(f"🏗 Updating service {service_name}.")
+            self.update_service(service_arn)
+            click.echo("\t✅ done.")
+        return services
+
+    def find_services_to_update(self) -> List[str]:
+        services = []
+
+        kwargs = {}
+        while True:
+            list_response = self.boto_client.call(
+                "list_services", maxResults=10, cluster=self.cluster_name, **kwargs
+            )
+            describe_response = self.boto_client.call(
+                "describe_services",
+                cluster=self.cluster_name,
+                services=list_response["serviceArns"],
+            )
+
+            for service in describe_response["services"]:
+                if service["status"] == "INACTIVE":
+                    continue
+
+                task_definition = service["taskDefinition"]
+                service_task_name = re.findall(r".+\/(.+)\:\d+?", task_definition)
+                if service_task_name == self.task_definition_family:
+                    services.append((service["serviceArn"], service["serviceName"]))
+
+            if list_response.get("nextToken"):
+                kwargs["nextToken"] = list_response["nextToken"]
+            else:
+                break
+
+        return services
+
+    def update_service(self, service_arn: str):
+        self.boto_client.call(
+            "update_service",
+            taskDefinition=self.task_definition_arn,
+            cluster=self.cluster_name,
+            service=service_arn,
+        )
+
+
+class WaitForUpdate:
+    def __init__(self, boto_client, services_in_clusters: Dict[str, str]) -> None:
+        self.boto_client = boto_client
+        self.services_in_clusters = services_in_clusters
+        self.timeout = 600
+        self.wait_time = 60
+        self.min_task_age = 60
+
+    def describe_all_services(self):
+        described_services = []
+        for cluster, services in self.services_in_clusters.items():
+            arns = [service_arn for service_arn, service_name in services]
+            while arns:
+                response = self.boto_client.call(
+                    "describe_services", cluster=cluster, services=arns[:10]
+                )
+                for service_description in response["services"]:
+                    service_description["clusterName"] = cluster
+                    described_services.append(service_description)
+
+                arns = arns[10:]
+
+        return described_services
+
+    def wait_for_all(self):
+        total_failures = 1
+        total_critical = False
+        deadline = time() + self.timeout
+        start_time = time()
+
+        while total_failures and not total_critical:
+            total_failures = 0
+            total_critical = False
+            services = self.describe_all_services()
+            for service in services:
+                failures, critical = self.check_single_service(service)
+                total_failures += failures
+                total_critical = total_critical or critical
+                sleep(0.2)
+
+            if total_critical:
+                click.echo("💀 Oh no! Deployment failed. Exiting.")
+                sys.exit(1)
+
+            if total_failures == 0:
+                click.echo("🍾 All done.")
+                return
+            else:
+                if time() > deadline:
+                    click.echo("💀 Oh no! Timeout reached. Exiting.")
+                    sys.exit(1)
+                else:
+                    click.echo(
+                        f"⏳ Waiting for things to settle ({total_failures} check/s/ failed)"
+                    )
+
+                    pause_time = time()
+                    if not os.environ.get("CI"):
+                        animation = "🕐🕑🕒🕓🕔🕕🕖🕗🕘🕙🕚🕛"
+                        for i in range(self.wait_time * 10):
+                            sys.stdout.write("\r" + animation[i % len(animation)])
+                            sys.stdout.flush()
+                            sleep(0.1)
+                        sys.stdout.write("\r")
+                        sys.stdout.flush()
+                    else:
+                        sleep(self.wait_time)
+
+                    time_passed = math.floor(time() - start_time)
+                    resumed_after = math.floor(time() - pause_time)
+                    click.echo("")
+                    click.echo(
+                        f"🚀 Resuming after {resumed_after}s ({time_passed}s passed from the beginning) "
+                    )
+
+    def check_single_service(self, service_description):
+        failures = 0
+
+        cluster_name = service_description["clusterName"]
+        service_name = service_description["serviceName"]
+        service_task_definition = service_description["taskDefinition"]
+        service_task_desired_count = service_description["desiredCount"]
+        service_task_running_count = service_description["runningCount"]
+        service_task_pending_count = service_description["pendingCount"]
+
+        deployments = service_description["deployments"]
+        primary_deployment = [d for d in deployments if d["status"] == "PRIMARY"][0]
+
+        click.echo("🔍 Running checks")
+        click.echo(f"🌎 Cluster: {cluster_name}")
+        click.echo(f"🏓 Service: {service_name}")
+
+        click.echo(f"\t👮‍♀️ Desired task count: {service_task_desired_count}")
+
+        if service_task_desired_count == service_task_running_count:
+            click.echo(f"\t😀 Running task count: {service_task_running_count}")
+        else:
+            click.echo(f"\t😱 Running task count: {service_task_running_count}")
+            failures += 1
+
+        if service_task_pending_count == 0:
+            click.echo(f"\t😀 Pending task count: {service_task_pending_count}")
+        else:
+            click.echo(f"\t😱 Pending task count: {service_task_pending_count}")
+            failures += 1
+
+        click.echo(f"\t👮🏽‍♂️ Desired task definition: {service_task_definition}")
+
+        task_arns = self.boto_client.call(
+            "list_tasks", serviceName=service_name, cluster=cluster_name
+        )["taskArns"]
+
+        for task_arn in task_arns:
+            task = self.boto_client.call(
+                "describe_tasks", tasks=[task_arn], cluster=cluster_name
+            )["tasks"][0]
+
+            task_age = int(
+                datetime.now().replace(tzinfo=timezone.utc).timestamp()
+                - task["createdAt"].replace(tzinfo=timezone.utc).timestamp()
+            )
+            task_task_definition = task["taskDefinitionArn"]
+
+            if task_age >= self.min_task_age:
+                click.echo(f"\t😀 Task {task_arn} age is OK")
+            else:
+                click.echo(
+                    f"\t😱 Task {task_arn} is to young ({task_age}s, {self.min_task_age}s minimum)"
+                )
+                failures += 1
+
+            if task_task_definition == service_task_definition:
+                click.echo(f"\t😀 Task {task_arn} task definition is OK")
+            else:
+                click.echo(
+                    f"\t😱 Task {task_arn} task definition is {task_task_definition}"
+                )
+                failures += 1
+
+        if primary_deployment["rolloutState"] == "COMPLETED":
+            click.echo("\t😀 Primary deployment completed.")
+        elif primary_deployment["rolloutState"] == "IN_PROGRESS":
+            click.echo("\t🧑‍🔧 Primary deployment is still in progress.")
+        elif primary_deployment["rolloutState"] == "FAILED":
+            click.echo("\t💀 Oh no! Primary deployment failed.")
+            failures += 1
+            return failures, True
+
+        if not failures:
+            click.echo("\t✅ Service updated successfully.")
+
+        return failures, False
+
+
+class ServiceUpdater:
+    CREATE_TO_UPDATE = {
+        "serviceName": "service",
+    }
+
+    ALLOWED_FIELDS = [
+        "cluster",
+        "service",
+        "desiredCount",
+        "taskDefinition",
+        "capacityProviderStrategy",
+        "deploymentConfiguration",
+        "networkConfiguration",
+        "placementConstraints",
+        "placementStrategy",
+        "platformVersion",
+        "forceNewDeployment",
+        "healthCheckGracePeriodSeconds",
+        "enableExecuteCommand",
+        "enableECSManagedTags",
+        "loadBalancers",
+        "propagateTags",
+        "serviceRegistries",
+    ]
+
+    def make_update_payload(self, create_payload):
+        payload_with_translated_fields = {
+            self.CREATE_TO_UPDATE.get(k, k): v for k, v in create_payload.items()
+        }
+
+        update_payload = {
+            k: v
+            for k, v in payload_with_translated_fields.items()
+            if k in self.ALLOWED_FIELDS
+        }
+
+        return update_payload
