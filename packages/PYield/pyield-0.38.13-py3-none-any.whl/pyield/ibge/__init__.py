e10861e0dc8ac528223014a55d7d80b02da4332d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 05:05:01 +0000

    Add package PYield/0.38.13/pyield-0.38.13-py3-none-any.whl

---
+++ packages/PYield/pyield-0.38.13-py3-none-any.whl/pyield/ibge/__init__.py
@@ -0,0 +1,13 @@
+from pyield.ibge.ipca import (
+    ipca_indexes,
+    ipca_last_indexes,
+    ipca_last_rates,
+    ipca_rates,
+)
+
+__all__ = [
+    "ipca_indexes",
+    "ipca_last_indexes",
+    "ipca_rates",
+    "ipca_last_rates",
+]
