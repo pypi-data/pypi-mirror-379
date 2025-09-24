0c6022b1ecf173ec5ce3a750d0280f25d43253d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 05:04:15 +0000

    Add package Vease-Viewer/2.3.14rc12/vease_viewer-2.3.14rc12.tar.gz

---
+++ packages/Vease-Viewer/vease_viewer-2.3.14rc12.tar.gz/vease_viewer-2.3.14rc12/src/vease_viewer/rpc/protocols.py
@@ -0,0 +1,29 @@
+# Standard library imports
+import os
+import importlib.metadata as metadata
+
+# Third party imports
+from opengeodeweb_viewer.utils_functions import get_schemas_dict, validate_schema
+from vtk.web import protocols as vtk_protocols
+from wslink import register as exportRpc
+
+# Local application imports
+
+
+class VtkVeaseViewerView(vtk_protocols.vtkWebProtocol):
+    prefix = "vease_viewer."
+    schemas_dict = get_schemas_dict(os.path.join(os.path.dirname(__file__), "schemas"))
+
+    def __init__(self):
+        super().__init__()
+
+    @exportRpc(prefix + schemas_dict["microservice_version"]["rpc"])
+    def microservice_version(self, params):
+        print(
+            self.prefix + self.schemas_dict["microservice_version"]["rpc"],
+            f"{params=}",
+            flush=True,
+        )
+        validate_schema(params, self.schemas_dict["microservice_version"])
+
+        return {"microservice_version": metadata.distribution("vease_viewer").version}
