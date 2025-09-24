3b6cef468725a0a914e65ba4526f8ff4548992a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 03:15:08 +0000

    Add package mc-netease-sdk-nyrev/1.1.4/mc_netease_sdk_nyrev-1.1.4.tar.gz

---
+++ packages/mc-netease-sdk-nyrev/mc_netease_sdk_nyrev-1.1.4.tar.gz/mc_netease_sdk_nyrev-1.1.4/libs/mod/client/ui/CustomUIScreenProxy.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+class CustomUIScreenProxy(object):
+	def __init__(self, screenName, screenNode):
+		pass
+
+	def GetScreenNode(self):
+		pass
+
+	def GetScreenName(self):
+		pass
+
+	def OnCreate(self):
+		pass
+
+	def OnDestroy(self):
+		pass
+
+	def OnTick(self):
+		pass
