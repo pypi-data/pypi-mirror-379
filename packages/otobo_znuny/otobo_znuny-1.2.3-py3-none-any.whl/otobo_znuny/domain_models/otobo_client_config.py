9205b3e67bb5653f2dd11dbd2122c0c48270dac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 16:07:44 +0000

    Add package otobo_znuny/1.2.3/otobo_znuny-1.2.3-py3-none-any.whl

---
+++ packages/otobo_znuny/otobo_znuny-1.2.3-py3-none-any.whl/otobo_znuny/domain_models/otobo_client_config.py
@@ -0,0 +1,15 @@
+from typing import Dict
+
+from pydantic import BaseModel, Field
+
+from otobo_znuny.domain_models.basic_auth_model import BasicAuth
+from otobo_znuny.domain_models.ticket_operation import TicketOperation
+
+type OperationUrlMap = dict[TicketOperation, str]
+
+class ClientConfig(BaseModel):
+    base_url: str
+    webservice_name: str
+    operation_url_map: OperationUrlMap
+
+
