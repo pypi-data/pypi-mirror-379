546be78f67577bbf72bc6f14aedb4f456bdb0397
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 07:04:03 +0000

    Add package albert/1.6.1/albert-1.6.1-py3-none-any.whl

---
+++ packages/albert/albert-1.6.1-py3-none-any.whl/albert/resources/hazards.py
@@ -0,0 +1,13 @@
+from albert.core.shared.models.base import EntityLinkWithName
+
+
+class HazardSymbol(EntityLinkWithName):
+    """Model representing a hazard symbol."""
+
+    pass
+
+
+class HazardStatement(EntityLinkWithName):
+    """Model representing a hazard statement."""
+
+    pass
