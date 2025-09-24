e5eefeab54ba0b0addb740572e68b6dc876f5f05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 06:03:49 +0000

    Add package pytest-when/2.2.0/pytest_when-2.2.0.tar.gz

---
+++ packages/pytest-when/pytest_when-2.2.0.tar.gz/pytest_when-2.2.0/README.md
@@ -0,0 +1,204 @@
+[![PyPI](https://img.shields.io/pypi/v/pytest-when.svg)](https://pypi.python.org/pypi/pytest-when)
+[![Build](https://github.com/Tinche/tightwrap/workflows/CI/badge.svg)](https://github.com/zhukovgreen/pytest-when/actions/workflows/test-lint.yml?query=workflow:%22Test+Action%22)
+[![Supported Python Versions](https://img.shields.io/python/required-version-toml?tomlFilePath=https://raw.githubusercontent.com/zhukovgreen/pytest-when/main/pyproject.toml)](https://raw.githubusercontent.com/zhukovgreen/pytest-when/main/pyproject.toml)
+[![Ruff](https://img.shields.io/endpoint?url=https://raw.githubusercontent.com/astral-sh/ruff/main/assets/badge/v2.json)](https://github.com/astral-sh/ruff)
+
+# pytest-when
+
+<img src="./logo.png" width="250"/>
+
+Inspired
+by [mokito-scala](https://github.com/mockito/mockito-scala), `pytest-when`
+provides a fixture for `pytest` to simplify the mocking of python objects:
+
+## Purpose
+
+More readable than the full `Given...When...Then` pattern, `pytest-when` is
+meant for developers who want to test for behaviour, without any extra
+overhead.
+Enable the mock only for a specific argument's values to make code more
+readable.
+
+## Benefits
+
+In this example, when you specify the first two arguments and *any* third
+argument, the attribute will be mocked,
+
+```python
+(
+    when(some_object, "attribute")
+    .called_with(1, 2, when.markers.any)
+    .then_return("attribute mocked")
+    # or
+    # .then_call(some_callable)
+    # or
+    # .then_raise(SomeException())
+)
+```
+
+Note that the `.called_with` method arguments are compared with the real
+callable signature.
+This gives additional protection against changing the real callable interface.
+
+With `when` fixture, the following expression:
+
+```python
+when(example_module, "some_normal_function").called_with(
+    "a",
+    1,
+    kwarg1="b",
+    kwarg2=2,
+).then_return("Mocked")
+
+```
+
+is roughly equal to:
+
+```python
+
+mock_only_for_calls = (
+    call("a", 1, kwarg1="b", kwarg2=2),
+    "Mocked",
+)
+
+def matches(
+        call: Call,
+        mocked_calls_registry: tuple[tuple[call, ReturnType], ...],
+) -> bool:
+    ...
+
+def create_call(*args, **kwargs) -> Call:
+    ...
+
+def side_effect_callback(*args, **kwargs):
+    if matches(create_call(*args, **kwargs), mock_only_for_calls):
+        return mock_only_for_calls[1]
+    return unittest.mock.DEFAULT
+
+
+mocker.patch.object(
+    example_module,
+    "some_normal_function",
+    autospec=True,
+    side_effect=side_effect_callback,
+)
+```
+
+where logic of `matches` and `create_call` is not trivial
+
+## Installation
+
+Install the package into your development environment, from
+[pypi](https://pypi.org/project/pytest-when/), using `pip`, for example:
+
+```bash
+pip install pytest-when
+```
+
+## Implementation
+
+Onced installed, the `when` fixture will be available just like the rest of
+the `pytest` plugins.
+See the following example of how to use it:
+
+```python
+# class which we're going to mock in the test
+class Klass1:
+    def some_method(
+            self,
+            arg1: str,
+            arg2: int,
+            *,
+            kwarg1: str,
+            kwarg2: str,
+    ) -> str:
+        return "some_method not mocked"
+
+
+def test_should_properly_patch_calls(when):
+    when(Klass1, "some_method").called_with(
+        "a",
+        when.markers.any,
+        kwarg1="b",
+        kwarg2=when.markers.any,
+    ).then_return("some method mocked")
+
+    assert (
+            Klass1().some_method(
+                "a",
+                1,
+                kwarg1="b",
+                kwarg2="c",
+            )
+            == "some method mocked"
+    )
+    assert (
+            Klass1().some_method(
+                "not mocked param",
+                1,
+                kwarg1="b",
+                kwarg2="c",
+            )
+            == "some method not mocked"
+    )
+
+
+# if you need to patch a function
+def test_patch_a_function(when):
+    when(example_module, "some_normal_function").called_with(
+        "a",
+        when.markers.any,
+        kwarg1="b",
+        kwarg2=when.markers.any,
+    ).then_return("some_normal_function mocked")
+
+    assert (
+            example_module.some_normal_function(
+                "a",
+                1,
+                kwarg1="b",
+                kwarg2="c",
+            )
+            == "some_normal_function mocked"
+    )
+    assert (
+            example_module.some_normal_function(
+                "not mocked param",
+                1,
+                kwarg1="b",
+                kwarg2="c",
+            )
+            == "some_normal_function not mocked"
+    )
+```
+
+It is possible to use `when` with class methods and standalone functions
+(in this case cls parameter will become a python module).
+
+You can patch the same object multiple times using different `called_with`
+parameters in a single test.
+
+You can also patch multiple targets (cls, method)
+
+See more examples at:
+[test_integration](tests/test_integration.py)
+
+## Setup for local developement
+
+The project can be extended by cloning the repo and
+installing [the `PDM` build tool](https://pdm-project.org/latest/#recommended-installation-method)
+So, the development environment requires:
+
+1. pdm <https://pdm.fming.dev/latest/#installation>
+2. python3.8 or greater
+
+```bash
+pdm install
+```
+
+To run tests and linters use:
+
+```bash
+make test
+make lint
+```
---
+++ packages/pytest-when/pytest_when-2.2.0.tar.gz/pytest_when-2.2.0/tests/test_integration.py
@@ -0,0 +1,373 @@
+import re
+
+import pytest
+
+from tests.resources import example_module
+
+
+class Klass1:
+    def some_method(
+        self,
+        arg1: str,
+        arg2: int,
+        *,
+        kwarg1: str,
+        kwarg2: str,
+    ) -> str:
+        return "Not mocked"
+
+    def some_method_with_defaults(
+        self,
+        arg1: str,
+        arg2: int,
+        *,
+        kwarg1: str,
+        kwarg2: str = "some default string",
+    ) -> str:
+        return "Not mocked"
+
+    @classmethod
+    def some_class_method(
+        cls,
+        arg1: str,
+        arg2: int,
+        *,
+        kwarg1: str,
+        kwarg2: str,
+    ) -> str:
+        return "Not mocked"
+
+
+class Klass2:
+    def some_method(
+        self,
+        arg1: str,
+        arg2: int,
+        *,
+        kwarg1: str,
+        kwarg2: str,
+    ) -> str:
+        return "Not mocked"
+
+
+def test_should_properly_patch_calls(when):
+    when(Klass1, "some_method").called_with(
+        "a",
+        when.markers.any,
+        kwarg1="b",
+        kwarg2=when.markers.any,
+    ).then_return("Mocked")
+
+    assert (
+        Klass1().some_method(
+            "a",
+            1,
+            kwarg1="b",
+            kwarg2="c",
+        )
+        == "Mocked"
+    )
+    assert (
+        Klass1().some_method(
+            "not mocked param",
+            1,
+            kwarg1="b",
+            kwarg2="c",
+        )
+        == "Not mocked"
+    )
+
+
+def test_should_work_with_classmethods(when):
+    when(Klass1, "some_class_method").called_with(
+        "a",
+        when.markers.any,
+        kwarg1="b",
+        kwarg2=when.markers.any,
+    ).then_return("Mocked")
+
+    assert (
+        Klass1().some_class_method(
+            "a",
+            1,
+            kwarg1="b",
+            kwarg2="c",
+        )
+        == "Mocked"
+    )
+    assert (
+        Klass1.some_class_method(
+            "not mocked param",
+            1,
+            kwarg1="b",
+            kwarg2="c",
+        )
+        == "Not mocked"
+    )
+
+
+def test_should_work_with_normal_functions(when):
+    when(example_module, "some_normal_function").called_with(
+        "a",
+        when.markers.any,
+        kwarg1="b",
+        kwarg2=when.markers.any,
+    ).then_return("Mocked")
+
+    assert (
+        example_module.some_normal_function(
+            "a",
+            1,
+            kwarg1="b",
+            kwarg2="c",
+        )
+        == "Mocked"
+    )
+    assert (
+        example_module.some_normal_function(
+            "not mocked param",
+            1,
+            kwarg1="b",
+            kwarg2="c",
+        )
+        == "Not mocked"
+    )
+
+
+def test_should_be_able_to_patch_multiple_calls(when):
+    when(Klass1, "some_method").called_with(
+        "a",
+        when.markers.any,
+        kwarg1="b",
+        kwarg2=when.markers.any,
+    ).then_return("Mocked first time")
+
+    when(Klass1, "some_method").called_with(
+        when.markers.any,
+        1,
+        kwarg1=when.markers.any,
+        kwarg2="b",
+    ).then_return("Mocked second time")
+
+    assert (
+        Klass1().some_method(
+            "a",
+            1,
+            kwarg1="b",
+            kwarg2="c",
+        )
+        == "Mocked first time"
+    )
+    assert (
+        Klass1().some_method(
+            "any",
+            1,
+            kwarg1="any too",
+            kwarg2="b",
+        )
+        == "Mocked second time"
+    )
+    assert (
+        Klass1().some_method(
+            "not mocked param",
+            1,
+            kwarg1="b",
+            kwarg2="c",
+        )
+        == "Not mocked"
+    )
+
+
+def test_should_properly_repatch(when):
+    when(Klass1, "some_method").called_with(
+        "a",
+        when.markers.any,
+        kwarg1="b",
+        kwarg2=when.markers.any,
+    ).then_return("Mocked first time")
+
+    when(Klass1, "some_method").called_with(
+        "a",
+        when.markers.any,
+        kwarg1="b",
+        kwarg2=when.markers.any,
+    ).then_return("Mocked second time")
+
+    assert (
+        Klass1().some_method(
+            "a",
+            1,
+            kwarg1="b",
+            kwarg2="c",
+        )
+        == "Mocked second time"
+    )
+
+
+def test_should_be_able_to_patch_multiple_objects(when):
+    when(Klass1, "some_method").called_with(
+        "a",
+        when.markers.any,
+        kwarg1="b",
+        kwarg2=when.markers.any,
+    ).then_return("Mocked Klass1")
+
+    when(Klass2, "some_method").called_with(
+        "b",
+        when.markers.any,
+        kwarg1="c",
+        kwarg2=when.markers.any,
+    ).then_return("Mocked Klass2")
+
+    assert (
+        Klass1().some_method(
+            "a",
+            1,
+            kwarg1="b",
+            kwarg2="c",
+        )
+        == "Mocked Klass1"
+    )
+    assert (
+        Klass1().some_method(
+            "not mocked param",
+            1,
+            kwarg1="b",
+            kwarg2="c",
+        )
+        == "Not mocked"
+    )
+
+    assert (
+        Klass2().some_method(
+            "b",
+            1,
+            kwarg1="c",
+            kwarg2="any",
+        )
+        == "Mocked Klass2"
+    )
+    assert (
+        Klass2().some_method(
+            "not mocked param",
+            1,
+            kwarg1="b",
+            kwarg2="c",
+        )
+        == "Not mocked"
+    )
+
+
+def test_should_work_with_default_params_in_functions(when):
+    patched_klass = (
+        when(Klass1, "some_method_with_defaults")
+        .called_with(
+            when.markers.any,
+            when.markers.any,
+            kwarg1=when.markers.any,
+            kwarg2="some default string",
+        )
+        .then_return("Mocked")
+    )
+
+    assert (
+        Klass1().some_method_with_defaults(
+            "a",
+            1,
+            kwarg1="b",
+            kwarg2="some default string",
+        )
+        == "Mocked"
+    )
+    assert (
+        Klass1().some_method(
+            "not mocked param",
+            1,
+            kwarg1="b",
+            kwarg2="c",
+        )
+        == "Not mocked"
+    )
+    patched_klass.assert_called()
+
+
+def test_should_work_with_variadic_args_kwargs(when):
+    patched_foo = (
+        when(example_module, "some_foo_with_variadic_args_kwargs")
+        .called_with(1, a=2)
+        .then_return("Mocked")
+    )
+    assert (
+        example_module.some_foo_with_variadic_args_kwargs(1, a=2) == "Mocked"
+    )
+    assert (
+        example_module.some_foo_with_variadic_args_kwargs(1, a=3)
+        == "Not mocked"
+    )
+    patched_foo.assert_called()
+    patched_foo = (
+        when(example_module, "some_foo_with_variadic_args_kwargs")
+        .called_with(2, 3, 4, a=1)
+        .then_return("Mocked1")
+    )
+    assert (
+        example_module.some_foo_with_variadic_args_kwargs(2, 3, 4, a=1)
+        == "Mocked1"
+    )
+
+    assert (
+        example_module.some_foo_with_variadic_args_kwargs(3, 2, 4, a=1)
+        == "Not mocked"
+    )
+
+
+def test_should_work_with_foo_without_args(when):
+    patched_foo = (
+        when(example_module, "some_foo_without_args")
+        .called_with()
+        .then_return("Mocked")
+    )
+    assert example_module.some_foo_without_args() == "Mocked"
+    patched_foo.assert_called()
+
+
+def test_should_work_with_star_kwargs(when, mocker):
+    class _:  # noqa: N801
+        @staticmethod
+        def foo(a, **kwargs):  # noqa: ARG004
+            return "Not mocked"
+
+    patched_foo = (
+        when(_, "foo")
+        .called_with(
+            1,
+            kwarg_a=when.markers.any,
+            kwarg_b="bbb",
+        )
+        .then_return("Mocked")
+    )
+    assert _.foo(1, kwarg_b="bbb", kwarg_a="aaa") == "Mocked"
+    assert _.foo(1, kwarg_a="aaa", kwarg_b="bbb") == "Mocked"
+    assert _.foo(2, kwarg_a="aaa", kwarg_b="bbb") == "Not mocked"
+    patched_foo.assert_called()
+
+
+def test_then_call_should_work(when):
+    patched_foo = (
+        when(example_module, "some_foo_without_args")
+        .called_with()
+        .then_call(lambda: "Mocked")
+    )
+    assert example_module.some_foo_without_args() == "Mocked"
+    patched_foo.assert_called()
+
+
+def test_then_raise_should_work(when):
+    patched_foo = (
+        when(example_module, "some_foo_without_args")
+        .called_with()
+        .then_raise(ValueError("Error msg"))
+    )
+    with pytest.raises(ValueError, match=re.compile("Error msg")):
+        example_module.some_foo_without_args()
+    patched_foo.assert_called()
