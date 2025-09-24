745ee706f7e4ee44a5f5edbe1fa14f2fb550793f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 17:05:27 +0000

    Add package fake-bpy-module/20250924/fake_bpy_module-20250924-py3-none-any.whl

---
+++ packages/fake-bpy-module/fake_bpy_module-20250924-py3-none-any.whl/bl_ui/node_add_menu_texture/__init__.pyi
@@ -0,0 +1,242 @@
+import typing
+import collections.abc
+import typing_extensions
+import numpy.typing as npt
+import _bpy_types
+import bpy.types
+
+class NODE_MT_category_texture_color(_bpy_types.Menu):
+    bl_idname: typing.Any
+    bl_label: typing.Any
+    bl_rna: typing.Any
+    id_data: typing.Any
+
+    def bl_rna_get_subclass(self) -> bpy.types.Struct:
+        """
+
+        :return: The RNA type or default when not found.
+        :rtype: bpy.types.Struct
+        """
+
+    def bl_rna_get_subclass_py(self) -> typing.Any:
+        """
+
+        :return: The class or default when not found.
+        :rtype: typing.Any
+        """
+
+    def draw(self, _context) -> None:
+        """
+
+        :param _context:
+        """
+
+class NODE_MT_category_texture_converter(_bpy_types.Menu):
+    bl_idname: typing.Any
+    bl_label: typing.Any
+    bl_rna: typing.Any
+    id_data: typing.Any
+
+    def bl_rna_get_subclass(self) -> bpy.types.Struct:
+        """
+
+        :return: The RNA type or default when not found.
+        :rtype: bpy.types.Struct
+        """
+
+    def bl_rna_get_subclass_py(self) -> typing.Any:
+        """
+
+        :return: The class or default when not found.
+        :rtype: typing.Any
+        """
+
+    def draw(self, _context) -> None:
+        """
+
+        :param _context:
+        """
+
+class NODE_MT_category_texture_distort(_bpy_types.Menu):
+    bl_idname: typing.Any
+    bl_label: typing.Any
+    bl_rna: typing.Any
+    id_data: typing.Any
+
+    def bl_rna_get_subclass(self) -> bpy.types.Struct:
+        """
+
+        :return: The RNA type or default when not found.
+        :rtype: bpy.types.Struct
+        """
+
+    def bl_rna_get_subclass_py(self) -> typing.Any:
+        """
+
+        :return: The class or default when not found.
+        :rtype: typing.Any
+        """
+
+    def draw(self, _context) -> None:
+        """
+
+        :param _context:
+        """
+
+class NODE_MT_category_texture_group(_bpy_types.Menu):
+    bl_idname: typing.Any
+    bl_label: typing.Any
+    bl_rna: typing.Any
+    id_data: typing.Any
+
+    def bl_rna_get_subclass(self) -> bpy.types.Struct:
+        """
+
+        :return: The RNA type or default when not found.
+        :rtype: bpy.types.Struct
+        """
+
+    def bl_rna_get_subclass_py(self) -> typing.Any:
+        """
+
+        :return: The class or default when not found.
+        :rtype: typing.Any
+        """
+
+    def draw(self, context) -> None:
+        """
+
+        :param context:
+        """
+
+class NODE_MT_category_texture_input(_bpy_types.Menu):
+    bl_idname: typing.Any
+    bl_label: typing.Any
+    bl_rna: typing.Any
+    id_data: typing.Any
+
+    def bl_rna_get_subclass(self) -> bpy.types.Struct:
+        """
+
+        :return: The RNA type or default when not found.
+        :rtype: bpy.types.Struct
+        """
+
+    def bl_rna_get_subclass_py(self) -> typing.Any:
+        """
+
+        :return: The class or default when not found.
+        :rtype: typing.Any
+        """
+
+    def draw(self, _context) -> None:
+        """
+
+        :param _context:
+        """
+
+class NODE_MT_category_texture_output(_bpy_types.Menu):
+    bl_idname: typing.Any
+    bl_label: typing.Any
+    bl_rna: typing.Any
+    id_data: typing.Any
+
+    def bl_rna_get_subclass(self) -> bpy.types.Struct:
+        """
+
+        :return: The RNA type or default when not found.
+        :rtype: bpy.types.Struct
+        """
+
+    def bl_rna_get_subclass_py(self) -> typing.Any:
+        """
+
+        :return: The class or default when not found.
+        :rtype: typing.Any
+        """
+
+    def draw(self, _context) -> None:
+        """
+
+        :param _context:
+        """
+
+class NODE_MT_category_texture_pattern(_bpy_types.Menu):
+    bl_idname: typing.Any
+    bl_label: typing.Any
+    bl_rna: typing.Any
+    bl_translation_context: typing.Any
+    id_data: typing.Any
+
+    def bl_rna_get_subclass(self) -> bpy.types.Struct:
+        """
+
+        :return: The RNA type or default when not found.
+        :rtype: bpy.types.Struct
+        """
+
+    def bl_rna_get_subclass_py(self) -> typing.Any:
+        """
+
+        :return: The class or default when not found.
+        :rtype: typing.Any
+        """
+
+    def draw(self, _context) -> None:
+        """
+
+        :param _context:
+        """
+
+class NODE_MT_category_texture_texture(_bpy_types.Menu):
+    bl_idname: typing.Any
+    bl_label: typing.Any
+    bl_rna: typing.Any
+    id_data: typing.Any
+
+    def bl_rna_get_subclass(self) -> bpy.types.Struct:
+        """
+
+        :return: The RNA type or default when not found.
+        :rtype: bpy.types.Struct
+        """
+
+    def bl_rna_get_subclass_py(self) -> typing.Any:
+        """
+
+        :return: The class or default when not found.
+        :rtype: typing.Any
+        """
+
+    def draw(self, _context) -> None:
+        """
+
+        :param _context:
+        """
+
+class NODE_MT_texture_node_add_all(_bpy_types.Menu):
+    bl_idname: typing.Any
+    bl_label: typing.Any
+    bl_rna: typing.Any
+    bl_translation_context: typing.Any
+    id_data: typing.Any
+
+    def bl_rna_get_subclass(self) -> bpy.types.Struct:
+        """
+
+        :return: The RNA type or default when not found.
+        :rtype: bpy.types.Struct
+        """
+
+    def bl_rna_get_subclass_py(self) -> typing.Any:
+        """
+
+        :return: The class or default when not found.
+        :rtype: typing.Any
+        """
+
+    def draw(self, _context) -> None:
+        """
+
+        :param _context:
+        """
