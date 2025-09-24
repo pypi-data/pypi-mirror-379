0453f2d7ae9c9cc581872f664389becadaded740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 07:04:51 +0000

    Add package pys7/1.1.0/pys7-1.1.0-py3-none-any.whl

---
+++ packages/pys7/pys7-1.1.0-py3-none-any.whl/pyS7/__init__.py
@@ -0,0 +1,12 @@
+from .address_parser import map_address_to_tag
+from .client import S7Client
+from .constants import ConnectionType, DataType, MemoryArea
+from .errors import (
+    S7AddressError,
+    S7CommunicationError,
+    S7ConnectionError,
+    S7Error,
+    S7ReadResponseError,
+    S7WriteResponseError,
+)
+from .tag import S7Tag
