7688219ae9c8fbbf98351dd66c3cd6220951cfd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 04:05:16 +0000

    Add package seapopym/0.0.2.5/seapopym-0.0.2.5-py3-none-any.whl

---
+++ packages/seapopym/seapopym-0.0.2.5-py3-none-any.whl/seapopym/model/acidity_model.py
@@ -0,0 +1,41 @@
+"""The no transport model with acidity-induced mortality."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from seapopym import function
+from seapopym.core.kernel import kernel_factory
+from seapopym.model.no_transport_model import NoTransportModel
+
+if TYPE_CHECKING:
+    from seapopym.configuration.acidity import AcidityConfiguration
+
+AcidityKernel = kernel_factory(
+    class_name="AcidityKernel",
+    kernel_unit=[
+        function.GlobalMaskKernel,
+        function.MaskByFunctionalGroupKernel,
+        function.DayLengthKernel,
+        function.AverageTemperatureKernel,
+        function.AverageAcidityKernel,
+        function.PrimaryProductionByFgroupKernel,
+        function.MinTemperatureByCohortKernel,
+        function.MaskTemperatureKernel,
+        function.MortalityTemperatureAcidityKernel,
+        function.production.ProductionKernel,
+        function.BiomassKernel,
+    ],
+)
+
+
+class AcidityModel(NoTransportModel):
+    """A pteropod 1D model that takes into account the mortality due to ocean acidification."""
+
+    @classmethod
+    def from_configuration(cls: type[AcidityModel], configuration: AcidityConfiguration) -> AcidityModel:
+        """Create a model from a configuration."""
+        state = configuration.state
+        chunk = configuration.forcing.chunk.as_dict()
+        parallel = configuration.forcing.parallel
+        return cls(state=state, kernel=AcidityKernel(chunk=chunk, parallel=parallel))
