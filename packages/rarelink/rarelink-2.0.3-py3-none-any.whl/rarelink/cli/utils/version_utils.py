f8f66fa9db2fa4396c33d9949aa35a49cb04c5f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 06:03:47 +0000

    Add package rarelink/2.0.3/rarelink-2.0.3-py3-none-any.whl

---
+++ packages/rarelink/rarelink-2.0.3-py3-none-any.whl/rarelink/cli/utils/version_utils.py
@@ -0,0 +1,10 @@
+
+
+def get_current_version() -> str:
+    """
+    Simulate fetching the most current RD-CDM version.
+    Will be extended to dynamically fetch the version from an API.
+    """
+    return "v2_0_2"
+
+
