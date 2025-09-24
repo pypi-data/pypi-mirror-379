21a2fa4bf8edbafecd685392299a71d13566b287
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 14:04:37 +0000

    Add package biblemate/0.0.81/biblemate-0.0.81.tar.gz

---
+++ packages/biblemate/biblemate-0.0.81.tar.gz/biblemate-0.0.81/biblemate/__init__.py
@@ -0,0 +1,11 @@
+from agentmake import AGENTMAKE_USER_DIR
+import os
+
+AGENTMAKE_CONFIG = {
+    "print_on_terminal": False,
+    "word_wrap": False,
+}
+OLLAMA_NOT_FOUND = "`Ollama` is not found! BibleMate AI uses `Ollama` to generate embeddings for semantic searches. You may install it from https://ollama.com/ so that you can perform semantic searches of the Bible with BibleMate AI."
+def fix_string(content):
+    return content.replace(" ", " ").replace("‑", "-")
+BIBLEMATEDATA = os.path.join(AGENTMAKE_USER_DIR, "biblemate", "data")
---
+++ packages/biblemate/biblemate-0.0.81.tar.gz/biblemate-0.0.81/biblemate/config.py
@@ -0,0 +1,13 @@
+agent_mode=False
+prompt_engineering=False
+max_steps=30
+lite=True
+hide_tools_order=True
+default_bible="NET"
+default_commentary="CBSC"
+default_encyclopedia="ISB"
+max_semantic_matches=15
+max_log_lines=2000
+mcp_port=33333
+embedding_model="paraphrase-multilingual"
+disabled_tools=['uba']
