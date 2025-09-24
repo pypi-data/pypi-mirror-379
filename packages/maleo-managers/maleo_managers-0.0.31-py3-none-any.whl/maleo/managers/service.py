10d5c0891cd719f869b3a88678a0ef7afb6406a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 04:05:17 +0000

    Add package maleo-managers/0.0.31/maleo_managers-0.0.31-py3-none-any.whl

---
+++ packages/maleo-managers/maleo_managers-0.0.31-py3-none-any.whl/maleo/managers/service.py
@@ -0,0 +1,45 @@
+from abc import ABC, abstractmethod
+from google.oauth2.service_account import Credentials
+from typing import Generic
+from maleo.schemas.service import ServiceContext
+from maleo.schemas.settings import ServiceSettingsT
+from maleo.logging.config import Config as LogConfig
+from maleo.logging.logger import ServiceLoggers
+from .config import ConfigT
+
+
+class ServiceManager(ABC, Generic[ServiceSettingsT, ConfigT]):
+    """ServiceManager class"""
+
+    def __init__(
+        self,
+        google_credentials: Credentials,
+        log_config: LogConfig,
+        settings: ServiceSettingsT,
+        config: ConfigT,
+    ):
+        self._google_credentials = google_credentials
+        self._log_config = log_config
+        self._settings = settings
+        self._config = config
+
+        self._service_context = ServiceContext(
+            environment=self._settings.ENVIRONMENT, key=self._settings.SERVICE_KEY
+        )
+
+        self._initialize_loggers()
+
+    def _initialize_loggers(self) -> None:
+        self.loggers = ServiceLoggers.new(
+            environment=self._settings.ENVIRONMENT,
+            service_key=self._settings.SERVICE_KEY,
+            config=self._log_config,
+        )
+
+    @abstractmethod
+    def _initialize_database(self):
+        """Initialize all given databases"""
+
+    @abstractmethod
+    def _initialize_google_cloud_storage(self):
+        """Initialize Google Cloud Storage"""
