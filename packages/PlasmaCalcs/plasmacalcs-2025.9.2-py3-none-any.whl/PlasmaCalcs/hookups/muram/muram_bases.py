f961ac84555a6ce67e69283ec808a4ce9d6140a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 05:04:09 +0000

    Add package PlasmaCalcs/2025.9.2/plasmacalcs-2025.9.2-py3-none-any.whl

---
+++ packages/PlasmaCalcs/plasmacalcs-2025.9.2-py3-none-any.whl/PlasmaCalcs/hookups/muram/muram_bases.py
@@ -0,0 +1,82 @@
+"""
+File Purpose: base quantities for MuramCalculator
+"""
+
+from ...mhd import MhdBasesLoader
+import numpy as np
+import xarray as xr
+
+### --------------------- MuramBasesLoader --------------------- ###
+
+class MuramBasesLoader(MhdBasesLoader):
+    '''base quantities based on Muram output.'''
+    @known_var(dims=['snap'])
+    def get_r(self):
+        '''mass density. (directly from Muram)
+        assumes single fluid mode, i.e. result corresponds to the single fluid from Muram.
+        '''
+        self.assert_single_fluid_mode('r')
+        return self.load_maindims_var_across_dims('result_prim_r', u='mass_density', dims=['snap'])
+
+    @known_var(dims=['snap', 'component'])
+    def get_u(self):
+        '''velocity. (directly from Muram)
+        assumes single fluid mode, i.e. result corresponds to the single fluid from Muram.
+        '''
+        self.assert_single_fluid_mode('u')
+        return self.load_maindims_var_across_dims('result_prim_u', u='speed', dims=['snap', 'component'])
+
+    @known_var(dims=['snap'])
+    def get_e(self):
+        '''energy density. (directly from Muram)
+        Per unit volume, e.g. the SI units would be Joules / meter^3.
+        assumes single fluid mode, i.e. result corresponds to the single fluid from Muram.
+        '''
+        self.assert_single_fluid_mode('e')
+        return self.load_maindims_var_across_dims('result_prim_e', u='energy_density', dims=['snap'])
+
+    @known_var(dims=['snap'])
+    def get_T(self):
+        '''Temperature. (directly from Muram)
+        Per unit volume, e.g. the SI units would be K.
+        assumes single fluid mode, i.e. result corresponds to the single fluid from Muram.
+        '''
+        self.assert_single_fluid_mode('T')
+        return self.load_maindims_var_across_dims('result_prim_eosT', u='temperature', dims=['snap'])
+
+    @known_var(dims=['snap'])
+    def get_ne(self):
+        '''Electron density. (directly from Muram)
+        Per unit volume, e.g. the SI units would be m^3.
+        assumes single fluid mode, i.e. result corresponds to the single fluid from Muram.
+        '''
+        self.assert_single_fluid_mode('ne')
+        return self.load_maindims_var_across_dims('result_prim_eosne', u=None, dims=['snap'])
+ 
+    @known_var(dims=['snap'])
+    def get_P(self):
+        '''Pressure. (directly from Muram)
+        Per unit volume, e.g. the SI units would be .
+        assumes single fluid mode, i.e. result corresponds to the single fluid from Muram.
+        '''
+        self.assert_single_fluid_mode('P')
+        return self.load_maindims_var_across_dims('result_prim_eosP', u=None, dims=['snap'])
+    
+    @known_var(dims=['snap', 'component'])
+    def get_B(self):
+        '''magnetic field. (directly from Muram)'''
+        return self.load_maindims_var_across_dims('result_prim_b', u='b', dims=['snap', 'component'])
+
+    @known_var
+    def get_gamma(self):
+        '''adiabatic index. Assumed to be 5/3.'''
+        return 5/3.
+
+    @known_pattern(r'd([xyz])')
+    def get_ds(self, var, *, _match=None):
+        '''dx (or dy, or dz) --> grid cell size along the x (or y, or z) axis.
+        result is probably a 0D xr.DataArray.
+        '''
+        x, = _match.groups()   
+        dx = self.params[f'd{x}'] * self.u('length')
+        return xr.DataArray(dx)
