4fd0a62d679d279d02d271f1579c48d2386db566
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 03:14:36 +0000

    Add package investing-algorithm-framework/7.9.1/investing_algorithm_framework-7.9.1.tar.gz

---
+++ packages/investing-algorithm-framework/investing_algorithm_framework-7.9.1.tar.gz/investing_algorithm_framework-7.9.1/investing_algorithm_framework/domain/backtesting/__init__.py
@@ -0,0 +1,19 @@
+from .backtest_summary_metrics import BacktestSummaryMetrics
+from .backtest_date_range import BacktestDateRange
+from .backtest_metrics import BacktestMetrics
+from .backtest_run import BacktestRun
+from .backtest import Backtest
+from .backtest_permutation_test import BacktestPermutationTest
+from .backtest_evaluation_focuss import BacktestEvaluationFocus
+from .combine_backtests import combine_backtests
+
+__all__ = [
+    "Backtest",
+    "BacktestSummaryMetrics",
+    "BacktestDateRange",
+    "BacktestMetrics",
+    "BacktestRun",
+    "BacktestPermutationTest",
+    "BacktestEvaluationFocus",
+    "combine_backtests"
+]
