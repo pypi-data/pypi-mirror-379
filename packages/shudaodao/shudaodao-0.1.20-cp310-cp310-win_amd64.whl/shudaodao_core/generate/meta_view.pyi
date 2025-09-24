d843e6b226472ebb559a7d2b6fb685627af24770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 03:15:15 +0000

    Add package shudaodao/0.1.20/shudaodao-0.1.20-cp310-cp310-win_amd64.whl

---
+++ packages/shudaodao/shudaodao-0.1.20-cp310-cp310-win_amd64.whl/shudaodao_core/generate/meta_view.pyi
@@ -0,0 +1,9 @@
+from .meta_column import MetaColumn as MetaColumn
+from .meta_table import MetaTable as MetaTable
+from dataclasses import dataclass
+
+@dataclass
+class MetaView(MetaTable):
+    def __init__(self, *args, **kwargs) -> None: ...
+    def get_columns(self) -> list[MetaColumn]: ...
+    def get_import_field(self) -> str: ...
