6446a5592c73efe05d3f2ff48209ab46b8f82a83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 04:05:24 +0000

    Add package webdiver/8.0/webdiver-8.0.tar.gz

---
+++ packages/webdiver/webdiver-8.0.tar.gz/webdiver-8.0/web_diver/examples/__init__.py
@@ -0,0 +1,6 @@
+import web_diver as _WEBDIVER
+
+_c0x_x0 = 'https://www.bondfaro.com.br/notebook'
+
+_WEBDIVER.www_diver_add_task(_c0x_x0)
+_WEBDIVER.www_diver_start('db_plugwarez_0x1.sqlite','_text')
