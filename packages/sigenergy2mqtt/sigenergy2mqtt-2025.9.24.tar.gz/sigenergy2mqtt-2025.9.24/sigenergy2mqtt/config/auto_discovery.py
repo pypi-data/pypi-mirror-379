d1f78bd144d7a400268712bdd7ba1bb4b7970bcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 17:04:38 +0000

    Add package sigenergy2mqtt/2025.9.24/sigenergy2mqtt-2025.9.24.tar.gz

---
+++ packages/sigenergy2mqtt/sigenergy2mqtt-2025.9.24.tar.gz/sigenergy2mqtt-2025.9.24/sigenergy2mqtt/config/auto_discovery.py
@@ -0,0 +1,192 @@
+from pymodbus import ExceptionResponse, FramerType, ModbusException
+from pymodbus.client import AsyncModbusTcpClient
+from scapy.all import ARP, Ether, srp, IP, ICMP, sr1
+import asyncio
+import ipaddress
+import logging
+import psutil
+import queue
+import threading
+import time
+
+
+def arp_worker(ip_chunk, result_dict, lock, timeout=2) -> None:
+    arp = ARP(pdst=ip_chunk)
+    ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+    packet = ether / arp
+
+    responses = srp(packet, timeout=timeout, verbose=0)[0]
+    with lock:
+        for _, received in responses:
+            result_dict[received.psrc] = received.hwsrc
+
+
+def arp_scan(network_cidr, threads=10, timeout=1) -> list[dict[str, str]]:
+    ip_list = [str(ip) for ip in ipaddress.IPv4Network(network_cidr, strict=False).hosts()]
+    chunk_size = len(ip_list) // threads + 1
+    chunks = [ip_list[i : i + chunk_size] for i in range(0, len(ip_list), chunk_size)]
+
+    result_dict = {}
+    lock = threading.Lock()
+    thread_list = []
+
+    for chunk in chunks:
+        t = threading.Thread(target=arp_worker, args=(chunk, result_dict, lock, timeout))
+        t.start()
+        thread_list.append(t)
+
+    for t in thread_list:
+        t.join()
+
+    return result_dict
+
+
+def ping_worker(ip_queue, result_list, timeout=0.5) -> None:
+    while not ip_queue.empty():
+        ip = ip_queue.get()
+        pkt = IP(dst=ip) / ICMP()
+        reply = sr1(pkt, timeout=timeout, verbose=0)
+        if reply:
+            result_list.append(ip)
+        ip_queue.task_done()
+
+
+def ping_scan(ip_list, threads=100, timeout=1) -> list[str]:
+    ip_queue = queue.Queue()
+    result_list = []
+
+    for ip in ip_list:
+        ip_queue.put(ip)
+
+    for _ in range(threads):
+        t = threading.Thread(target=ping_worker, args=(ip_queue, result_list))
+        t.daemon = True
+        t.start()
+
+    ip_queue.join()
+    return result_list
+
+
+async def probe_worker(modbus: AsyncModbusTcpClient, address: int, count: int = 1, device_id: int = 247) -> bool:
+    try:
+        result = await modbus.read_input_registers(address=address, count=count, device_id=device_id)
+        if result and not result.isError():
+            return True
+    except ModbusException:  # as e:
+        # logging.debug(f"Modbus probe failed for {modbus.comm_params.host}:{modbus.comm_params.port} at address {address} with device_id {device_id}: {e}")
+        while not modbus.connected:
+            modbus.close()
+            await modbus.connect()
+    except Exception as e:
+        logging.debug(f"Unexpected error during Modbus probe for {modbus.comm_params.host}:{modbus.comm_params.port} at address {address} with device_id {device_id}: {e}")
+    return False
+
+
+async def get_serial_number(modbus: AsyncModbusTcpClient, device_id: int = 1) -> str | None:
+    try:
+        rr = await modbus.read_input_registers(address=30515, count=10, device_id=device_id)
+        if rr and not rr.isError() and not isinstance(rr, ExceptionResponse):
+            serial = modbus.convert_from_registers(rr.registers, AsyncModbusTcpClient.DATATYPE.STRING)
+            return serial
+    except ModbusException as e:
+        logging.debug(f"Failed to retrieve serial number for {modbus.comm_params.host}:{modbus.comm_params.port} device_id {device_id}: {e}")
+    except Exception as e:
+        logging.debug(f"Unexpected error when acquiring serial number for {modbus.comm_params.host}:{modbus.comm_params.port} device_id {device_id}: {e}")
+    return None
+
+
+serial_numbers = []
+
+
+async def register_probe(ip: str, port: int, results: list) -> None:
+    modbus = AsyncModbusTcpClient(host=ip, port=port, framer=FramerType.SOCKET, timeout=0.25, retries=0)
+    try:
+        await modbus.connect()
+        if modbus.connected:
+            try:
+                logging.info(f"Found Modbus device at {ip}:{port}")
+                device = {"host": ip, "port": port, "ac-chargers": [], "dc-chargers": [], "inverters": []}
+                if await probe_worker(modbus, address=30051, device_id=247):  # Plant running state
+                    logging.info(f" -> Found Sigenergy Plant at {ip}:{port}")
+                    for device_id in range(1, 247):
+                        if await probe_worker(modbus, address=31501, device_id=device_id):  # [DC Charger] Charging current
+                            serial = await get_serial_number(modbus, device_id=device_id)
+                            if serial and serial not in serial_numbers:
+                                serial_numbers.append(serial)
+                                logging.info(f" -> Found Inverter {device_id} ({serial}) and DC-Charger at {ip}:{port}: Device ID={device_id}")
+                                device["dc-chargers"].append(device_id)
+                                device["inverters"].append(device_id)
+                            else:
+                                logging.info(f" -> IGNORED Inverter {device_id} at {ip}:{port} - serial number {serial} already discovered")
+                            continue
+                        if await probe_worker(modbus, address=30578, device_id=device_id):  # Inverter Running state
+                            serial = await get_serial_number(modbus, device_id=device_id)
+                            if serial and serial not in serial_numbers:
+                                serial_numbers.append(serial)
+                                logging.info(f" -> Found Inverter {device_id} ({serial}) at {ip}:{port}: Device ID={device_id}")
+                                device["inverters"].append(device_id)
+                            else:
+                                logging.info(f" -> IGNORED Inverter {device_id} at {ip}:{port} - serial number {serial} already discovered")
+                            continue
+                        if len(device["inverters"]) > 0 and await probe_worker(modbus, address=32000, device_id=device_id):  # AC Charger System state
+                            logging.info(f" -> Found AC-Charger at {ip}:{port}: Device ID={device_id}")
+                            device["ac-chargers"].append(device_id)
+                            continue
+                    if len(device["inverters"]) == 0 and len(device["dc-chargers"]) == 0 and len(device["ac-chargers"]) == 0:
+                        logging.info(f" -> Ignored Modbus device at {ip}:{port}: No new inverters or chargers found")
+                    else:
+                        results.append(device)
+                else:
+                    logging.info(f" -> Ignored Modbus device at {ip}: No Plant running state found")
+            finally:
+                modbus.close()
+    except ModbusException as e:
+        logging.debug(f"Modbus connection to {ip}:{port} failed: {e}")
+
+
+def scan(port: int = 502) -> list[dict[str, int, list[int], list[int], list[int]]]:
+    logging.getLogger("pymodbus").setLevel(logging.CRITICAL)
+    logging.getLogger("scapy").setLevel(logging.CRITICAL)
+
+    started = time.perf_counter()
+
+    networks = {}
+    for iface_name, iface_info in psutil.net_if_addrs().items():
+        if "docker" not in iface_name and iface_name not in ("lo", "hassio"):
+            for addr in iface_info:
+                if addr.family.name == "AF_INET" and not addr.address.startswith("127."):
+                    ip = addr.address
+                    netmask = addr.netmask
+                    if ip and netmask:
+                        network = f"{ip}/{netmask}"
+                        networks[ip] = ipaddress.IPv4Network(network, strict=False)
+                        logging.info(f"Found network '{iface_name}' {networks[ip]} via {network}")
+                        break
+
+    loop = asyncio.new_event_loop()
+    results = []
+    for addr, subnet in networks.items():
+        logging.info(f"Scanning for active devices in network {subnet.with_prefixlen}...")
+
+        arp_results = arp_scan(subnet.with_netmask, timeout=0.5)
+        all_ips = [str(ip) for ip in subnet.hosts()]
+        missing_ips = [ip for ip in all_ips if ip not in arp_results and ip != addr]
+
+        ping_results = ping_scan(missing_ips, timeout=0.5)
+        active_ips = list(set([addr]) | set(arp_results.keys()) | set(ping_results))
+
+        for ip in active_ips:
+            loop.run_until_complete(register_probe(ip, port, results))
+    loop.close()
+
+    elapsed = time.perf_counter() - started
+    sorted_ips = sorted(results, key=lambda result: ipaddress.ip_address(result["host"]))
+    logging.info(f"Scan completed in {elapsed:.2f} seconds")
+
+    return sorted_ips
+
+
+if __name__ == "__main__":
+    logging.getLogger("root").setLevel(logging.INFO)
+    results = scan(port=502)
+    logging.info(f"Auto-discovered: {results}")
---
+++ packages/sigenergy2mqtt/sigenergy2mqtt-2025.9.24.tar.gz/sigenergy2mqtt-2025.9.24/sigenergy2mqtt/pvoutput/service.py
@@ -0,0 +1,281 @@
+from contextlib import asynccontextmanager
+from dataclasses import asdict, dataclass, is_dataclass
+from sigenergy2mqtt.config import Config
+from sigenergy2mqtt.devices import Device
+from sigenergy2mqtt.mqtt import MqttClient, MqttHandler
+from typing import Any
+import asyncio
+import logging
+import requests
+import time
+
+
+@dataclass
+class Topic:
+    topic: str
+    gain: float
+    state: float = 0.0
+    timestamp: time.struct_time = None
+
+    @staticmethod
+    def json_decoder(obj):
+        if "topic" in obj and "gain" in obj and "state" in obj and "timestamp" in obj:
+            topic = Topic(**obj)
+            if isinstance(topic.timestamp, list):
+                topic.timestamp = time.struct_time(topic.timestamp)
+            return topic
+        return obj
+
+    @staticmethod
+    def json_encoder(obj):
+        if is_dataclass(obj):
+            return asdict(obj)
+        raise TypeError(f"Object of type {type(obj).__name__} is not JSON serializable")
+
+
+class Service(Device):
+    def __init__(self, name: str, unique_id: str, model: str, logger: logging.Logger):
+        super().__init__(name, -1, unique_id, "sigenergy2mqtt", model)
+        self._logger = logger
+        self._lock = asyncio.Lock()
+
+    @property
+    def logger(self) -> logging.Logger:
+        return self._logger
+
+    @property
+    def request_headers(self) -> dict[str, str]:
+        return {
+            "X-Pvoutput-Apikey": Config.pvoutput.api_key,
+            "X-Pvoutput-SystemId": Config.pvoutput.system_id,
+            "X-Rate-Limit": "1",
+        }
+
+    @asynccontextmanager
+    async def lock(self, timeout=None):
+        acquired: bool = False
+        try:
+            if timeout is None:
+                acquired = await self._lock.acquire()
+            else:
+                acquired = await asyncio.wait_for(self._lock.acquire(), timeout)
+                if not acquired:
+                    raise TimeoutError("Failed to acquire lock within the timeout period.")
+            yield
+        finally:
+            if acquired and self._lock.locked():
+                self._lock.release()
+
+    # region Device overrides
+
+    def publish_availability(self, mqtt: MqttClient, ha_state, qos=2) -> None:
+        pass
+
+    def publish_discovery(self, mqtt: MqttClient, clean=False) -> Any:
+        pass
+
+    # endregion
+
+    async def upload_payload(self, url: str, payload: dict[str, any]) -> bool:
+        self.logger.info(f"{self.__class__.__name__} Uploading {payload=}")
+        uploaded = False
+        for i in range(1, 4, 1):
+            try:
+                if Config.pvoutput.testing:
+                    uploaded = True
+                    self.logger.info(f"{self.__class__.__name__} Testing mode, not sending upload to {url=}")
+                    break
+                else:
+                    self.logger.debug(f"{self.__class__.__name__} Attempt #{i} to {url=}...")
+                    with requests.post(url, headers=self.request_headers, data=payload, timeout=10) as response:
+                        limit = int(response.headers["X-Rate-Limit-Limit"])
+                        remaining = int(response.headers["X-Rate-Limit-Remaining"])
+                        at = float(response.headers["X-Rate-Limit-Reset"])
+                        reset = round(at - time.time())
+                        if response.status_code == 200:
+                            uploaded = True
+                            self.logger.debug(
+                                f"{self.__class__.__name__} Attempt #{i} OKAY status_code={response.status_code} {limit=} {remaining=} reset={time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(at))} ({reset}s)"
+                            )
+                            break
+                        else:
+                            self.logger.warning(f"{self.__class__.__name__} Attempt #{i} FAILED status_code={response.status_code} reason={response.reason}")
+                        if int(response.headers["X-Rate-Limit-Remaining"]) < 10:
+                            self.logger.warning(f"{self.__class__.__name__} Only {remaining} requests left, sleeping until {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(at))} ({reset}s)")
+                            await asyncio.sleep(reset)
+                        else:
+                            response.raise_for_status()
+                            break
+            except requests.exceptions.HTTPError as exc:
+                self.logger.error(f"{self.__class__.__name__} HTTP Error: {exc}")
+            except requests.exceptions.ConnectionError as exc:
+                self.logger.error(f"{self.__class__.__name__} Error Connecting: {exc}")
+            except requests.exceptions.Timeout as exc:
+                self.logger.error(f"{self.__class__.__name__} Timeout Error: {exc}")
+            except Exception as exc:
+                self.logger.error(f"{self.__class__.__name__} {exc}")
+            if i <= 2:
+                self.logger.info(f"{self.__class__.__name__} Retrying in 10 seconds")
+                await asyncio.sleep(10)
+        else:
+            self.logger.error(f"{self.__class__.__name__} Failed to upload to {url} after 3 attempts")
+        return uploaded
+
+
+class ServiceTopics(dict[str, Topic]):
+    def __init__(
+        self,
+        service: Service,
+        enabled: bool,
+        name: str,
+        logger: logging.Logger,
+        requires_donation: bool = False,
+        averaged: bool = True,
+        value_key: str = None,
+        datetime_key: str = None,
+        decimals: int = 1,
+        bypass_updating_check: bool = False,
+    ):
+        self._last_update_warning: float = None
+        self._service = service
+        self._enabled = enabled
+        self._name = name
+        self._logger = logger
+        self._requires_donation = requires_donation
+        self._averaged = averaged
+        self._value_key = value_key
+        self._datetime_key = datetime_key
+        self._decimals = decimals
+        self._bypass_updating_check = bypass_updating_check
+
+    @property
+    def enabled(self) -> bool:
+        return self._enabled
+
+    @enabled.setter
+    def enabled(self, value: bool) -> None:
+        assert isinstance(value, bool), "Enabled must be a boolean value"
+        self._enabled = value
+
+    @property
+    def requires_donation(self) -> bool:
+        return self._requires_donation
+
+    def aggregate(self, exclude_zero: bool) -> tuple[float, str, int]:
+        if not self.enabled:
+            return None, None, 0
+        if len(self) == 0:
+            self._logger.debug(f"{self._service.__class__.__name__} No {self._name} topics registered, skipping aggregation")
+            return None, None, 0
+        at: str = "00:00"
+        count: int = 0
+        total: float = 0.0
+        for value in self.values():
+            if value.timestamp is not None and (not exclude_zero or value.state > 0.0):
+                total += value.state * value.gain
+                at = time.strftime("%H:%M", value.timestamp)
+                count += 1
+        if self._logger.isEnabledFor(logging.DEBUG):
+            self._logger.debug(
+                f"{self._service.__class__.__name__} Aggregated {self._name}: {total=} {count=} {at=} ({[(v.state, time.strftime('%H:%M', value.timestamp) if value.timestamp else None) for v in self.values()]})"
+            )
+        if count > 0:
+            return total, at, count
+        else:
+            return None, None, count
+
+    def _average_into(self, payload: dict[str, any], value_key: str, datetime_key: str = None) -> bool:
+        total, at, count = self.aggregate(exclude_zero=True)
+        if count > 0 and total is not None:
+            value = round(total / count, self._decimals)
+            payload[value_key] = int(value) if self._decimals == 0 else value
+            if datetime_key is not None:
+                payload[datetime_key] = at
+                self._logger.debug(f"{self._service.__class__.__name__} Averaged {self._name}: {total} / {count} = {payload[value_key]} into {value_key=} ({datetime_key}={at})")
+            else:
+                self._logger.debug(f"{self._service.__class__.__name__} Averaged {self._name}: {total} / {count} = {payload[value_key]} into {value_key=}")
+            return True
+        else:
+            if value_key in payload:
+                del payload[value_key]
+            self._logger.warning(f"{self._service.__class__.__name__} Removed '{value_key}' from payload because {count=} and {total=}")
+            return False
+
+    def _sum_into(self, payload: dict[str, any], value_key: str, datetime_key: str = None) -> bool:
+        total, at, count = self.aggregate(exclude_zero=False)
+        if count > 0 and total is not None:
+            payload[value_key] = int(total) if self._decimals == 0 else round(total, self._decimals)
+            if datetime_key is not None:
+                payload[datetime_key] = at
+                self._logger.debug(f"{self._service.__class__.__name__} Summed {self._name}: {total} into {value_key=} ({datetime_key}={at})")
+            else:
+                self._logger.debug(f"{self._service.__class__.__name__} Summed {self._name}: {total} into {value_key=}")
+            return True
+        else:
+            if value_key in payload:
+                del payload[value_key]
+            self._logger.warning(f"{self._service.__class__.__name__} Removed '{value_key}' from payload because {count=} and {total=}")
+            return False
+
+    def add_to_payload(self, payload: dict[str, any], interval_minutes: int, now: time.struct_time) -> bool:
+        if self._bypass_updating_check or self.check_is_updating(interval_minutes, now):
+            if self._averaged:
+                return self._average_into(payload, self._value_key, self._datetime_key)
+            else:
+                return self._sum_into(payload, self._value_key, self._datetime_key)
+        else:
+            return False
+
+    def check_is_updating(self, interval_minutes: int, now: time.struct_time) -> bool:
+        if self.enabled:
+            interval_seconds = interval_minutes * 60
+            topics = 0
+            updated = 0
+            for value in self.values():
+                topics += 1
+                if value.timestamp is not None:
+                    seconds = int(time.mktime(now) - time.mktime(value.timestamp))
+                    minutes = int(seconds / 60.0)
+                    if seconds < interval_seconds:
+                        updated += 1
+                        self._logger.debug(f"{self._service.__class__.__name__} Topic '{value.topic}' for {self._name} last updated {seconds}s ago (interval={interval_seconds}s)")
+                    else:
+                        if self._last_update_warning is None or (time.time() - self._last_update_warning) > 3600:
+                            self._logger.warning(f"{self._service.__class__.__name__} Topic '{value.topic}' for {self._name} has not been updated for {minutes}m???")
+                            self._last_update_warning = time.time()
+                elif self._last_update_warning is None or (time.time() - self._last_update_warning) > 3600:
+                    self._logger.warning(f"{self._service.__class__.__name__} Topic '{value.topic}' for {self._name} has never been updated???")
+                    self._last_update_warning = time.time()
+            return topics == updated
+        else:
+            return False
+
+    def register(self, topic: str, gain: float) -> bool:
+        if self.enabled:
+            if topic is None or topic == "" or topic.isspace():
+                self._logger.debug(f"{self._service.__class__.__name__} Ignored subscription request for empty topic")
+            elif topic not in self:
+                self[topic] = Topic(topic, gain)
+        else:
+            self._logger.debug(f"{self._service.__class__.__name__} Ignored subscription request for '{topic}' because {self._name} uploading is disabled")
+
+    def subscribe(self, mqtt: MqttClient, mqtt_handler: MqttHandler) -> None:
+        for topic in self.keys():
+            if self.enabled:
+                result = mqtt_handler.register(mqtt, topic, self.update)
+                self._logger.debug(f"{self._service.__class__.__name__} Subscribed to topic '{topic}' to record {self._name} ({result=})")
+            else:
+                self._logger.debug(f"{self._service.__class__.__name__} Not subscribing to topic '{topic}' because {self._name} uploading is disabled")
+
+    async def update(self, modbus: Any, mqtt: MqttClient, value: float | int | str, topic: str, handler: MqttHandler) -> bool:
+        if self.enabled:
+            if Config.pvoutput.update_debug_logging:
+                self._logger.debug(f"{self._service.__class__.__name__} Updating {self._name} from '{topic}' {value=}")
+            state = value if isinstance(value, float) else float(value)
+            if state >= 0.0:
+                async with self._service.lock(timeout=1):
+                    self[topic].state = state
+                    self[topic].timestamp = time.localtime()
+            return True
+        else:
+            return False
