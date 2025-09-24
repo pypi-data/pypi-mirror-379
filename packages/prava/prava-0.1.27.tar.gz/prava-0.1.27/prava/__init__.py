b242848796685610d5f01e0206c157e875e5f931
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 03:14:54 +0000

    Add package prava/0.1.27/prava-0.1.27.tar.gz

---
+++ packages/prava/prava-0.1.27.tar.gz/prava-0.1.27/prava/__init__.py
@@ -0,0 +1,10 @@
+"""Prava Python SDK
+
+Client for Prava Control API and Tasks API.
+"""
+
+from .client import Prava, ControlClient  # noqa: F401
+
+__all__ = ["Prava", "ControlClient"]
+__version__ = "0.1.0"
+
