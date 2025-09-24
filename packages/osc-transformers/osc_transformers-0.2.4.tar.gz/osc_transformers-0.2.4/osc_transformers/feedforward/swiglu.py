a0ce304675ac80b83ef1714a61d63e8c3b150e4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 07:05:16 +0000

    Add package osc-transformers/0.2.4/osc_transformers-0.2.4.tar.gz

---
+++ packages/osc-transformers/osc_transformers-0.2.4.tar.gz/osc_transformers-0.2.4/osc_transformers/feedforward/swiglu.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from ..registry import Registry
+from .base import FeedForward
+
+
+@Registry.feedforward.register("SwiGLU")
+class SwiGLU(FeedForward):
+    def __init__(
+        self,
+        in_dim: int,
+        hidden_dim: int,
+        up_bias: bool = False,
+        gate_bias: bool = False,
+        down_bias: bool = False,
+    ):
+        super().__init__()
+        self.up_proj = nn.Linear(in_dim, hidden_dim, bias=up_bias)
+        self.gate_proj = nn.Linear(in_dim, hidden_dim, bias=gate_bias)
+        self.down_proj = nn.Linear(hidden_dim, in_dim, bias=down_bias)
+
+    @torch.compile
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x1 = self.up_proj(x)
+        x2 = self.gate_proj(x)
+        x = F.silu(x2) * x1
+        x = self.down_proj(x)
+        return x
