2ca495c4fca5b86256ff548c9dd20fd161a4c0c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 03:15:15 +0000

    Add package shudaodao/0.1.20/shudaodao-0.1.20-cp310-cp310-musllinux_1_2_x86_64.whl

---
+++ packages/shudaodao/shudaodao-0.1.20-cp310-cp310-musllinux_1_2_x86_64.whl/shudaodao_common/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# @License  ：(C)Copyright 2025, 数道智融科技
+# @Author   ：李锋
+# @Software ：PyCharm
+# @Date     ：2025/8/27 上午1:29
+# @Desc     ：
+
+from .controller_table.dept import Dept_Router
+from .controller_table.user import User_Router
+from .shared.controller.upload import Upload_Router
+from .shared.service.upload import UploadService
