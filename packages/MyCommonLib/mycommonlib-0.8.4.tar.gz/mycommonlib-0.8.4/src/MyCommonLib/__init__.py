f8895813af55655acb3fab619892fc7b705ac27c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 17:05:04 +0000

    Add package MyCommonLib/0.8.4/mycommonlib-0.8.4.tar.gz

---
+++ packages/MyCommonLib/mycommonlib-0.8.4.tar.gz/mycommonlib-0.8.4/src/MyCommonLib/__init__.py
@@ -0,0 +1,7 @@
+from MyCommonLib.configuration import Configure, read_yaml, write_yaml
+from MyCommonLib.constants import *
+from semantic_version_tools import Vers
+from MyCommonLib.dictman import dict2Table
+from MyCommonLib.string_manipulator import snake_case, camel_case, sentence_case
+
+__version__ = Vers(VERSION)
