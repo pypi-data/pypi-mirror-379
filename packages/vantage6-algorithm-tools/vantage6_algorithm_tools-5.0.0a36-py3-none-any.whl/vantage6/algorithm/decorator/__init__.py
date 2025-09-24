664abb230eee86797385214305e23b2dd44c39de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 03:14:53 +0000

    Add package vantage6-algorithm-tools/5.0.0a36/vantage6_algorithm_tools-5.0.0a36-py3-none-any.whl

---
+++ packages/vantage6-algorithm-tools/vantage6_algorithm_tools-5.0.0a36-py3-none-any.whl/vantage6/algorithm/decorator/__init__.py
@@ -0,0 +1,10 @@
+# ruff: noqa: F401
+from vantage6.algorithm.decorator.action import (
+    central,
+    data_extraction,
+    federated,
+    preprocessing,
+)
+from vantage6.algorithm.decorator.algorithm_client import algorithm_client
+from vantage6.algorithm.decorator.data import dataframe, dataframes
+from vantage6.algorithm.decorator.metadata import metadata
