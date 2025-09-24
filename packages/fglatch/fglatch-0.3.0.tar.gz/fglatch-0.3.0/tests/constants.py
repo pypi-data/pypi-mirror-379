fc5e3d0af2cb21b94a3e268ba4aa0f99851cb209
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 05:04:47 +0000

    Add package fglatch/0.3.0/fglatch-0.3.0.tar.gz

---
+++ packages/fglatch/fglatch-0.3.0.tar.gz/fglatch-0.3.0/tests/constants.py
@@ -0,0 +1,7 @@
+"""Constants used by the test suite."""
+
+MOCK_TABLE_1_ID: str = "11730"
+"""Identifier for mock-table-1."""
+
+MOCK_RECORD_1_ID: str = "1128032177"
+"""Identifier for mock_record_1 in mock-table-1."""
