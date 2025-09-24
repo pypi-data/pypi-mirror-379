e44d11e888a214af8a18269f56cc93230ee87727
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 06:03:47 +0000

    Add package netbox-routing-complex/0.2.2/netbox_routing_complex-0.2.2-py3-none-any.whl

---
+++ packages/netbox-routing-complex/netbox_routing_complex-0.2.2-py3-none-any.whl/netbox_routing_complex/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from .constants import APP_LABEL
+from . import models, views
+
+print("Attempting import of url patterns")
+urlpatterns = [
+    path('bfd-configs/',                 views.BFDConfigListView.as_view(),   name='bfdconfig_list'),
+    path('bfd-configs/add/',             views.BFDConfigEditView.as_view(),   name='bfdconfig_add'),
+    path('bfd-configs/<int:pk>/',        views.BFDConfigView.as_view(),       name='bfdconfig'),
+    path('bfd-configs/<int:pk>/edit/',   views.BFDConfigEditView.as_view(),   name='bfdconfig_edit'),
+    path('bfd-configs/<int:pk>/delete/', views.BFDConfigDeleteView.as_view(), name='bfdconfig_delete'),
+]
+print(urlpatterns)
+
+#this is where we link views to URLs
+#<int:pk> is a path converter which allows the user to enter an integer to get a specific object from the database by its id
+
+#from netbox.views.generic import ObjectChangeLogView
+
+# #changelog endpoint
+# path('bfd-configs/<int:pk>/changelog/', ObjectChangeLogView.as_view(), name='bfdconfig_changelog', kwargs={
+#     'model': models.BFDConfig
+# }),
