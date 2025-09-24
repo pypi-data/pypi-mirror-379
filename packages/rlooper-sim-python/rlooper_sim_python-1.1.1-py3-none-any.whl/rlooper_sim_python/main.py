09e40cacef0ac52e9c25281fad9c8eac9ec746de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 18:05:24 +0000

    Add package rlooper-sim-python/1.1.1/rlooper_sim_python-1.1.1-py3-none-any.whl

---
+++ packages/rlooper-sim-python/rlooper_sim_python-1.1.1-py3-none-any.whl/rlooper_sim_python/main.py
@@ -0,0 +1,50 @@
+import argparse
+import os
+import sys
+import logging
+import pandas as pd
+from . import simulation
+
+currentDir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(currentDir)
+
+def parseArgv():
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+
+    parser = argparse.ArgumentParser(description='R-loop Peak Simulator')
+    parser.add_argument('-i','--fasta', type=str, help='Path to the FASTA file')
+    parser.add_argument('-s','--sigma', type=float, help='sigma value [0.07]')
+    parser.add_argument('-a','--a', type=float, help='a value [10]')
+    args = parser.parse_args()
+
+    if args.fasta:
+        fastaFile = args.fasta
+        logger.info(f' fastaFile: {fastaFile}')
+    if args.sigma:
+        sigma = args.sigma
+        logger.info(f' sigma: {sigma}')
+    if args.a:
+        a = args.a
+        logger.info(f' a: {a}')
+    mysim = simulation.simulation_params()
+    mysim.setFastaFile(args.fasta)
+    mysim.setSigma(args.sigma)
+    mysim.seta(args.a)
+
+    return(mysim)
+
+
+def main():
+    mysim = parseArgv()
+    myres = simulation.simulation_main(mysim)
+    print(myres)
+
+main()
+
+
+
+
+
+
+
