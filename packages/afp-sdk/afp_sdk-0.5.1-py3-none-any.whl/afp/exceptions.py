3ba2315493595050642f12ed9e1fb2a0711e105e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 03:14:40 +0000

    Add package afp-sdk/0.5.1/afp_sdk-0.5.1-py3-none-any.whl

---
+++ packages/afp-sdk/afp_sdk-0.5.1-py3-none-any.whl/afp/exceptions.py
@@ -0,0 +1,30 @@
+class AFPException(Exception):
+    pass
+
+
+class ConfigurationError(AFPException):
+    pass
+
+
+class ClearingSystemError(AFPException):
+    pass
+
+
+class ExchangeError(AFPException):
+    pass
+
+
+class AuthenticationError(ExchangeError):
+    pass
+
+
+class AuthorizationError(ExchangeError):
+    pass
+
+
+class NotFoundError(ExchangeError):
+    pass
+
+
+class ValidationError(ExchangeError):
+    pass
