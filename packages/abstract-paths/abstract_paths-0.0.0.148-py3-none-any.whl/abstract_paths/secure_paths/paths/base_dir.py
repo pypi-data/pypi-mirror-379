371038b6824abc3c144a7d5a3c3e5760d8455832
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 18:05:56 +0000

    Add package abstract-paths/0.0.0.148/abstract_paths-0.0.0.148-py3-none-any.whl

---
+++ packages/abstract-paths/abstract_paths-0.0.0.148-py3-none-any.whl/abstract_paths/secure_paths/paths/base_dir.py
@@ -0,0 +1,8 @@
+import os
+WWW_DIR = '/var/www'
+MEDIA_DIR = f"{WWW_DIR}/media"
+ABS_UPLOAD_DIR = f"{MEDIA_DIR}/users"
+get_base_path
+def get_base_path(path):
+    base_dir = os.path.join(MEDIA_DIR, path)
+    return base_dir
