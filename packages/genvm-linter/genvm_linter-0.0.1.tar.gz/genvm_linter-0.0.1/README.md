93199e0c0bf2c4fbb31cfbe0c687276181daa99d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 18:05:25 +0000

    Add package genvm-linter/0.0.1/genvm_linter-0.0.1.tar.gz

---
+++ packages/genvm-linter/genvm_linter-0.0.1.tar.gz/genvm_linter-0.0.1/README.md
@@ -0,0 +1,5 @@
+# genvm-linter
+
+GenVM Linter - Implementation coming soon.
+
+This is a placeholder package. The actual linter implementation will be published shortly.
---
+++ packages/genvm-linter/genvm_linter-0.0.1.tar.gz/genvm_linter-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="genvm-linter",
+    version="0.0.1",
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="GenVM Linter - placeholder package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/genvm-linter",  # Update this
+    packages=find_packages(),
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+    ],
+)
