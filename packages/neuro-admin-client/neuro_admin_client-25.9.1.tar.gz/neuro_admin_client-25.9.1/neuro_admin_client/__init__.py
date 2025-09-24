9a8631ee95078fedf2e4c1c1511386694f7e5700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 03:15:01 +0000

    Add package neuro-admin-client/25.9.1/neuro_admin_client-25.9.1.tar.gz

---
+++ packages/neuro-admin-client/neuro_admin_client-25.9.1.tar.gz/neuro_admin_client-25.9.1/neuro_admin_client/__init__.py
@@ -0,0 +1,24 @@
+from . import entities
+from .admin_client import AdminClient, AdminClientDummy, GetUserResponse
+from .auth_client import AuthClient
+from .bearer_auth import BearerAuth
+from .entities import *  # noqa: F403
+from .security import (
+    check_permissions,
+    get_job_id_from_identity,
+    get_kind,
+    get_untrusted_user_name,
+)
+
+__all__ = [
+    "AuthClient",
+    "check_permissions",
+    "AdminClient",
+    "AdminClientDummy",
+    "GetUserResponse",
+    "BearerAuth",
+    "get_untrusted_user_name",
+    "get_job_id_from_identity",
+    "get_kind",
+    *entities.__all__,
+]
