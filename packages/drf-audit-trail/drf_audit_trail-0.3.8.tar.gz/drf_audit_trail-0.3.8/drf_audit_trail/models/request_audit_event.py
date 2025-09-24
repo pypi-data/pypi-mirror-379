1f815d61dc1042a8a1d0c769ce8503c7754b2c0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 03:15:17 +0000

    Add package drf-audit-trail/0.3.8/drf_audit_trail-0.3.8.tar.gz

---
+++ packages/drf-audit-trail/drf_audit_trail-0.3.8.tar.gz/drf_audit_trail-0.3.8/drf_audit_trail/models/request_audit_event.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from drf_audit_trail.managers import RequestAuditEventManager
+from drf_audit_trail.mixins import BaseModelMixin
+
+
+class RequestAuditEvent(BaseModelMixin):
+    """
+    This is the model for the request audit trail.
+    """
+
+    user = models.CharField(_("User identifier"), null=True, blank=True, max_length=120)
+
+    ip_addresses = models.CharField(_("Ip addresses"), max_length=50, null=True)
+
+    """
+        methods: GET, POST, PUT, DELETE, PATCH, OPTIONS
+    """
+    method = models.CharField(_("Method"), max_length=10)
+    url = models.CharField(_("URL"), null=False, max_length=2048)
+
+    # Request Parameters
+    query_params = models.CharField(
+        _("Query Parameters"), blank=True, null=True, max_length=2048
+    )
+    request_type = models.CharField(
+        _("Request Type"), max_length=10, null=True, blank=True
+    )
+    request_body = models.TextField(_("Request Body"), blank=True, null=True)
+    response_body = models.TextField(_("Response Body"), blank=True, null=True)
+
+    # Response Information
+    status_code = models.IntegerField(_("Status Code"), null=True)
+    response_time = models.FloatField(_("Response Time (ms)"), null=True)
+    response_size = models.IntegerField(_("Response Size (bytes)"), null=True)
+
+    # Error Handling
+    error_type = models.CharField(
+        _("Error Type"), max_length=255, blank=True, null=True
+    )
+    error_message = models.TextField(_("Error Message"), blank=True, null=True)
+    error_stacktrace = models.TextField(_("Error Stacktrace"), blank=True, null=True)
+
+    objects: RequestAuditEventManager = RequestAuditEventManager()
+
+    def __str__(self) -> str:
+        return "RequestAuditEvent: %s" % self.pk
+
+    class Meta:
+        verbose_name = _("Request event")
+        verbose_name_plural = _("Request events")
