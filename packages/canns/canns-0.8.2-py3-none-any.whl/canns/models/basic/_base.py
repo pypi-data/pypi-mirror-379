a85d74fe9938ddc311d8341c9e4e0577247277fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 03:15:26 +0000

    Add package canns/0.8.2/canns-0.8.2-py3-none-any.whl

---
+++ packages/canns/canns-0.8.2-py3-none-any.whl/canns/models/basic/_base.py
@@ -0,0 +1,17 @@
+import brainstate
+
+
+class BasicModel(brainstate.nn.Dynamics):
+    """
+    Base class for basic models.
+    """
+
+    pass
+
+
+class BasicModelGroup(brainstate.nn.DynamicsGroup):
+    """
+    Base class for groups of basic models.
+    """
+
+    pass
