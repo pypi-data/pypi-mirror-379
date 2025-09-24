046b710ac1c1b19c60b925346ae0653c0f7121f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 03:14:54 +0000

    Add package deltalake2db/1.0.7/deltalake2db-1.0.7-py3-none-any.whl

---
+++ packages/deltalake2db/deltalake2db-1.0.7-py3-none-any.whl/deltalake2db/__init__.py
@@ -0,0 +1,21 @@
+from .duckdb import get_sql_for_delta
+from .duckdb import get_sql_for_delta_expr
+from .duckdb import create_view_for_delta as duckdb_create_view_for_delta
+from .duckdb import apply_storage_options as duckdb_apply_storage_options
+from .polars import (
+    scan_delta_union as polars_scan_delta,
+    get_polars_schema,
+    PolarsSettings,
+)
+from .protocol_check import is_protocol_supported
+
+__all__ = [
+    "get_sql_for_delta",
+    "get_sql_for_delta_expr",
+    "duckdb_create_view_for_delta",
+    "duckdb_apply_storage_options",
+    "polars_scan_delta",
+    "get_polars_schema",
+    "PolarsSettings",
+    "is_protocol_supported",
+]
