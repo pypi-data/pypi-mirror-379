5a921bcc250d203335fe3bac9a61823ce1f9979e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 05:04:48 +0000

    Add package qSide/2025.9.23/qside-2025.9.23-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/qSide/qside-2025.9.23-cp311-cp311-macosx_11_0_arm64.whl/qSide/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2018-2025 Connet Information Technology Company, Shanghai.
+
+__version__ = "2025.09.23"
+
+
+from .action import QAction
+from .label import QElideLabel
+from .layout import QGrid, QForm, QStacked, QHBox, QVBox
+from .pushbutton import QPushButton
+from .seperator import QHLine, QVLine
