6cacaa1387f768d66711ea6997a13c3830ffdbf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 05:04:08 +0000

    Add package nfca/0.1.0/nfca-0.1.0-py3-none-any.whl

---
+++ packages/nfca/nfca-0.1.0-py3-none-any.whl/nfca/main.py
@@ -0,0 +1,11 @@
+from rich import print
+import sys
+import torch
+
+def main():
+    print("Hello from codespacetest!")
+    print(sys.version)
+    print(torch.__version__)
+
+if __name__ == "__main__":
+    main()
