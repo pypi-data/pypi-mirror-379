c590460bc85cdd2259bab63d410a77759819ab34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 03:14:33 +0000

    Add package oarepo-runtime/2.0.0.dev22/oarepo_runtime-2.0.0.dev22.tar.gz

---
+++ packages/oarepo-runtime/oarepo_runtime-2.0.0.dev22.tar.gz/oarepo_runtime-2.0.0.dev22/oarepo_runtime/proxies.py
@@ -0,0 +1,25 @@
+#
+# Copyright (c) 2025 CESNET z.s.p.o.
+#
+# This file is a part of oarepo-runtime (see http://github.com/oarepo/oarepo-runtime).
+#
+# oarepo-runtime is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+
+"""Proxies."""
+
+from __future__ import annotations
+
+from contextvars import ContextVar
+from typing import TYPE_CHECKING
+
+from flask import current_app
+from werkzeug.local import LocalProxy
+
+if TYPE_CHECKING:
+    from oarepo_runtime.ext import OARepoRuntime
+
+current_runtime: LocalProxy[OARepoRuntime] = LocalProxy(lambda: current_app.extensions["oarepo-runtime"])  # type: ignore[assignment]
+
+current_timezone: ContextVar = ContextVar("timezone")  # idk how or exactly why to use the LocalProxy here
