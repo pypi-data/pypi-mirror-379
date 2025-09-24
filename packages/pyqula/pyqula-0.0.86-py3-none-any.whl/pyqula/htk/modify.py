9cf6f5b46f9bdba663ace99c6bc5d143b274f5f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 05:05:41 +0000

    Add package pyqula/0.0.86/pyqula-0.0.86-py3-none-any.whl

---
+++ packages/pyqula/pyqula-0.0.86-py3-none-any.whl/pyqula/htk/modify.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def modify_hamiltonian_matrices(self,f0,use_geometry=False):
+    """Apply a certain function to all the matrices"""
+    # wrapper function
+    if use_geometry: # use geometry
+        g = self.geometry # get the geometry
+        def f(m,dr): 
+            return f0(m,g.r,g.replicas(d=-np.array(dr))) # use r1 and r2
+    else:
+        def f(m,dr): return f0(m) # do not use geometry
+    # apply the function
+    self.intra = f(self.intra,[0,0,0]) # modify intracell
+    if self.dimensionality==0: return  # zero dimensional systems
+    self.turn_multicell() # multicell for all
+    if self.is_multicell: # for multicell hamiltonians
+      for i in range(len(self.hopping)): # loop over hoppings
+        # modify Hamiltonian matrix
+        self.hopping[i].m = f(self.hopping[i].m,self.hopping[i].dir) 
+    else: # conventional way, now disabled
+      raise
