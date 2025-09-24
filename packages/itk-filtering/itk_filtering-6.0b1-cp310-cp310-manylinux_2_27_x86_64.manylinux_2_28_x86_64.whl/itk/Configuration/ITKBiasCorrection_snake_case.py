064bf40279b27808eea2405bc16b8533318ec899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 18:05:51 +0000

    Add package itk-filtering/6.0b1/itk_filtering-6.0b1-cp310-cp310-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/itk-filtering/itk_filtering-6.0b1-cp310-cp310-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/itk/Configuration/ITKBiasCorrection_snake_case.py
@@ -0,0 +1,1 @@
+snake_case_functions = ('image_to_image_filter', 'n4_bias_field_correction_image_filter', )
---
+++ packages/itk-filtering/itk_filtering-6.0b1-cp310-cp310-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/itk/Configuration/ITKImageFrequency_snake_case.py
@@ -0,0 +1,1 @@
+snake_case_functions = ('frequency_band_image_filter', 'unary_frequency_domain_filter', )
