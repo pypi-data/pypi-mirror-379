6cd3bea9fff656ddc3f858aaac20a32bb0c13453
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 06:04:03 +0000

    Add package inventree-bulk-plugin/1.4.3/inventree_bulk_plugin-1.4.3.tar.gz

---
+++ packages/inventree-bulk-plugin/inventree_bulk_plugin-1.4.3.tar.gz/inventree_bulk_plugin-1.4.3/inventree_bulk_plugin/core.py
@@ -0,0 +1,110 @@
+import json
+import os
+from dataclasses import dataclass
+
+from django.core.exceptions import ValidationError
+from rest_framework.request import Request
+
+from plugin import InvenTreePlugin
+from plugin.base.ui.mixins import UIFeature
+from plugin.mixins import UserInterfaceMixin, UrlsMixin, AppMixin, SettingsMixin
+
+from .api import api_urls
+from . import PLUGIN_VERSION
+
+
+@dataclass
+class Panel:
+    title: str
+    model: str
+    icon: str
+    args: dict
+    key: str
+
+
+def validate_json(value):
+    try:
+        json.loads(value)
+    except Exception as e:
+        raise ValidationError(str(e))
+
+
+class InvenTreeBulkPlugin(
+    AppMixin, UserInterfaceMixin, UrlsMixin, SettingsMixin, InvenTreePlugin
+):
+    AUTHOR = "wolflu05"
+    DESCRIPTION = "InvenTree Bulk plugin"
+    VERSION = PLUGIN_VERSION
+
+    # 0.9.1 - due to "invoke update" doesn't run collectstatic (see inventree/InvenTree#4077)
+    # 0.12.6 - Settings do not work in combination with api views (see inventree/InvenTree#5408)
+    # 0.12.7 - Fix missing filters for get settings validator (see inventree/InvenTree#5480)
+    # 0.15.8 - plugin_static template tag was added (see inventree/InvenTree#7764)
+    # 0.18.0 - Migrate to PUI
+    MIN_VERSION = "0.18.0"
+
+    TITLE = "InvenTree Bulk Plugin"
+    SLUG = "inventree-bulk-plugin"
+    NAME = "InvenTreeBulkPlugin"
+
+    SETTINGS = {
+        "DEFAULT_DOWNLOAD_HEADERS": {
+            "name": "Default download headers",
+            "description": "Set default download headers that should be used each time in json format",
+            "default": "{}",
+            "validator": validate_json,
+        }
+    }
+
+    PREACT_PANELS: list[Panel] = [
+        Panel(
+            "Location bulk creation",
+            model="stocklocation",
+            icon="ti:tools:outline",
+            args={"objectType": "STOCK_LOCATION"},
+            key="bulk-creation-location",
+        ),
+        Panel(
+            "Category bulk creation",
+            model="partcategory",
+            icon="ti:tools:outline",
+            args={"objectType": "PART_CATEGORY"},
+            key="bulk-creation-category",
+        ),
+        Panel(
+            "Part bulk creation",
+            model="partcategory",
+            icon="ti:tools:outline",
+            args={"objectType": "PART"},
+            key="bulk-creation-part",
+        ),
+    ]
+
+    def get_ui_panels(
+        self, request: Request, context: dict, **kwargs
+    ) -> list[UIFeature]:
+        panels = []
+
+        target_model = context.get("target_model", None)
+        target_id = context.get("target_id", None)
+
+        for panel in self.PREACT_PANELS:
+            if target_model == panel.model and target_id is not None:
+                panels.append({
+                    "key": panel.key,
+                    "title": panel.title,
+                    "source": self.plugin_static_file(
+                        "bulk-creation-panel.dev.js:renderPanel"
+                        if os.environ.get("INVENTREE_BULK_DEV", False)
+                        else "dist/bulk-creation-panel.js:renderPanel"
+                    ),
+                    "icon": panel.icon,
+                    "context": {
+                        "args": panel.args,
+                    },
+                })
+
+        return panels
+
+    def setup_urls(self):
+        return api_urls
---
+++ packages/inventree-bulk-plugin/inventree_bulk_plugin-1.4.3.tar.gz/inventree_bulk_plugin-1.4.3/inventree_bulk_plugin/tests/integration/test_InvenTreeBulkPlugin.py
@@ -0,0 +1,71 @@
+import json
+
+from django.test import TestCase
+from django.core.exceptions import ValidationError
+
+from plugin import registry
+
+from ...models import validate_template
+from ...core import InvenTreeBulkPlugin, validate_json
+from ... import PLUGIN_VERSION
+
+
+class InvenTreeBulkPluginModelTestCase(TestCase):
+    def test_validate_template(self):
+        with self.assertRaisesRegex(ValidationError, "validation errors for BulkDefinitionSchema"):
+            validate_template("{}")
+
+        with self.assertRaisesRegex(ValidationError, "template is no valid json format"):
+            validate_template("no json structure")
+
+        with self.assertRaisesRegex(ValueError, f"The server runs on v{PLUGIN_VERSION} which is incompatible to v999.9.9."):
+            schema = json.dumps({
+                "version": "999.9.9",
+                "input": {},
+                "templates": [],
+                "output": {}
+            })
+            validate_template(schema)
+
+        valid_schema = json.dumps({
+            "version": "1.0.0",
+            "input": {},
+            "templates": [],
+            "output": {
+                "generate": {
+                    "name": "Test",
+                    "description": "Test description"
+                },
+            }
+        })
+        self.assertJSONEqual(valid_schema, validate_template(valid_schema))
+
+
+class InvenTreeBulkPluginTestCase(TestCase):
+    def test_get_custom_panels(self):
+        bulk_plugin: InvenTreeBulkPlugin = registry.get_plugin("inventree-bulk-plugin")
+
+        def assert_contains_by_title(title, panels):
+            found = None
+            for panel in panels:
+                if panel["title"] == title:
+                    found: dict = panel
+            self.assertIsNotNone(found)
+
+        panels = bulk_plugin.get_ui_panels(None, {"target_model": "stocklocation", "target_id": 1})
+        assert_contains_by_title("Location bulk creation", panels)
+
+        panels = bulk_plugin.get_ui_panels(None, {"target_model": "partcategory", "target_id": 1})
+        assert_contains_by_title("Category bulk creation", panels)
+        assert_contains_by_title("Part bulk creation", panels)
+
+    def test_validate_json(self):
+        valid_json = '{"ts": 13}'
+        not_valid_json = '{"ts""13"}'
+
+        # no error, should pass validator
+        validate_json(valid_json)
+
+        # should throw an error
+        with self.assertRaises(ValidationError):
+            validate_json(not_valid_json)
