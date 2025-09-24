5c6cb56e95737f28f0afd74aae0dbf1033f12514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 03:14:47 +0000

    Add package pydracor-base/1.0.1/pydracor_base-1.0.1.tar.gz

---
+++ packages/pydracor-base/pydracor_base-1.0.1.tar.gz/pydracor_base-1.0.1/pydracor_base/api/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+
+# import apis into api package
+from pydracor_base.api.dts_api import DTSApi
+from pydracor_base.api.admin_api import AdminApi
+from pydracor_base.api.public_api import PublicApi
+from pydracor_base.api.webhook_api import WebhookApi
+from pydracor_base.api.wikidata_api import WikidataApi
+
