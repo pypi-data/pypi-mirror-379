92803fd8f1ea327bbbd98f0505de49dac2d63649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 18:06:02 +0000

    Add package itk-filtering/6.0b1/itk_filtering-6.0b1-cp39-cp39-macosx_15_0_arm64.whl

---
+++ packages/itk-filtering/itk_filtering-6.0b1-cp39-cp39-macosx_15_0_arm64.whl/itk/Configuration/ITKImageFrequencyConfig.py
@@ -0,0 +1,15 @@
+depends = ('ITKPyBase', 'ITKCommon', )
+templates = (  ('FrequencyBandImageFilter', 'itk::FrequencyBandImageFilter', 'itkFrequencyBandImageFilterICD2', True, 'itk::Image< std::complex< double >,2 >'),
+  ('FrequencyBandImageFilter', 'itk::FrequencyBandImageFilter', 'itkFrequencyBandImageFilterICF2', True, 'itk::Image< std::complex< float >,2 >'),
+  ('FrequencyBandImageFilter', 'itk::FrequencyBandImageFilter', 'itkFrequencyBandImageFilterICD3', True, 'itk::Image< std::complex< double >,3 >'),
+  ('FrequencyBandImageFilter', 'itk::FrequencyBandImageFilter', 'itkFrequencyBandImageFilterICF3', True, 'itk::Image< std::complex< float >,3 >'),
+  ('FrequencyBandImageFilter', 'itk::FrequencyBandImageFilter', 'itkFrequencyBandImageFilterICD4', True, 'itk::Image< std::complex< double >,4 >'),
+  ('FrequencyBandImageFilter', 'itk::FrequencyBandImageFilter', 'itkFrequencyBandImageFilterICF4', True, 'itk::Image< std::complex< float >,4 >'),
+  ('UnaryFrequencyDomainFilter', 'itk::UnaryFrequencyDomainFilter', 'itkUnaryFrequencyDomainFilterICD2', True, 'itk::Image< std::complex< double >,2 >'),
+  ('UnaryFrequencyDomainFilter', 'itk::UnaryFrequencyDomainFilter', 'itkUnaryFrequencyDomainFilterICF2', True, 'itk::Image< std::complex< float >,2 >'),
+  ('UnaryFrequencyDomainFilter', 'itk::UnaryFrequencyDomainFilter', 'itkUnaryFrequencyDomainFilterICD3', True, 'itk::Image< std::complex< double >,3 >'),
+  ('UnaryFrequencyDomainFilter', 'itk::UnaryFrequencyDomainFilter', 'itkUnaryFrequencyDomainFilterICF3', True, 'itk::Image< std::complex< float >,3 >'),
+  ('UnaryFrequencyDomainFilter', 'itk::UnaryFrequencyDomainFilter', 'itkUnaryFrequencyDomainFilterICD4', True, 'itk::Image< std::complex< double >,4 >'),
+  ('UnaryFrequencyDomainFilter', 'itk::UnaryFrequencyDomainFilter', 'itkUnaryFrequencyDomainFilterICF4', True, 'itk::Image< std::complex< float >,4 >'),
+)
+factories = ()
