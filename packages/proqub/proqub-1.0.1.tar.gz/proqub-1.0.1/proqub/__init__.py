331c33617d507173d760400a0ef89f35e87b81a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 04:05:03 +0000

    Add package proqub/1.0.1/proqub-1.0.1.tar.gz

---
+++ packages/proqub/proqub-1.0.1.tar.gz/proqub-1.0.1/proqub/__init__.py
@@ -0,0 +1,10 @@
+"""
+ProQub: A Hyperspectral Cube Processing Library.
+
+This package provides tools for common hyperspectral processing tasks
+like radiance-to-reflectance conversion and destriping in a memory-efficient pipeline.
+"""
+
+from .processor import CubeProcessor
+
+__version__ = "1.0.1"
