e498228adeb440946690f7998cd8946ea68ed427
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 15:04:41 +0000

    Add package rlooper-sim-python/1.0.2/rlooper_sim_python-1.0.2-py3-none-any.whl

---
+++ packages/rlooper-sim-python/rlooper_sim_python-1.0.2-py3-none-any.whl/rlooper_sim_python/__init__.py
@@ -0,0 +1,8 @@
+"""
+Rlooper with R-loop Peak Simulator (Python Implementation)
+
+"""
+
+from .version import __version__, __author__, __description__
+
+__all__ = ['__version__', '__author__', '__description__']
