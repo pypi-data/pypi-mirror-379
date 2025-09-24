a93ad2541cfa4a093c2a3caf4ea2b9fe7fcdf040
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 07:04:08 +0000

    Add package ol-themed-jupyter/0.1.0/ol_themed_jupyter-0.1.0.tar.gz

---
+++ packages/ol-themed-jupyter/ol_themed_jupyter-0.1.0.tar.gz/ol_themed_jupyter-0.1.0/src/index.ts
@@ -0,0 +1,45 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+import { LabIcon } from '@jupyterlab/ui-components';
+import { Widget } from '@lumino/widgets';
+import { PageConfig } from '@jupyterlab/coreutils';
+// @ts-expect-error: Suppressing extraneous error
+import mitJupyterSvgStr from '../style/logo.svg';
+
+export const mitJupyterIcon = new LabIcon({
+  name: 'ui-components:mit-jupyter',
+  svgstr: mitJupyterSvgStr
+});
+/**
+ * Initialization data for the ol-themed-jupyter extension.
+ */
+
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'ol-themed-jupyter:plugin',
+  description: 'Provides MIT OpenLearning themes for Jupyter',
+  autoStart: true,
+  activate: (app: JupyterFrontEnd) => {
+    const baseUrl = PageConfig.getBaseUrl();
+    const node = document.createElement('a');
+    node.href = `${baseUrl}tree`;
+    node.target = '_blank';
+    node.rel = 'noopener noreferrer';
+    const logo = new Widget({ node });
+
+    mitJupyterIcon.element({
+      container: node,
+      elementPosition: 'center',
+      padding: '2px 2px 2px 8px',
+      height: '28px',
+      width: 'auto',
+      cursor: 'pointer',
+      margin: 'auto'
+    });
+    logo.id = 'jp-NotebookLogo';
+    app.shell.add(logo, 'top', { rank: 0 });
+  }
+};
+
+export default plugin;
