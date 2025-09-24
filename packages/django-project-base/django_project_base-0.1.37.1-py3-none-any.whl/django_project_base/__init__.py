6cebafe643a4b14f98d19c781c95e7337cbe585d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 03:14:55 +0000

    Add package django-project-base/0.1.37.1/django_project_base-0.1.37.1-py3-none-any.whl

---
+++ packages/django-project-base/django_project_base-0.1.37.1-py3-none-any.whl/django_project_base/__init__.py
@@ -0,0 +1,6 @@
+__title__ = "django project base"
+# __version__ = "{version-string}"  #TODO: Switch back to version string when merging to main
+__version__ = "0.1.37.1"
+default_app_config = "django_project_base.apps.DjangoProjectBaseConfig"
+
+VERSION = __version__
