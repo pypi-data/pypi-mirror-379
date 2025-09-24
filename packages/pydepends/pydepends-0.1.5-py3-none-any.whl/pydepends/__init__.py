a58f8158695bcbc3db58f257d481a1c3a9e7f14c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 07:05:20 +0000

    Add package pydepends/0.1.5/pydepends-0.1.5-py3-none-any.whl

---
+++ packages/pydepends/pydepends-0.1.5-py3-none-any.whl/pydepends/__init__.py
@@ -0,0 +1,4 @@
+from pydepends.depends import Provider as Provider
+from pydepends.depends import inject as inject
+from pydepends.depends import Depends as Depends
+from pydepends.depends import Dependency as Dependency
