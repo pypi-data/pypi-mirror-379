aa8e3e15298e5ed37a65a927907e14ac1d4d3dff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 18:05:36 +0000

    Add package maleo-metadata/0.2.77/maleo_metadata-0.2.77-py3-none-any.whl

---
+++ packages/maleo-metadata/maleo_metadata-0.2.77-py3-none-any.whl/maleo/metadata/schemas/medical_role.py
@@ -0,0 +1,54 @@
+from maleo.mixins.general import Codes
+from maleo.mixins.hierarchy import IsRoot, IsParent, IsChild, IsLeaf
+from maleo.mixins.identity import (
+    Ids,
+    UUIDs,
+    ParentIds,
+    Keys,
+    Names,
+)
+from maleo.schemas.request import (
+    ReadSingleQuery as BaseReadSingleQuery,
+    ReadPaginatedMultipleQuery,
+)
+from maleo.types.integer import OptionalListOfIntegers
+from maleo.types.string import OptionalListOfStrings
+from maleo.types.uuid import OptionalListOfUUIDs
+from ..mixins.medical_role import Granularity
+
+
+class CommonQuery(Granularity):
+    pass
+
+
+class ReadSingleQuery(CommonQuery, BaseReadSingleQuery):
+    pass
+
+
+class ReadMultipleSpecializationsQuery(
+    CommonQuery,
+    ReadPaginatedMultipleQuery,
+    Names[OptionalListOfStrings],
+    Keys[OptionalListOfStrings],
+    Codes[OptionalListOfStrings],
+    UUIDs[OptionalListOfUUIDs],
+    Ids[OptionalListOfIntegers],
+):
+    pass
+
+
+class ReadMultipleQuery(
+    CommonQuery,
+    ReadPaginatedMultipleQuery,
+    Names[OptionalListOfStrings],
+    Keys[OptionalListOfStrings],
+    Codes[OptionalListOfStrings],
+    IsLeaf,
+    IsChild,
+    IsParent,
+    IsRoot,
+    ParentIds[OptionalListOfIntegers],
+    UUIDs[OptionalListOfUUIDs],
+    Ids[OptionalListOfIntegers],
+):
+    pass
