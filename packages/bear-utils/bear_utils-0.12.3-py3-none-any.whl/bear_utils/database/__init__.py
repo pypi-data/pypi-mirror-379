837e89fd1e251fc36e0543db32bf2150d09ede93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 16:06:40 +0000

    Add package bear-utils/0.12.3/bear_utils-0.12.3-py3-none-any.whl

---
+++ packages/bear-utils/bear_utils-0.12.3-py3-none-any.whl/bear_utils/database/__init__.py
@@ -0,0 +1,27 @@
+"""Database Manager Module for managing database connections and operations."""
+
+from ._db_config import (
+    DatabaseConfig,
+    get_default_config,
+    mysql_default_db,
+    postgres_default_db,
+    sqlite_default_db,
+    sqlite_memory_db,
+)
+from ._db_manager import DatabaseManager, MySQLDB, PostgresDB, SingletonDB, SqliteDB
+from ._schemas import Schemas
+
+__all__ = [
+    "DatabaseConfig",
+    "DatabaseManager",
+    "MySQLDB",
+    "PostgresDB",
+    "Schemas",
+    "SingletonDB",
+    "SqliteDB",
+    "get_default_config",
+    "mysql_default_db",
+    "postgres_default_db",
+    "sqlite_default_db",
+    "sqlite_memory_db",
+]
