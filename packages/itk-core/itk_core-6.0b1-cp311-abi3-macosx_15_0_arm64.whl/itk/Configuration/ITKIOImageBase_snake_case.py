eb3407a0442120c61755b9235fc645f28dd14598
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 18:05:27 +0000

    Add package itk-core/6.0b1/itk_core-6.0b1-cp311-abi3-macosx_15_0_arm64.whl

---
+++ packages/itk-core/itk_core-6.0b1-cp311-abi3-macosx_15_0_arm64.whl/itk/Configuration/ITKIOImageBase_snake_case.py
@@ -0,0 +1,1 @@
+snake_case_functions = ('image_file_reader', 'image_file_writer', 'image_series_reader', 'image_series_writer', )
---
+++ packages/itk-core/itk_core-6.0b1-cp311-abi3-macosx_15_0_arm64.whl/itk/Configuration/ITKVTK_snake_case.py
@@ -0,0 +1,1 @@
+snake_case_functions = ('vtk_image_export', 'vtk_image_import', )
