99cd34a4eea1de3535ecf58f130f6217b4f3bfa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 17:05:05 +0000

    Add package netbox-load-balancing/1.2.0/netbox_load_balancing-1.2.0.tar.gz

---
+++ packages/netbox-load-balancing/netbox_load_balancing-1.2.0.tar.gz/netbox_load_balancing-1.2.0/netbox_load_balancing/graphql/enums.py
@@ -0,0 +1,27 @@
+import strawberry
+
+from netbox_load_balancing.choices import (
+    HealthMonitorTypeChoices,
+    HealthMonitorHTTPVersionChoices,
+    PoolAlgorythmChoices,
+    PoolSessionPersistenceChoices,
+    PoolBackupSessionPersistenceChoices,
+    ListenerProtocolChoices,
+)
+
+NetBoxLoadBalancingHealthMonitorTypeEnum = strawberry.enum(
+    HealthMonitorTypeChoices.as_enum()
+)
+NetBoxLoadBalancingHealthMonitorHTTPVersionEnum = strawberry.enum(
+    HealthMonitorHTTPVersionChoices.as_enum()
+)
+NetBoxLoadBalancingPoolAlgorythmEnum = strawberry.enum(PoolAlgorythmChoices.as_enum())
+NetBoxLoadBalancingPoolSessionPersistenceEnum = strawberry.enum(
+    PoolSessionPersistenceChoices.as_enum()
+)
+NetBoxLoadBalancingPoolBackupSessionPersistenceEnum = strawberry.enum(
+    PoolBackupSessionPersistenceChoices.as_enum()
+)
+NetBoxLoadBalancingListenerProtocolEnum = strawberry.enum(
+    ListenerProtocolChoices.as_enum()
+)
