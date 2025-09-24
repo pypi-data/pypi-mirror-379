62083be77c870d12349159851ce1ab4ff46ce20b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 14:04:44 +0000

    Add package v440/2.0.0.dev45/v440-2.0.0.dev45.tar.gz

---
+++ packages/v440/v440-2.0.0.dev45.tar.gz/v440-2.0.0.dev45/src/v440/_utils/VList.py
@@ -0,0 +1,62 @@
+from typing import *
+
+import setdoc
+from datahold import OkayList
+
+from v440._utils.BaseList import BaseList
+from v440.core.VersionError import VersionError
+
+
+class VList(OkayList, BaseList):
+
+    @setdoc.basic
+    def __eq__(self: Self, other: Any) -> bool:
+        ans: bool
+        try:
+            alt: Self = type(self)(other)
+        except VersionError:
+            ans = False
+        else:
+            ans = self._data == alt._data
+        return ans
+
+    @setdoc.basic
+    def __ge__(self: Self, other: Any, /) -> bool:
+        ans: bool
+        try:
+            alt: Self = type(self)(other)
+        except Exception:
+            ans = self.data >= other
+        else:
+            ans = alt <= self
+        return ans
+
+    @setdoc.basic
+    def __iadd__(self: Self, other: Any, /) -> Self:
+        self.data += type(self)(other).data
+        return self
+
+    @setdoc.basic
+    def __imul__(self: Self, other: Any, /) -> Self:
+        self.data = self.data * other
+        return self
+
+    @setdoc.basic
+    def __le__(self: Self, other: Any, /) -> bool:
+        ans: bool
+        try:
+            alt: Self = type(self)(other)
+        except Exception:
+            ans = self.data <= other
+        else:
+            ans = self._data <= alt._data
+        return ans
+
+    def __sorted__(self: Any, /, **kwargs: Any) -> Self:
+        "This magic method implements sorted(self, **kwargs)."
+        ans: Any = self.copy()
+        ans.sort(**kwargs)
+        return ans
+
+    def isempty(self: Self) -> bool:
+        return len(self) == 0
