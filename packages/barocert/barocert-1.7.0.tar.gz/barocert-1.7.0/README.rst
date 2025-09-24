299c4174cc955b10a0c910f83d0ea226cb5caf71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 15:04:59 +0000

    Add package barocert/1.7.0/barocert-1.7.0.tar.gz

---
+++ packages/barocert/barocert-1.7.0.tar.gz/barocert-1.7.0/README.rst
@@ -0,0 +1,36 @@
+####
+barocert.py
+####
+barocert 1.7.0
+
+Barocert API for Python.
+
+* requirements
+
+  * Python 2.6+
+  * linkhub
+  * pycryptodome 2.6.1+
+
+************
+Installation
+************
+
+Install via pip:
+
+::
+
+    $ pip install Barocert
+
+Install from source:
+
+::
+
+    $ git clone git://github.com/barocert/barocert.py.git
+    $ cd barocert.py
+    $ python setup.py install
+
+사이트
+-------------------------------
+`Pypi <https://pypi.python.org/pypi/popbill/>`_,
+`링크허브(Linkhub) <https://www.linkhub.co.kr/>`_,
+`Barocert <https://www.Barocert.com/>`_,
---
+++ packages/barocert/barocert-1.7.0.tar.gz/barocert-1.7.0/barocert/__init__.py
@@ -0,0 +1,30 @@
+__version__ = '1.3.0'
+Version = __version__  # for backward compatibility
+__all__ = ["BarocertException",
+           "KakaoCMS",
+           "KakaoIdentity",
+           "KakaoSign",
+           "KakaoMultiSign",
+           "KakaoMultiSignTokens",
+           "KakaocertService",
+           "NaverCMS",
+           "NaverIdentity",
+           "NaverSign",
+           "NaverMultiSign",
+           "NaverMultiSignTokens",
+           "NavercertService",
+           "PassCMS",
+           "PassIdentity",
+           "PassLogin",
+           "PassSign",
+           "PassIdentityVerify",
+           "PassSignVerify",
+           "PassCMSVerify",
+           "PassLoginVerify",
+           "PasscertService"
+           ]
+
+from .base import *
+from .kakaocertService import *
+from .navercertService import *
+from .passcertService import *
