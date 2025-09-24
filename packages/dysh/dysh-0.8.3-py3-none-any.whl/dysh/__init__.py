f2741269403b20a7a183bd6c251608b79e4c2378
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 06:03:49 +0000

    Add package dysh/0.8.3/dysh-0.8.3-py3-none-any.whl

---
+++ packages/dysh/dysh-0.8.3-py3-none-any.whl/dysh/__init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for dysh."""
+
+__version__ = "0.8.3"
+
+all = ["version"]
+
+
+def version():
+    """Version of the dysh code
+
+    Returns
+    -------
+    version : str
+        dysh version.
+    """
+    return __version__
