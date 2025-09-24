c004a37f05fbd88b169f06150332900a1cb31e3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 04:05:30 +0000

    Add package orca-python/0.8.0/orca_python-0.8.0-py3-none-any.whl

---
+++ packages/orca-python/orca_python-0.8.0-py3-none-any.whl/orca_python/exceptions.py
@@ -0,0 +1,26 @@
+class BaseOrcaException(Exception):
+    """Base exception for the Python Orca client"""
+
+
+class InvalidAlgorithmArgument(BaseOrcaException):
+    """Raised when an argument to `@algorithm` is not correct"""
+
+
+class InvalidAlgorithmReturnType(BaseOrcaException):
+    """Raised when the return type of an algorithm is not valid"""
+
+
+class InvalidWindowArgument(BaseOrcaException):
+    """Raised when an argument to the Window class is not valid"""
+
+
+class InvalidMetadataFieldArgument(BaseOrcaException):
+    """Raised when an argument to a metadata field is not valid"""
+
+
+class InvalidDependency(BaseOrcaException):
+    """Raised when a dependency is invalid"""
+
+
+class MissingDependency(BaseOrcaException):
+    """Raised when a dependency is missing"""
