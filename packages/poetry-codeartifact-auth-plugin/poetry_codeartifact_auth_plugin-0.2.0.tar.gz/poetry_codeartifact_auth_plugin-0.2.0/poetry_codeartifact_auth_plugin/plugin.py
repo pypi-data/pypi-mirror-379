561141406865e891e5cef47680a3c609029ea8e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 03:14:43 +0000

    Add package poetry-codeartifact-auth-plugin/0.2.0/poetry_codeartifact_auth_plugin-0.2.0.tar.gz

---
+++ packages/poetry-codeartifact-auth-plugin/poetry_codeartifact_auth_plugin-0.2.0.tar.gz/poetry_codeartifact_auth_plugin-0.2.0/poetry_codeartifact_auth_plugin/plugin.py
@@ -0,0 +1,60 @@
+import re
+
+import boto3
+from cleo.io.io import IO
+from poetry.plugins.plugin import Plugin
+from poetry.poetry import Poetry
+from tomlkit import TOMLDocument
+
+
+class CodeartifactLoginPlugin(Plugin):
+    def find_sources(self, data: TOMLDocument) -> list:
+        sources = data.get("tool", {}).get("poetry", {}).get("source", [])
+        if not sources:
+            return []
+        if not isinstance(sources, list):
+            sources = [sources]
+        for source in sources:
+            if not isinstance(source, dict):
+                raise ValueError("Source must be a dictionary")
+            source_name = source.get("name", None)
+            # Pypi should not have a url set as it's implicit for Poetry
+            if ("url" not in source and source_name and source_name.lower() != "pypi") or not source_name:
+                raise ValueError("Source must have 'name' and 'url' keys")
+
+        return sources
+
+    def find_codeartifact_sources(self, data: TOMLDocument) -> list:
+        sources = self.find_sources(data)
+        return [source for source in sources if "codeartifact" in source["url"]]
+
+    def get_codeartifact_credentials(self, source: dict) -> dict:
+        client = boto3.client("codeartifact")
+        try:
+            domain_data = re.match(
+                r"^https://(?P<domain>[^.]+)\.d\.codeartifact", source["url"]
+            )
+            if not domain_data:
+                raise ValueError("Invalid CodeArtifact URL")
+            domain, domain_owner = domain_data.group(1).split("-")
+            response = client.get_authorization_token(
+                domain=domain,
+                domainOwner=domain_owner,
+            )
+            auth_token = response.get("authorizationToken")
+            if not auth_token:
+                raise ValueError("Failed to get authorization token")
+            return auth_token
+        finally:
+            client.close()
+
+    def activate(self, poetry: Poetry, io: IO):
+        for source in self.find_codeartifact_sources(poetry.pyproject.data):
+            token = self.get_codeartifact_credentials(source)
+            poetry.config.merge(
+                config={
+                    "http-basic": {
+                        source["name"]: {"username": "aws", "password": token}
+                    }
+                },
+            )
