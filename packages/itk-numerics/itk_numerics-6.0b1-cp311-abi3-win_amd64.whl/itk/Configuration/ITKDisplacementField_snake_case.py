a8af936103e10772d80b4440145515ae7c1fecf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 18:06:10 +0000

    Add package itk-numerics/6.0b1/itk_numerics-6.0b1-cp311-abi3-win_amd64.whl

---
+++ packages/itk-numerics/itk_numerics-6.0b1-cp311-abi3-win_amd64.whl/itk/Configuration/ITKDisplacementField_snake_case.py
@@ -0,0 +1,1 @@
+snake_case_functions = ('displacement_field_jacobian_determinant_filter', 'transform_to_displacement_field_filter', )
---
+++ packages/itk-numerics/itk_numerics-6.0b1-cp311-abi3-win_amd64.whl/itk/Configuration/ITKEigen_snake_case.py
@@ -0,0 +1,1 @@
+snake_case_functions = ('eigen_analysis2_d_image_filter', )
