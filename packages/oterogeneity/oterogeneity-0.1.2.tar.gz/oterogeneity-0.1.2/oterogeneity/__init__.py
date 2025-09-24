b5290c94a6a2cd96dd6532ae3484f4034bc72698
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 18:05:56 +0000

    Add package oterogeneity/0.1.2/oterogeneity-0.1.2.tar.gz

---
+++ packages/oterogeneity/oterogeneity-0.1.2.tar.gz/oterogeneity-0.1.2/oterogeneity/__init__.py
@@ -0,0 +1,1 @@
+from oterogeneity_main import *
---
+++ packages/oterogeneity/oterogeneity-0.1.2.tar.gz/oterogeneity-0.1.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='oterogeneity',
+    version='0.1.2',
+    description='Python library to compute heterogeneity indexes based on optimal transport.',
+    author='Joseph TOUZET',
+    packages=find_packages(),
+    install_requires=['numpy', 'scikit-learn', 'pot']
+)
