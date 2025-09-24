32f260001bf0adb602892b38b1900c51c5022e16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 03:15:09 +0000

    Add package python-cq/0.7.3/python_cq-0.7.3.tar.gz

---
+++ packages/python-cq/python_cq-0.7.3.tar.gz/python_cq-0.7.3/cq/middlewares/scope.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from contextlib import AsyncExitStack
+from dataclasses import dataclass, field
+from typing import TYPE_CHECKING, Any
+
+from injection import adefine_scope
+from injection.exceptions import ScopeAlreadyDefinedError
+
+if TYPE_CHECKING:  # pragma: no cover
+    from cq import MiddlewareResult
+
+__all__ = ("InjectionScopeMiddleware",)
+
+
+@dataclass(repr=False, eq=False, frozen=True, slots=True)
+class InjectionScopeMiddleware:
+    scope_name: str
+    exist_ok: bool = field(default=False, kw_only=True)
+    threadsafe: bool | None = field(default=None, kw_only=True)
+
+    async def __call__(self, *args: Any, **kwargs: Any) -> MiddlewareResult[Any]:
+        async with AsyncExitStack() as stack:
+            context_manager = adefine_scope(self.scope_name, threadsafe=self.threadsafe)
+            try:
+                await stack.enter_async_context(context_manager)
+            except ScopeAlreadyDefinedError:
+                if not self.exist_ok:
+                    raise
+            del context_manager
+            yield
