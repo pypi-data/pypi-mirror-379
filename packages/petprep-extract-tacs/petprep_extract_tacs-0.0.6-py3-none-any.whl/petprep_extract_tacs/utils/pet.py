b18bbcdfd827fa1e260dd911663917cd29d662f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 03:14:44 +0000

    Add package petprep-extract-tacs/0.0.6/petprep_extract_tacs-0.0.6-py3-none-any.whl

---
+++ packages/petprep-extract-tacs/petprep_extract_tacs-0.0.6-py3-none-any.whl/petprep_extract_tacs/utils/pet.py
@@ -0,0 +1,44 @@
+def create_weighted_average_pet(pet_file, json_file):
+
+    import json
+    from niworkflows.interfaces.bids import ReadSidecarJSON
+    import nibabel as nib
+    import numpy as np
+    import os
+    from pathlib import Path
+
+    """
+    Create a time-weighted average of dynamic PET data using mid-frames
+
+    :param pet_file: Path to input dynamic PET volume
+    :type pet_file: str
+    :param json_file: Path to BIDS sidecar JSON file
+    :type json_file: str
+    :return: Path to the weighted average PET file
+    :rtype: str
+    """
+
+    img = nib.load(pet_file)
+    data = img.get_fdata()
+
+    # Load the .json file
+    with open(json_file, "r") as jf:
+        meta = json.load(jf)
+
+    frames_start = np.array(meta["FrameTimesStart"])
+    frames_duration = np.array(meta["FrameDuration"])
+
+    frames = range(data.shape[-1])
+
+    new_pth = os.getcwd()
+
+    mid_frames = frames_start + frames_duration / 2
+    wavg = np.trapz(data[..., frames], dx=np.diff(mid_frames[frames]), axis=3) / np.sum(
+        mid_frames
+    )
+
+    out_name = Path(pet_file.replace("_pet.", "_desc-wavg_pet.")).name
+    out_file = os.path.join(new_pth, out_name)
+    nib.save(nib.Nifti1Image(wavg, img.affine), out_file)
+
+    return out_file
