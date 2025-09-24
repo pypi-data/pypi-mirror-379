92019574816cd48842dab561c504f642540143b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 07:05:07 +0000

    Add package edc-qol/1.1.1/edc_qol-1.1.1.tar.gz

---
+++ packages/edc-qol/edc_qol-1.1.1.tar.gz/edc_qol-1.1.1/src/edc_qol/modeladmin_mixins/__init__.py
@@ -0,0 +1,20 @@
+from .eq5d3l_model_admin_mixin import (
+    Eq5d3lModelAdminMixin,
+    eq5d3l_fieldsets,
+    eq5d3l_radio_fields,
+)
+from .icecapa_model_admin_mixin import (
+    IcecapaModelAdminMixin,
+    icecapa_fieldsets,
+    icecapa_radio_fields,
+)
+
+
+__all__ = [
+    "Eq5d3lModelAdminMixin",
+    "eq5d3l_fieldsets",
+    "eq5d3l_radio_fields",
+    "IcecapaModelAdminMixin",
+    "icecapa_fieldsets",
+    "icecapa_radio_fields",
+]
