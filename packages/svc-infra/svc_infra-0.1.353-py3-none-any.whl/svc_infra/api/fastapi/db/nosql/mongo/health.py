ec08ca3fefe1eaa3ee554314caa25ba1d11b0e59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 07:04:35 +0000

    Add package svc-infra/0.1.353/svc_infra-0.1.353-py3-none-any.whl

---
+++ packages/svc-infra/svc_infra-0.1.353-py3-none-any.whl/svc_infra/api/fastapi/db/nosql/mongo/health.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from svc_infra.api.fastapi import DualAPIRouter
+from svc_infra.db.nosql.mongo.client import ping_mongo
+
+
+def make_mongo_health_router(
+    *, prefix: str = "/_mongo/health", include_in_schema: bool = False
+) -> DualAPIRouter:
+    router = DualAPIRouter(prefix=prefix, include_in_schema=include_in_schema)
+
+    @router.get("")
+    async def healthcheck():
+        ok = await ping_mongo()
+        return {"ok": ok}
+
+    return router
