9985e77ffb9dc415839bf0591d989004ed564b41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-24 18:05:59 +0000

    Add package itk-filtering/6.0b1/itk_filtering-6.0b1-cp311-abi3-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/itk-filtering/itk_filtering-6.0b1-cp311-abi3-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/itk/Configuration/ITKImageNoise_snake_case.py
@@ -0,0 +1,1 @@
+snake_case_functions = ('additive_gaussian_noise_image_filter', 'noise_base_image_filter', 'salt_and_pepper_noise_image_filter', 'shot_noise_image_filter', 'speckle_noise_image_filter', )
