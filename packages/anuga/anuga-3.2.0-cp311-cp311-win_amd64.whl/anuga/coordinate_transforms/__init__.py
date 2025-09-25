6946248da81bf0c4c6251f3fe9aebeb70d5ff310
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 16:07:01 +0000

    Add package anuga/3.2.0/anuga-3.2.0-cp311-cp311-win_amd64.whl

---
+++ packages/anuga/anuga-3.2.0-cp311-cp311-win_amd64.whl/anuga/coordinate_transforms/__init__.py
@@ -0,0 +1,8 @@
+
+
+from .redfearn import *
+from .point import *
+
+from numpy._pytesttester import PytestTester
+test = PytestTester(__name__)
+del PytestTester
---
+++ packages/anuga/anuga-3.2.0-cp311-cp311-win_amd64.whl/anuga/simulation/__init__.py
@@ -0,0 +1,10 @@
+""" Simulation Module
+
+    This module wraps the standard anuga script into a simulation class which
+    allows for the user to define the standard creation of domain etc
+
+"""
+
+from numpy._pytesttester import PytestTester
+test = PytestTester(__name__)
+del PytestTester
