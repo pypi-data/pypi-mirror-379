fb90158290b41d2bcfcd14b4fd0d2ba7b93e7c7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 07:06:30 +0000

    Add package whad/1.2.10/whad-1.2.10-py3-none-any.whl

---
+++ packages/whad/whad-1.2.10-py3-none-any.whl/whad/common/converters/hid/__init__.py
@@ -0,0 +1,76 @@
+from whad.common.converters.hid.exceptions import (
+    HIDCodeNotFound,
+    HIDKeyNotFound,
+    HIDLocaleNotFound,
+)
+from whad.common.converters.hid.mappings import HID_MAP, HID_SPECIALS
+
+
+class HIDConverter:
+    """
+    This class provides a basic API to convert an HID code to an human friendly keystroke and vice versa.
+    """
+
+    @staticmethod
+    def get_hid_code_from_key(
+        key: str, alt=False, ctrl=False, shift=False, gui=False, locale: str = "fr"
+    ) -> tuple[int, int]:
+        """
+        This function converts a key to a tuple composed of HID code and HID modifiers.
+
+        :param key: main key to convert
+        :param alt: alt key is pressed
+        :param ctrl: ctrl key is pressed
+        :param shift: shift key is pressed
+        :param gui: gui key is pressed
+        :param locale: Keyboard keymap to consider
+        """
+        if locale not in HID_MAP:
+            raise HIDLocaleNotFound(locale)
+
+        matching_codes = [m for m, k in HID_MAP[locale].items() if k == key]
+        if not matching_codes:
+            # Key was not found, try special keys
+            matching_codes = [m for m, k in HID_SPECIALS.items() if k == key]
+        if not matching_codes:
+            # Key was not found in locale keymap and in special keys
+            raise HIDKeyNotFound(key, alt, ctrl, shift, gui)
+        # Consider first keycode found as keys are ordered from most common to rarest
+        hid_code, modifiers = matching_codes[0]
+
+        # If caller specified some modifiers, apply them
+        modifier_keys = {"ALT": alt, "CTRL": ctrl, "SHIFT": shift, "GUI": gui}
+        for key, pressed in modifier_keys.items():
+            if pressed:
+                mapping = [m for m, k in HID_SPECIALS.items() if k == key][0]
+                modifiers += mapping[1]
+
+        return (hid_code, modifiers)
+
+    @staticmethod
+    def get_key_from_hid_code(
+        hid_code: int = 0, modifiers: int = 0, locale: str = "fr"
+    ) -> str:
+        """
+        This function converts an HID code and HID modifiers to the corresponding keystroke.
+
+        :param hid_code: HID code to convert
+        :param modifiers: HID modifiers to convert
+        :param locale: Keyboard keymap to consider
+        """
+        # Handle Ctrl, Alt and Gui keys
+        if modifiers & 1:
+            modifiers -= 1
+            return "CTRL+" + HIDConverter.get_key_from_hid_code(hid_code, modifiers, locale)
+        if modifiers & 4:
+            modifiers -= 4
+            return "ALT+" + HIDConverter.get_key_from_hid_code(hid_code, modifiers, locale)
+        if modifiers & 8:
+            modifiers -= 8
+            return "GUI+" + HIDConverter.get_key_from_hid_code(hid_code, modifiers, locale)
+
+        if (hid_code, modifiers) in HID_MAP[locale]:
+            return HID_MAP[locale][(hid_code, modifiers)]
+        if (hid_code, modifiers) in HID_SPECIALS:
+            return HID_SPECIALS[(hid_code, modifiers)]
+        raise HIDCodeNotFound(hid_code, modifiers)
