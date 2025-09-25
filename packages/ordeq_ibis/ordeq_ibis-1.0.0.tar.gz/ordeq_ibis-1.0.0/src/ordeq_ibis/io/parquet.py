069e4240b957685541196bf70c1cfa7ea2aa9242
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 07:06:57 +0000

    Add package ordeq_ibis/1.0.0/ordeq_ibis-1.0.0.tar.gz

---
+++ packages/ordeq_ibis/ordeq_ibis-1.0.0.tar.gz/ordeq_ibis-1.0.0/src/ordeq_ibis/io/parquet.py
@@ -0,0 +1,48 @@
+from dataclasses import dataclass
+from functools import cached_property
+from pathlib import Path
+
+import ibis
+from ibis import BaseBackend, Table
+from ordeq import IO
+
+
+@dataclass(frozen=True, kw_only=True)
+class IbisParquet(IO[Table]):
+    """IO to load from and save to PARQUET data using Ibis.
+
+    Example usage:
+
+    ```python
+    >>> from pathlib import Path
+    >>> from ordeq_ibis import IbisParquet
+    >>> MyParquetUsingPolars = IbisParquet(
+    ...     path=Path("path/to.parquet"),
+    ...     resource="polars://"
+    ... )
+
+    >>> MyParquetUsingDuckDB = IbisParquet(
+    ...     path=Path("path/to.parquet"),
+    ...     resource="duckdb://"
+    ... )
+
+    ```
+
+    See [1] on how to configure the `resource`.
+
+    [1]: https://ibis-project.org/reference/connection
+
+    """
+
+    path: Path
+    resource: Path | str
+
+    def load(self) -> Table:
+        return self._backend.read_parquet(self.path)
+
+    def save(self, t: Table) -> None:
+        self._backend.to_parquet(t, self.path)
+
+    @cached_property
+    def _backend(self) -> BaseBackend:
+        return ibis.connect(self.resource)
