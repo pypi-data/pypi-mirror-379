86d053d1936eb015c2c537fc326eb426b5c88328
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 03:18:06 +0000

    Add package libmdbx/0.3.1/libmdbx-0.3.1-cp313-cp313-win_amd64.whl

---
+++ packages/libmdbx/libmdbx-0.3.1-cp313-cp313-win_amd64.whl/mdbx/__init__.py
@@ -0,0 +1,17 @@
+from .mdbx import (
+    Env,
+    Geometry,
+    MDBXTXNFlags,
+    MDBXError,
+    MDBXOption,
+    Cursor,
+    MDBXCursorOp,
+    MDBXDBFlags,
+    MDBXEnvFlags,
+    MDBXStat,
+    MDBXErrorExc,
+    MDBXEnvinfo,
+    MDBXCopyMode,
+    get_version_info,
+    get_build_info,
+)
