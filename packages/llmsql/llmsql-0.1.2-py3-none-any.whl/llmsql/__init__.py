90f592fd2c2c80e3dbd8bec0e5cb6c4bf1e6cf86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 03:17:53 +0000

    Add package llmsql/0.1.2/llmsql-0.1.2-py3-none-any.whl

---
+++ packages/llmsql/llmsql-0.1.2-py3-none-any.whl/llmsql/__init__.py
@@ -0,0 +1,19 @@
+import logging
+import os
+
+__version__ = "0.1.2"
+
+
+def __getattr__(name: str):
+    if name == "LLMSQLVLLMInference":
+        from .inference.inference import LLMSQLVLLMInference
+
+        return LLMSQLVLLMInference
+    elif name == "LLMSQLEvaluator":
+        from .evaluation.evaluator import LLMSQLEvaluator
+
+        return LLMSQLEvaluator
+    raise AttributeError(f"module {__name__} has no attribute {name!r}")
+
+
+__all__ = ["LLMSQLEvaluator", "LLMSQLVLLMInference"]
