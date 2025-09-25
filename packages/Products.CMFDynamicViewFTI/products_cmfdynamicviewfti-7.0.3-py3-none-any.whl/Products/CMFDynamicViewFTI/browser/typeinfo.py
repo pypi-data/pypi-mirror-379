8963e09b505343fb2757677d773019cb3f5ea182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 04:06:01 +0000

    Add package Products.CMFDynamicViewFTI/7.0.3/products_cmfdynamicviewfti-7.0.3-py3-none-any.whl

---
+++ packages/Products.CMFDynamicViewFTI/products_cmfdynamicviewfti-7.0.3-py3-none-any.whl/Products/CMFDynamicViewFTI/browser/typeinfo.py
@@ -0,0 +1,16 @@
+"""
+Add DynamicView FTI form (ZMI)
+"""
+
+# $Id$
+
+from Products.CMFCore.browser.typeinfo import FactoryTypeInformationAddView
+from Products.CMFDynamicViewFTI import DynamicViewTypeInformation
+
+
+class DVFactoryTypeInformationAddView(FactoryTypeInformationAddView):
+    """See FactoryTypeInformationAddView that does all the job"""
+
+    klass = DynamicViewTypeInformation
+
+    description = "A dynamic view type information object defines a portal type."
