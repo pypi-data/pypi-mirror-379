50db520526936feb33be83a3b274339ddf6378a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 03:17:43 +0000

    Add package soda-core-sqlserver/3.5.6/soda_core_sqlserver-3.5.6.tar.gz

---
+++ packages/soda-core-sqlserver/soda_core_sqlserver-3.5.6.tar.gz/soda_core_sqlserver-3.5.6/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import find_namespace_packages, setup
+
+package_name = "soda-core-sqlserver"
+package_version = "3.5.6"
+description = "Soda Core SQL Server Package"
+
+requires = [f"soda-core=={package_version}", "pyodbc", "azure-identity~=1.17.1"]
+# TODO Fix the params
+setup(
+    name=package_name,
+    version=package_version,
+    install_requires=requires,
+    packages=find_namespace_packages(include=["soda*"]),
+)
