340784ca67195ba9d28c7ece7e6d666bcfbd4831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 14:06:51 +0000

    Add package fairchem-core/2.7.1/fairchem_core-2.7.1.tar.gz

---
+++ packages/fairchem-core/fairchem_core-2.7.1.tar.gz/fairchem_core-2.7.1/src/fairchem/core/components/calculate/__init__.py
@@ -0,0 +1,12 @@
+"""
+Copyright (c) Meta Platforms, Inc. and affiliates.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+"""
+
+from __future__ import annotations
+
+from .calculate_runner import CalculateRunner
+
+__all__ = ["CalculateRunner"]
