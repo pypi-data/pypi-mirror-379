61c0312a0a47b49fdc773fb4ba5aa1a1e5b98277
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 03:17:43 +0000

    Add package soda-core-trino/3.5.6/soda_core_trino-3.5.6.tar.gz

---
+++ packages/soda-core-trino/soda_core_trino-3.5.6.tar.gz/soda_core_trino-3.5.6/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import find_namespace_packages, setup
+
+package_name = "soda-core-trino"
+package_version = "3.5.6"
+description = "Soda Core Trino Package"
+
+requires = [f"soda-core=={package_version}", "trino>=0.315.0"]
+
+setup(
+    name=package_name,
+    version=package_version,
+    install_requires=requires,
+    packages=find_namespace_packages(include=["soda*"]),
+)
