9d9d1f20abfa79fda59e5d881362973049dee7dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 03:17:54 +0000

    Add package togura/0.1.0a2/togura-0.1.0a2.tar.gz

---
+++ packages/togura/togura-0.1.0a2.tar.gz/togura-0.1.0a2/tests/test_jpcoar.py
@@ -0,0 +1,9 @@
+import togura.jpcoar as jpcoar
+
+
+def test_resource_type_uri():
+    assert jpcoar.resource_type_uri("test") is None
+    assert (
+        jpcoar.resource_type_uri("article")
+        == "http://purl.org/coar/resource_type/c_6501"
+    )
