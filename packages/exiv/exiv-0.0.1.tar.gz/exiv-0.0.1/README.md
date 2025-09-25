df3e09ec970cadc59498ab56c5a366090b0aa00e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 07:06:02 +0000

    Add package exiv/0.0.1/exiv-0.0.1.tar.gz

---
+++ packages/exiv/exiv-0.0.1.tar.gz/exiv-0.0.1/README.md
@@ -0,0 +1,8 @@
+# exiv
+[WIP]
+This is in active development and will be pusblished soon.
+
+## Install
+```bash
+pip install exiv
+```
---
+++ packages/exiv/exiv-0.0.1.tar.gz/exiv-0.0.1/exiv/core.py
@@ -0,0 +1,6 @@
+def hello():
+    return "Hello from exiv!"
+
+def add(a: int, b: int) -> int:
+    """Return the sum of two numbers."""
+    return a + b
