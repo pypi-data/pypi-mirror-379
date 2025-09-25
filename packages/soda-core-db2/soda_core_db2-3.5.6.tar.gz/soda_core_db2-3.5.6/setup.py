6647490f458ff95a2c02c8c2a977f50150dad1ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 03:17:42 +0000

    Add package soda-core-db2/3.5.6/soda_core_db2-3.5.6.tar.gz

---
+++ packages/soda-core-db2/soda_core_db2-3.5.6.tar.gz/soda_core_db2-3.5.6/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import find_namespace_packages, setup
+
+package_name = "soda-core-db2"
+package_version = "3.5.6"
+# TODO Add proper description
+description = "Soda Core IBM DB2 Package"
+
+requires = [f"soda-core=={package_version}", "ibm-db==3.2.3"]
+# TODO Fix the params
+setup(
+    name=package_name,
+    version=package_version,
+    install_requires=requires,
+    packages=find_namespace_packages(include=["soda*"]),
+)
