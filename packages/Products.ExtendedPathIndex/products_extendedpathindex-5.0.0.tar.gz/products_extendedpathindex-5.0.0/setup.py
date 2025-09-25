d6ed924a6f02987b063644e7d8cc57dbe1202205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 04:05:59 +0000

    Add package Products.ExtendedPathIndex/5.0.0/products_extendedpathindex-5.0.0.tar.gz

---
+++ packages/Products.ExtendedPathIndex/products_extendedpathindex-5.0.0.tar.gz/products_extendedpathindex-5.0.0/setup.py
@@ -0,0 +1,47 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+version = "5.0.0"
+
+setup(
+    name="Products.ExtendedPathIndex",
+    version=version,
+    description="Zope catalog index for paths",
+    long_description=(open("README.rst").read() + "\n" + open("CHANGES.rst").read()),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: Core",
+        "Framework :: Plone :: 6.0",
+        "Framework :: Plone :: 6.1",
+        "Framework :: Plone :: 6.2",
+        "Framework :: Zope",
+        "Framework :: Zope :: 5",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+    ],
+    keywords="Zope catalog index",
+    author="Plone Foundation",
+    author_email="plone-developers@lists.sourceforge.net",
+    url="https://github.com/plone/Products.ExtendedPathIndex",
+    license="GPL version 2",
+    packages=find_packages("src"),
+    namespace_packages=["Products"],
+    package_dir={"": "src"},
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=3.9",
+    install_requires=[
+        "setuptools",
+        "Zope",
+        "Products.ZCatalog",
+    ],
+)
