41285dfb80bad71f1d35d314ced59f2a4fee0479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 03:17:42 +0000

    Add package soda-core-athena/3.5.6/soda_core_athena-3.5.6.tar.gz

---
+++ packages/soda-core-athena/soda_core_athena-3.5.6.tar.gz/soda_core_athena-3.5.6/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import find_namespace_packages, setup
+
+package_name = "soda-core-athena"
+package_version = "3.5.6"
+description = "Soda Core Athena Package"
+
+requires = [
+    f"soda-core=={package_version}",
+    "PyAthena>=2.2.0, <3.0",
+]
+# TODO Fix the params
+setup(
+    name=package_name,
+    version=package_version,
+    install_requires=requires,
+    packages=find_namespace_packages(include=["soda*"]),
+)
