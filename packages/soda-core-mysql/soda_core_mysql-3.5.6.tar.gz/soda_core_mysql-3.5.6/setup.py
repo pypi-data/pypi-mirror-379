47ce327c70b8237e71d4d7d304917db423bb1d0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 03:17:43 +0000

    Add package soda-core-mysql/3.5.6/soda_core_mysql-3.5.6.tar.gz

---
+++ packages/soda-core-mysql/soda_core_mysql-3.5.6.tar.gz/soda_core_mysql-3.5.6/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import find_namespace_packages, setup
+
+package_name = "soda-core-mysql"
+package_version = "3.5.6"
+description = "Soda Core MySQL Package"
+
+requires = [
+    f"soda-core=={package_version}",
+    "mysql-connector-python==8.0.30",
+]
+# TODO Fix the params
+setup(
+    name=package_name,
+    version=package_version,
+    install_requires=requires,
+    packages=find_namespace_packages(include=["soda*"]),
+)
