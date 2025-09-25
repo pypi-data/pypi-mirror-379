2b33c3ee75e9d574764b97ba68d38a1a3a9cae84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 16:07:17 +0000

    Add package gnps-mhd-integration/0.0.6/gnps_mhd_integration-0.0.6.tar.gz

---
+++ packages/gnps-mhd-integration/gnps_mhd_integration-0.0.6.tar.gz/gnps_mhd_integration-0.0.6/gnps2mhd/convertor_factory.py
@@ -0,0 +1,31 @@
+from mhd_model.convertors.mhd.convertor import BaseMhdConvertor, BaseMhdConvertorFactory
+from mhd_model.model.definitions import (
+    MHD_MODEL_V0_1_DEFAULT_SCHEMA_NAME,
+    MHD_MODEL_V0_1_LEGACY_PROFILE_NAME,
+    MHD_MODEL_V0_1_MS_PROFILE_NAME,
+)
+
+from gnps2mhd.v0_1.legacy.convertor import LegacyProfileV01Convertor
+from gnps2mhd.v0_1.ms.convertor import MsProfileV01Convertor
+
+
+class Gnps2MhdConvertorFactory(BaseMhdConvertorFactory):
+    def get_convertor(
+        self,
+        target_mhd_model_schema_uri: str,
+        target_mhd_model_profile_uri: str,
+    ) -> BaseMhdConvertor:
+        if target_mhd_model_schema_uri == MHD_MODEL_V0_1_DEFAULT_SCHEMA_NAME:
+            if target_mhd_model_profile_uri == MHD_MODEL_V0_1_LEGACY_PROFILE_NAME:
+                return LegacyProfileV01Convertor(
+                    target_mhd_model_schema_uri=target_mhd_model_schema_uri,
+                    target_mhd_model_profile_uri=target_mhd_model_profile_uri,
+                )
+            elif target_mhd_model_profile_uri == MHD_MODEL_V0_1_MS_PROFILE_NAME:
+                return MsProfileV01Convertor(
+                    target_mhd_model_schema_uri=target_mhd_model_schema_uri,
+                    target_mhd_model_profile_uri=target_mhd_model_profile_uri,
+                )
+            raise NotImplementedError()
+        else:
+            raise NotImplementedError()
