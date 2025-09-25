310b28f422496c17b7f84736f06937d9d721117d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 06:06:49 +0000

    Add package ahnufbasiccalculator/0.0.1/ahnufbasiccalculator-0.0.1.tar.gz

---
+++ packages/ahnufbasiccalculator/ahnufbasiccalculator-0.0.1.tar.gz/ahnufbasiccalculator-0.0.1/ahnufBasicCalc/__init__.py
@@ -0,0 +1,18 @@
+def add(num1, num2):
+    return(num1+num2)
+
+def sub(num1,num2):
+    if(num1>=num2):
+        return(num1-num2)
+    else:
+        return(num2-num1)
+    
+def add(num1, num2):
+    return(num1*num2) 
+
+def div(num1, num2):
+    if(num2==0):
+        print("INFINITY :)") 
+    else:
+        return(num1/num2)    
+     
