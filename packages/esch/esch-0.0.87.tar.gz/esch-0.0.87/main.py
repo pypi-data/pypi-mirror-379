365d50395dd53e44e8f2e8dd6b35ba57175dec86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 05:04:44 +0000

    Add package esch/0.0.87/esch-0.0.87.tar.gz

---
+++ packages/esch/esch-0.0.87.tar.gz/esch-0.0.87/main.py
@@ -0,0 +1,28 @@
+# %% main.py
+#   esch main file for testing
+# by: Noah Syrkis
+
+# Imports
+import esch
+import numpy as np
+from numpy import random
+
+# row column depth channel time
+N, C, H, W, T = int(2), int(4), int(8), int(16), int(32)
+
+
+# grid tests
+grid_test = [
+    ((N, C, H, W, T), "n c h w t"),
+    ((H, W, T), "h w t"),
+    ((N, H, W), "n h w"),
+    ((H, W), "h w"),
+    ((H, T), "h t"),
+    ((W, T), "w t"),
+    ((T,), "t"),
+]
+
+for shape, pattern in grid_test:
+    arr = (random.random(shape) - 0.5) * 2
+    e = esch.draw(pattern, arr, debug=True)
+    e.save(f"figs_{pattern}.svg")
