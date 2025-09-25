ce111b75861bccce281170f010afb7ee59ec854c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 16:06:54 +0000

    Add package calipytion/1.0.2/calipytion-1.0.2-py3-none-any.whl

---
+++ packages/calipytion/calipytion-1.0.2-py3-none-any.whl/calipytion/units/predefined.py
@@ -0,0 +1,43 @@
+# Deprecated units after migrating to mdmodels which provides rich units via strings
+
+# Molarity
+M = "mol / l"
+mM = "mmol / l"
+uM = "umol / l"
+nM = "nmol / l"
+
+# Substance
+mol = "mol"
+mmol = "mmol"
+umol = "umol"
+nmol = "nmol"
+
+# Mass
+gram = "g"
+g = "g"
+mg = "mg"
+ug = "ug"
+ng = "ng"
+kg = "kg"
+
+# Volume
+litre = "l"
+l = "l"  # noqa: E741
+ml = "ml"
+ul = "ul"
+nl = "nl"
+
+
+# Time
+second = "s"
+s = "s"
+minute = "min"
+hour = "h"
+h = "h"
+day = "d"
+d = "d"
+
+# Temperature
+K = "K"
+celsius = "C"
+C = "C"
