2e7c24cb40b3d1c69944e96282a33d2539ce18c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 14:06:31 +0000

    Add package bridgernadesigner/0.1.0/bridgernadesigner-0.1.0.tar.gz

---
+++ packages/bridgernadesigner/bridgernadesigner-0.1.0.tar.gz/bridgernadesigner-0.1.0/bridgernadesigner/main.py
@@ -0,0 +1,61 @@
+import click
+import sys
+from bridgernadesigner import errors, run
+from bridgernadesigner.classes import SCAFFOLD_NAME_TO_CLASS
+
+
+@click.group(invoke_without_command=True)
+@click.option('--target', '-t', required=True, help="14nt target sequence")
+@click.option('--donor', '-d', required=True, help="14nt donor sequence")
+@click.option('--scaffold', '-s', required=True, type=click.Choice(['IS621', 'ISCro4_WT', 'ISCro4_enhanced']), help="Template bridge RNA scaffold")
+@click.option('--output-format', '-of', default='fasta', type=click.Choice(['fasta', 'stockholm']))
+@click.pass_context
+def cli(ctx, target, donor, scaffold, output_format):
+    if ctx.invoked_subcommand is None:
+        ctx.invoke(default_command, target=target, donor=donor, scaffold=scaffold, output_format=output_format)
+
+@cli.command()
+@click.pass_context
+def default_command(ctx, target, donor, scaffold, output_format):
+
+    target = target.upper()
+    donor = donor.upper()
+    brna_scaffold = SCAFFOLD_NAME_TO_CLASS[scaffold]
+
+    try:
+        brna_scaffold.check_target_length(target)
+    except errors.TargetLengthError as e:
+        print("ERROR:", e)
+        print("Exiting...")
+        sys.exit()
+
+    try:
+        brna_scaffold.check_donor_length(donor)
+    except errors.DonorLengthError as e:
+        print("ERROR:", e)
+        print("Exiting...")
+        sys.exit()
+
+    try:
+        brna_scaffold.check_target_is_dna(target)
+    except errors.TargetNotDNAError as e:
+        print("ERROR:", e)
+        print("Exiting...")
+        sys.exit()
+
+    try:
+        brna_scaffold.check_donor_is_dna(donor)
+    except errors.DonorNotDNAError as e:
+        print("ERROR:", e)
+        print("Exiting...")
+        sys.exit()
+
+    brna = run.design_bridge_rna(target, donor, scaffold)
+    if output_format == "fasta":
+        print(brna.format_fasta())
+    elif output_format == "stockholm":
+        print(brna.format_stockholm())
+
+
+if __name__ == '__main__':
+    cli()
---
+++ packages/bridgernadesigner/bridgernadesigner-0.1.0.tar.gz/bridgernadesigner-0.1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import os
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name="bridgernadesigner",
+    version='0.1.0',
+    description='Software package to design bridge RNAs as described by Durrant & Perry et al. 2024 and Perry et al. 2025',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/hsulab-arc/BridgeRNADesigner',
+    author="Matt Durrant",
+    author_email="matthew@arcinstitute.org",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        'biopython',
+        'click==7.0'
+    ],
+    zip_safe=False,
+    entry_points = {
+        'console_scripts': [
+            'brna-design = bridgernadesigner.main:cli'
+        ]
+    }
+    )
