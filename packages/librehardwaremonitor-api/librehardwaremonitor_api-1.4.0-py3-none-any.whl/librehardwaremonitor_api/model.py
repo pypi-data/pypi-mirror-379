6aea91dc074a1fadd65005e6b3ff55248c722746
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 15:05:43 +0000

    Add package librehardwaremonitor-api/1.4.0/librehardwaremonitor_api-1.4.0-py3-none-any.whl

---
+++ packages/librehardwaremonitor-api/librehardwaremonitor_api-1.4.0-py3-none-any.whl/librehardwaremonitor_api/model.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+from types import MappingProxyType
+from typing import NewType
+
+DeviceId = NewType("DeviceId", str)
+DeviceName = NewType("DeviceName", str)
+
+@dataclass(frozen=True)
+class LibreHardwareMonitorSensorData:
+    """Data class to hold all data for a specific sensor."""
+    name: str
+    value: str
+    min: str
+    max: str
+    unit: str | None
+    device_id: str
+    device_name: str
+    device_type: str
+    sensor_id: str
+
+
+@dataclass(frozen=True)
+class LibreHardwareMonitorData:
+    """Data class to hold device names and data for all sensors."""
+    main_device_ids_and_names: MappingProxyType[DeviceId, DeviceName]
+    sensor_data: MappingProxyType[str, LibreHardwareMonitorSensorData]
