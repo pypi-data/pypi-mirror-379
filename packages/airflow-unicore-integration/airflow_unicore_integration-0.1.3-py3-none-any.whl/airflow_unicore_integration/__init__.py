db188971eddb59a1c906bc31bb5848f3dceaa4e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 15:06:19 +0000

    Add package airflow-unicore-integration/0.1.3/airflow_unicore_integration-0.1.3-py3-none-any.whl

---
+++ packages/airflow-unicore-integration/airflow_unicore_integration-0.1.3-py3-none-any.whl/airflow_unicore_integration/__init__.py
@@ -0,0 +1,12 @@
+def get_provider_info():
+    return {
+        "package-name": "airflow-unicore-integration",
+        "name": "Unicore",
+        "description": "Apache Airflow Unicore provider containing Operators and hooks.",
+        "connection-types": [
+            {
+                "connection-type": "unicore",
+                "hook-class-name": "airflow_unicore_integration.hooks.unicore_hooks.UnicoreHook",
+            }
+        ],
+    }
