a3fc08d7c523220b0d9480d9acbc538604e2d71c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 06:06:45 +0000

    Add package kittycad/1.2.5/kittycad-1.2.5-py3-none-any.whl

---
+++ packages/kittycad/kittycad-1.2.5-py3-none-any.whl/kittycad/models/text_to_cad_create_body.py
@@ -0,0 +1,13 @@
+from typing import Optional
+
+from .base import KittyCadBaseModel
+
+
+class TextToCadCreateBody(KittyCadBaseModel):
+    """Body for generating parts from text."""
+
+    kcl_version: Optional[str] = None
+
+    project_name: Optional[str] = None
+
+    prompt: str
