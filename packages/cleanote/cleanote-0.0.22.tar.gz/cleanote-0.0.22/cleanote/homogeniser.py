af8411e4b7fcf837eecc4cf2628c630e607ff80b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 04:05:56 +0000

    Add package cleanote/0.0.22/cleanote-0.0.22.tar.gz

---
+++ packages/cleanote/cleanote-0.0.22.tar.gz/cleanote-0.0.22/cleanote/homogeniser.py
@@ -0,0 +1,30 @@
+from typing import Iterable, Union
+from .types import Doc, Context
+from .model import Model
+
+
+class Homogeniser:
+    def run(
+        self,
+        model_loader: Model,
+        docs: Union[Doc, Iterable[Doc]],
+        ctx: Context,
+    ) -> Union[Doc, Iterable[Doc]]:
+        print(f"[Homogeniser] Initializing model loader '{model_loader.name}'...")
+        model_loader.initialize()
+        print("[Homogeniser] Initialization completed.\n")
+
+        # 2) Normalize input to a list for consistent handling
+        is_single = not isinstance(docs, Iterable) or isinstance(docs, Doc)
+        in_docs = [docs] if is_single else list(docs)
+        print(
+            f"[Homogeniser] Sending {len(in_docs)} document(s) to the model loader..."
+        )
+
+        # 3) Send documents to the model loader
+        out_docs = in_docs  # [model_loader.transform(d, ctx) for d in in_docs]
+
+        # 4) Return output
+        print(f"[Homogeniser] Model loader returned {len(out_docs)} document(s).")
+        print("[Homogeniser] Done.")
+        return out_docs[0] if is_single else out_docs
