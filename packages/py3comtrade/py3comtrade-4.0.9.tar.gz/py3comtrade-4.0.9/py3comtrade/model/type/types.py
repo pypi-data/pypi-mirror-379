77d0c00c90dedcb278bd78929ee3d069eb1ca121
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 14:06:33 +0000

    Add package py3comtrade/4.0.9/py3comtrade-4.0.9.tar.gz

---
+++ packages/py3comtrade/py3comtrade-4.0.9.tar.gz/py3comtrade-4.0.9/py3comtrade/model/type/types.py
@@ -0,0 +1,37 @@
+from enum import Enum
+from pathlib import Path
+
+import numpy as np
+from numpy.typing import NDArray
+from typing_extensions import TypedDict
+
+Array = NDArray
+FloatArray64 = NDArray[np.float64]
+FloatArray32 = NDArray[np.float32]
+IntArray64 = NDArray[np.int64]
+IntArray32 = NDArray[np.int32]
+
+
+class FilePath(TypedDict):
+    # 配置文件路径
+    cfg_path: Path
+    # 数据文件路径
+    dat_path: Path
+    # 模型文件路径
+    dmf_path: Path
+
+
+class IdxType(Enum):
+    INDEX = (0, "index索引")
+    CFGAN = (1, "cfg_idx索引")
+
+
+class ChannelType(Enum):
+    ANALOG = (0, "模拟通道")
+    DIGITAL = (1, "开关量通道")
+    ALL  = (2, "全部通道")
+
+class ValueType(Enum):
+    INSTANT = (0, "瞬时值")
+    RAW = (1, "原始采样值")
+    RMS = (2, "有效值值")
---
+++ packages/py3comtrade/py3comtrade-4.0.9.tar.gz/py3comtrade-4.0.9/py3comtrade/reader/nrates_parser.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#  Copyright (c) [2019] [name of copyright holder]
+#  [py3comtrade] is licensed under Mulan PSL v2.
+#  You can use this software according to the terms and conditions of the Mulan
+#  PSL v2.
+#  You may obtain a copy of Mulan PSL v2 at:
+#           http://license.coscl.org.cn/MulanPSL2
+#  THIS SOFTWARE IS PROVIDED ON CFGAN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+#  KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+#  NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+#  See the Mulan PSL v2 for more details.
+from py3comtrade.model.config_sample import ConfigSample
+from py3comtrade.model.nrate import Nrate
+
+
+def create_nrates(freg, nrate_num):
+    """
+    创建采样段对象
+    """
+    freg = float(freg.strip())
+    freg = 50.0 if freg == 0.0 else freg
+    nrate_num = int(nrate_num.strip())
+    return ConfigSample(freg=freg, nrate_num=nrate_num)
+
+
+def create_nrate(line):
+    """
+    解析采样段信息
+    """
+    samp, end_point = line.strip().split(",")
+    return Nrate(samp=samp, end_point=end_point)
