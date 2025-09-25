32ad8431d582b31ead02a8c58b49184d57ddd4de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 03:17:43 +0000

    Add package soda-core-dremio/3.5.6/soda_core_dremio-3.5.6.tar.gz

---
+++ packages/soda-core-dremio/soda_core_dremio-3.5.6.tar.gz/soda_core_dremio-3.5.6/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import find_namespace_packages, setup
+
+package_name = "soda-core-dremio"
+package_version = "3.5.6"
+description = "Soda Core Dremio Package"
+
+requires = [f"soda-core=={package_version}", "pyodbc", "pyarrow"]
+
+setup(
+    name=package_name,
+    version=package_version,
+    install_requires=requires,
+    packages=find_namespace_packages(include=["soda*"]),
+)
