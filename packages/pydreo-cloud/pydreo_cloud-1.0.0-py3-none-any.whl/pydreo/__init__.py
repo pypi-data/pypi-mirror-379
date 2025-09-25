577ef07b1daa63aa4d308fd743756396ffd8cabe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 16:07:17 +0000

    Add package pydreo-cloud/1.0.0/pydreo_cloud-1.0.0-py3-none-any.whl

---
+++ packages/pydreo-cloud/pydreo_cloud-1.0.0-py3-none-any.whl/pydreo/__init__.py
@@ -0,0 +1,16 @@
+# -----------------------------------------------------------
+# Library to login to Dreo cloud and get device info.
+#
+# (C) 2025 Brooke Wang
+# Released under MIT License
+# email developer@dreo.com
+# -----------------------------------------------------------
+
+"""
+DreoCloud Python Library
+
+A Python library for interacting with the DreoCloud API to manage smart home devices.
+Provides authentication, device discovery, status monitoring, and control capabilities.
+"""
+
+__version__ = "1.0.0"
