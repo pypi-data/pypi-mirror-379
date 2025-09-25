2e5e0523c501a345dd4eba656c867014a8a52468
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 08:05:36 +0000

    Add package aissemble-machine-learning-training-base/1.13.0rc1/aissemble_machine_learning_training_base-1.13.0rc1.tar.gz

---
+++ packages/aissemble-machine-learning-training-base/aissemble_machine_learning_training_base-1.13.0rc1.tar.gz/aissemble_machine_learning_training_base-1.13.0rc1/src/aissemble_machine_learning_training_base/generated/pipeline/pipeline_base.py
@@ -0,0 +1,38 @@
+###
+# #%L
+# aiSSEMBLE::Test::MDA::Machine Learning::Machine Learning Training Base
+# %%
+# Copyright (C) 2021 Booz Allen
+# %%
+# This software package is licensed under the Booz Allen Public License. All Rights Reserved.
+# #L%
+###
+from krausening.logging import LogManager
+
+class PipelineBase:
+    """
+    Performs pipeline level process for ExampleMachineLearningBasePipeline.
+
+    GENERATED CODE - DO NOT MODIFY
+
+    Generated from: templates/pipeline.base.py.vm
+    """
+
+    _instance = None
+    logger = LogManager.get_instance().get_logger('PipelineBase')
+
+
+    def __new__(cls):
+        """
+        Create a singleton class for pipeline level process
+        """
+        if cls._instance is None:
+            print("Creating the PipelineBase")
+            cls._instance = super(PipelineBase, cls).__new__(cls)
+        return cls._instance
+
+
+
+
+
+
