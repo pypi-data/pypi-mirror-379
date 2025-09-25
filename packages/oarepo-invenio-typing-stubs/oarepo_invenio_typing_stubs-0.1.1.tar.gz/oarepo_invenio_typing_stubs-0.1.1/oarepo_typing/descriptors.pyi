cdb2c7925a9054ed71f685d62d9e7e7ca6408547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 15:05:38 +0000

    Add package oarepo-invenio-typing-stubs/0.1.1/oarepo_invenio_typing_stubs-0.1.1.tar.gz

---
+++ packages/oarepo-invenio-typing-stubs/oarepo_invenio_typing_stubs-0.1.1.tar.gz/oarepo_invenio_typing_stubs-0.1.1/oarepo_typing/descriptors.pyi
@@ -0,0 +1,11 @@
+from typing import Generic, Self, TypeVar, overload
+
+T = TypeVar("T")  # owner type
+V = TypeVar("V")  # value type
+
+class Descriptor(Generic[T, V]):
+    @overload
+    def __get__(self, instance: None, owner: type[T]) -> Self: ...
+    @overload
+    def __get__(self, instance: T, owner: type[T]) -> V: ...
+    def __set__(self, instance: T, value: V) -> None: ...
