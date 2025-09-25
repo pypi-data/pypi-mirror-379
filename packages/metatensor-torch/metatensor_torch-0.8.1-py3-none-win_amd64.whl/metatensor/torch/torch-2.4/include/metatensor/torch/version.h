30241c4ffc1aab9a3ea84cf4a1ce4aee6aa4e7b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 04:05:10 +0000

    Add package metatensor-torch/0.8.1/metatensor_torch-0.8.1-py3-none-win_amd64.whl

---
+++ packages/metatensor-torch/metatensor_torch-0.8.1-py3-none-win_amd64.whl/metatensor/torch/torch-2.4/include/metatensor/torch/version.h
@@ -0,0 +1,11 @@
+/** Full version of metatensor-torch as a string */
+#define METATENSOR_TORCH_VERSION "0.8.1"
+
+/** Major version number of metatensor-torch as an integer */
+#define METATENSOR_TORCH_VERSION_MAJOR 0
+
+/** Minor version number of metatensor-torch as an integer */
+#define METATENSOR_TORCH_VERSION_MINOR 8
+
+/** Patch version number of metatensor-torch as an integer */
+#define METATENSOR_TORCH_VERSION_PATCH 1
