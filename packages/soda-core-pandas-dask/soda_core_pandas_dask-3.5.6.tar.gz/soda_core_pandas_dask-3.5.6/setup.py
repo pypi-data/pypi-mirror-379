21f72ed22c14c9b06d772e27b83ae0ce45a0408f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 03:17:42 +0000

    Add package soda-core-pandas-dask/3.5.6/soda_core_pandas_dask-3.5.6.tar.gz

---
+++ packages/soda-core-pandas-dask/soda_core_pandas_dask-3.5.6.tar.gz/soda_core_pandas_dask-3.5.6/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import find_namespace_packages, setup
+
+package_name = "soda-core-pandas-dask"
+package_version = "3.5.6"
+description = "Soda Core Dask Package"
+
+# 2023.10 or its subdependencies introduces breaking changes in how rows are counted, so we stay away from it for now.
+requires = [f"soda-core=={package_version}", "dask>=2022.10.0", "dask-sql>=2022.12.0,<2023.10.0"]
+
+
+setup(
+    name=package_name,
+    version=package_version,
+    install_requires=requires,
+    packages=find_namespace_packages(include=["soda*"]),
+)
