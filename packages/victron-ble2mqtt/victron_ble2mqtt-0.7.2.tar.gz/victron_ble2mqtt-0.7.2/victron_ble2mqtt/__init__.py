9fb0f6e8e382801488a135326642cc24bd8ed321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 05:05:05 +0000

    Add package victron-ble2mqtt/0.7.2/victron_ble2mqtt-0.7.2.tar.gz

---
+++ packages/victron-ble2mqtt/victron_ble2mqtt-0.7.2.tar.gz/victron_ble2mqtt-0.7.2/victron_ble2mqtt/__init__.py
@@ -0,0 +1,8 @@
+"""
+    victron_ble2mqtt
+    Emit MQTT events from victron-ble
+"""
+
+# See https://packaging.python.org/en/latest/specifications/version-specifiers/
+__version__ = '0.7.2'
+__author__ = 'Jens Diemer <git@jensdiemer.de>'
---
+++ packages/victron-ble2mqtt/victron_ble2mqtt-0.7.2.tar.gz/victron_ble2mqtt-0.7.2/victron_ble2mqtt/user_settings.py
@@ -0,0 +1,46 @@
+import dataclasses
+import sys
+
+from cli_base.systemd.data_classes import BaseSystemdServiceInfo, BaseSystemdServiceTemplateContext
+from ha_services.mqtt4homeassistant.data_classes import MqttSettings
+
+
+@dataclasses.dataclass
+class SystemdServiceTemplateContext(BaseSystemdServiceTemplateContext):
+    """
+    Context values for the systemd service file content.
+    """
+
+    verbose_service_name: str = 'victron-ble2mqtt'
+    exec_start: str = f'{sys.argv[0]} publish-loop'
+
+
+@dataclasses.dataclass
+class SystemdServiceInfo(BaseSystemdServiceInfo):
+    """
+    Information for systemd helper functions.
+    """
+
+    template_context: SystemdServiceTemplateContext = dataclasses.field(default_factory=SystemdServiceTemplateContext)
+
+
+@dataclasses.dataclass
+class UserSettings:
+    """
+    Victron-BLE -> MQTT - settings
+
+    Note: Insert at least our "device_keys" and your MQTT settings.
+
+    See README for more information.
+    """
+
+    device_name: str = 'Victron'
+    publish_throttle_seconds: int = 1  # Minimum time between publishing messages to MQTT, in seconds.
+
+    # Add device keys here:
+    device_keys: list[str] = dataclasses.field(default_factory=list)
+
+    # Information about the MQTT server:
+    mqtt: dataclasses = dataclasses.field(default_factory=MqttSettings)
+
+    systemd: dataclasses = dataclasses.field(default_factory=SystemdServiceInfo)
