fb4439e554f984cbfa3a12905cf051f2271d61ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 04:05:10 +0000

    Add package kubeflow/0.1.0/kubeflow-0.1.0-py3-none-any.whl

---
+++ packages/kubeflow/kubeflow-0.1.0-py3-none-any.whl/kubeflow/trainer/__init__.py
@@ -0,0 +1,62 @@
+# Copyright 2024 The Kubeflow Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# Import the Kubeflow Trainer client.
+from kubeflow.trainer.api.trainer_client import TrainerClient  # noqa: F401
+
+# import backends and its associated configs
+from kubeflow.trainer.backends.kubernetes.types import KubernetesBackendConfig
+from kubeflow.trainer.backends.localprocess.types import LocalProcessBackendConfig
+
+# Import the Kubeflow Trainer constants.
+from kubeflow.trainer.constants.constants import DATASET_PATH, MODEL_PATH  # noqa: F401
+
+# Import the Kubeflow Trainer types.
+from kubeflow.trainer.types.types import (
+    BuiltinTrainer,
+    CustomTrainer,
+    DataFormat,
+    DataType,
+    HuggingFaceDatasetInitializer,
+    HuggingFaceModelInitializer,
+    Initializer,
+    Loss,
+    Runtime,
+    RuntimeTrainer,
+    TorchTuneConfig,
+    TorchTuneInstructDataset,
+    TrainerType,
+)
+
+__all__ = [
+    "BuiltinTrainer",
+    "CustomTrainer",
+    "DataFormat",
+    "DATASET_PATH",
+    "DataType",
+    "HuggingFaceDatasetInitializer",
+    "HuggingFaceModelInitializer",
+    "Initializer",
+    "Loss",
+    "MODEL_PATH",
+    "Runtime",
+    "TorchTuneConfig",
+    "TorchTuneInstructDataset",
+    "RuntimeTrainer",
+    "TrainerClient",
+    "TrainerType",
+    "LocalProcessBackendConfig",
+    "KubernetesBackendConfig",
+]
