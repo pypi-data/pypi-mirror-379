719da9a6db4dbd06acbcab81c8ce25b0dafeeff6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 17:07:12 +0000

    Add package intugle/0.1.10/intugle-0.1.10.tar.gz

---
+++ packages/intugle/intugle-0.1.10.tar.gz/intugle-0.1.10/src/intugle/__init__.py
@@ -0,0 +1,3 @@
+from intugle.analysis.models import DataSet as DataSet
+from intugle.knowledge_builder import KnowledgeBuilder as KnowledgeBuilder
+from intugle.dp_builder import DataProductBuilder as DataProductBuilder
