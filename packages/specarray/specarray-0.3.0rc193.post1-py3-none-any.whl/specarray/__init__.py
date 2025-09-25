afec09902559217c4576ad47fb96762003bd88db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 05:04:56 +0000

    Add package specarray/0.3.0rc193.post1/specarray-0.3.0rc193.post1-py3-none-any.whl

---
+++ packages/specarray/specarray-0.3.0rc193.post1-py3-none-any.whl/specarray/__init__.py
@@ -0,0 +1,10 @@
+#   -------------------------------------------------------------
+#   Copyright (c) Microsoft Corporation. All rights reserved.
+#   Licensed under the MIT License. See LICENSE in project root for information.
+#   -------------------------------------------------------------
+"""Python Package Template"""
+from __future__ import annotations
+
+__version__ = "0.3.0""-rc193-post1"
+
+from .specarray import SpecArray
