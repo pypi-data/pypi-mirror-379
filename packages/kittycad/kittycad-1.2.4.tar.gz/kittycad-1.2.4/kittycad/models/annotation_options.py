65cbde9b86aa820e73e0b1d6d850cd3b5a8eb9a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 05:06:15 +0000

    Add package kittycad/1.2.4/kittycad-1.2.4.tar.gz

---
+++ packages/kittycad/kittycad-1.2.4.tar.gz/kittycad-1.2.4/kittycad/models/annotation_options.py
@@ -0,0 +1,21 @@
+from typing import Optional
+
+from ..models.annotation_line_end_options import AnnotationLineEndOptions
+from ..models.annotation_text_options import AnnotationTextOptions
+from ..models.color import Color
+from ..models.point3d import Point3d
+from .base import KittyCadBaseModel
+
+
+class AnnotationOptions(KittyCadBaseModel):
+    """Options for annotations"""
+
+    color: Optional[Color] = None
+
+    line_ends: Optional[AnnotationLineEndOptions] = None
+
+    line_width: Optional[float] = None
+
+    position: Optional[Point3d] = None
+
+    text: Optional[AnnotationTextOptions] = None
