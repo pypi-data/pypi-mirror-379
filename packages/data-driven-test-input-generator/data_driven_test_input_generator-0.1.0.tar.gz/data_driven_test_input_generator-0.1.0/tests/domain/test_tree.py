cbc82c5636de3f7427b6b24319a170ba35612de9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 04:05:44 +0000

    Add package data-driven-test-input-generator/0.1.0/data_driven_test_input_generator-0.1.0.tar.gz

---
+++ packages/data-driven-test-input-generator/data_driven_test_input_generator-0.1.0.tar.gz/data_driven_test_input_generator-0.1.0/tests/domain/test_tree.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import unittest
+import os
+import pickle
+import polars as pl
+from ddtig.domain import TestTree
+from ddtig.user_interface import SystemSpecsHandler
+
+class TestTestTree(unittest.TestCase):
+    
+    def setUp(self):
+        """ Setup:
+            Import Hoeffding Tree
+        """
+        dirpath = os.path.dirname(os.path.abspath(__file__))
+        hoeffding_file = os.path.join(dirpath, f"resources/Appliances_Hoeffding.pkl")
+        with open(hoeffding_file, "rb") as f:
+            hoeffding_tree = pickle.load(f)
+
+        dataset = pl.read_csv(os.path.join(dirpath, "resources/Appliances.csv"))
+        specs = SystemSpecsHandler(dataset, specs_file=None, logger=None)
+        self.testtree = TestTree(hoeffding_tree, specs, logger=None).test_tree
+        
+    def test_testtree(self):
+        """ Test:
+            1. Number of nodes
+            2. Number of leaves
+            3. Sample size
+        """
+
+        self.assertTrue((len(self.testtree) == 29), "Number of nodes is not correct.")
+
+        leaves = {leaf: leaf_info for leaf, leaf_info in self.testtree.items() if leaf_info.samples > 0}
+        self.assertTrue((len(leaves) == 15), "Number of leaves is not correct.")
+
+        samples = [leaf_info.samples for _, leaf_info in self.testtree.items() if leaf_info.samples > 0]
+        self.assertTrue((sum(samples) == 19735), "Sample size is not correct.")
+        
+
+if __name__ == '__main__':
+    unittest.main()
