f7c6d497da3713f22602d1511101ce6db8cd95c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 14:06:54 +0000

    Add package mensajes-jcorredor/5.1/mensajes_jcorredor-5.1-py3-none-any.whl

---
+++ packages/mensajes-jcorredor/mensajes_jcorredor-5.1-py3-none-any.whl/mensajes/adios/despedida.py
@@ -0,0 +1,6 @@
+def despedir():
+    print("Adiós, me despido desde despedida.despedir()")
+
+class Despedida:
+    def __init__(self):
+        print("Adiós, me despedido desde Despedida.__init__()")
