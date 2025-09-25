fe9f8d9225b28a49eccb2cc425c2e32e063a4d77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 07:05:56 +0000

    Add package autoxkit/0.2.1/autoxkit-0.2.1.tar.gz

---
+++ packages/autoxkit/autoxkit-0.2.1.tar.gz/autoxkit-0.2.1/autoxkit/__init__.py
@@ -0,0 +1,23 @@
+from .mousekey import (
+    MouseControl,
+
+    KeyBoardControl,
+
+    HookListener, KeyEvent, MouseEvent,
+
+    HotkeyListener
+)
+
+__all__ = [
+    # 鼠标控制
+    "MouseControl",
+
+    # 键盘控制
+    "KeyBoardControl",
+
+    # 鼠标键盘钩子
+    "HookListener", "KeyEvent", "MouseEvent",
+
+    # 热键监听器
+    "HotkeyListener",
+]
---
+++ packages/autoxkit/autoxkit-0.2.1.tar.gz/autoxkit-0.2.1/autoxkit/icmatch/__init__.py
@@ -0,0 +1,6 @@
+from .color_match import (get_screen_color, color_match)
+
+__all__ = [
+    # 颜色匹配相关函数
+    "get_screen_color", "color_match",
+]
