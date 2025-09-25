723ef4080205dd1b56b7651dee442e72130c804e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 03:18:11 +0000

    Add package marimo-base/0.16.2/marimo_base-0.16.2.tar.gz

---
+++ packages/marimo-base/marimo_base-0.16.2.tar.gz/marimo_base-0.16.2/marimo/_smoke_tests/anywidget_smoke_tests/mosaic_example.py
@@ -0,0 +1,57 @@
+# /// script
+# requires-python = ">=3.11"
+# dependencies = [
+#     "pandas",
+#     "mosaic-widget",
+#     "marimo",
+#     "pyyaml",
+# ]
+# ///
+# Copyright 2024 Marimo. All rights reserved.
+
+import marimo
+
+__generated_with = "0.15.5"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import pandas as pd
+    import marimo as mo
+    import os
+    import yaml
+
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+
+
+    from mosaic_widget import MosaicWidget
+
+    weather = pd.read_csv(
+        "https://uwdata.github.io/mosaic-datasets/data/seattle-weather.csv",
+        parse_dates=["date"],
+    )
+
+    # Load weather spec, remove data key to ensure load from Pandas
+    with open(dir_path + "/weather.yaml") as f:
+        spec = yaml.safe_load(f)
+        spec.pop("data")
+
+    w = mo.ui.anywidget(MosaicWidget(spec, data={"weather": weather}))
+    return (w,)
+
+
+@app.cell
+def _(w):
+    w
+    return
+
+
+@app.cell
+def _(w):
+    w.value
+    return
+
+
+if __name__ == "__main__":
+    app.run()
