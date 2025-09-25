e1361df34f9e61392bd03e66f2312295e7a983b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 06:06:57 +0000

    Add package Cirilla/0.1.3/cirilla-0.1.3.tar.gz

---
+++ packages/Cirilla/cirilla-0.1.3.tar.gz/cirilla-0.1.3/cirilla/Cirilla_model/__init__.py
@@ -0,0 +1,6 @@
+from .bert_model import CirillaBERT, BertArgs
+from .dataloader import JSONLDataset
+from .model import Cirilla, Args
+from .modules import benchmark_model_part
+from .tokenizer_modules import CirillaTokenizer
+from .training import TrainingArgs, CirillaTrainer
