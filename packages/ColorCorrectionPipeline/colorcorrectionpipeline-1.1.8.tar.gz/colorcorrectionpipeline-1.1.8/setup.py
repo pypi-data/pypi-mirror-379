077d6cdbd7d2504c6e1f5f59ece1b85042dc9173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 08:06:02 +0000

    Add package ColorCorrectionPipeline/1.1.8/colorcorrectionpipeline-1.1.8.tar.gz

---
+++ packages/ColorCorrectionPipeline/colorcorrectionpipeline-1.1.8.tar.gz/colorcorrectionpipeline-1.1.8/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.readlines()
+
+setuptools.setup(
+    name="ColorCorrectionPipeline",
+    version="1.1.8",
+    author="Collins Wakholi, Devin A. Rippner",
+    author_email="wcoln@yahoo.com, devinrippner@gmail.com",
+    description="A Stepwise color‐correction pipeline with flat‐field, gamma, white‐balance, and color‐correction stages.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/collinswakholi/ColorCorrectionPackage",
+    project_urls={
+        "Bug Tracker": "https://github.com/collinswakholi/ColorCorrectionPackage/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords=["color", "image-processing", "flat-field", "gamma-correction", "white-balance", "color-correction"],
+    packages=setuptools.find_packages(), # Automatically finds packages in 'src/'
+    python_requires=">=3.8",
+    install_requires=requirements,
+    include_package_data=True
+)
