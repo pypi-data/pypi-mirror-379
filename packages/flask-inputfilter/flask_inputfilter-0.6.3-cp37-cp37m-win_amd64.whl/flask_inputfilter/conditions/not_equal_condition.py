d3632af90ef1c678998620826bd20a7f1e27aef3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 06:06:13 +0000

    Add package flask-inputfilter/0.6.3/flask_inputfilter-0.6.3-cp37-cp37m-win_amd64.whl

---
+++ packages/flask-inputfilter/flask_inputfilter-0.6.3-cp37-cp37m-win_amd64.whl/flask_inputfilter/conditions/not_equal_condition.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+from typing import Any
+
+from flask_inputfilter.models import BaseCondition
+
+
+class NotEqualCondition(BaseCondition):
+    """
+    Checks if two specified fields are not equal.
+
+    **Parameters:**
+
+    - **first_field** (*str*): The first field to compare.
+    - **second_field** (*str*): The second field to compare.
+
+    **Expected Behavior:**
+
+    Validates that the values of ``first_field`` and ``second_field``
+    are not equal.
+
+    **Example Usage:**
+
+    .. code-block:: python
+
+        class DifferenceFilter(InputFilter):
+            def __init__(self):
+                super().__init__()
+
+                self.add(
+                    'field1'
+                )
+
+                self.add(
+                    'field2'
+                )
+
+                self.add_condition(NotEqualCondition('field1', 'field2'))
+    """
+
+    __slots__ = ("first_field", "second_field")
+
+    def __init__(self, first_field: str, second_field: str) -> None:
+        self.first_field = first_field
+        self.second_field = second_field
+
+    def check(self, data: dict[str, Any]) -> bool:
+        return data.get(self.first_field) != data.get(self.second_field)
