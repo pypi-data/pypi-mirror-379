30eaf94761ed2edcc17c0b161f482c8f21998d72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 03:17:43 +0000

    Add package soda-core-vertica/3.5.6/soda_core_vertica-3.5.6.tar.gz

---
+++ packages/soda-core-vertica/soda_core_vertica-3.5.6.tar.gz/soda_core_vertica-3.5.6/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import find_namespace_packages, setup
+
+package_name = "soda-core-vertica"
+package_version = "3.5.6"
+description = "Soda Core Vertica Package"
+
+requires = [f"soda-core=={package_version}", "vertica-python>=1.0.3, <2.0"]
+
+setup(
+    name=package_name,
+    version=package_version,
+    install_requires=requires,
+    packages=find_namespace_packages(include=["soda*"]),
+)
