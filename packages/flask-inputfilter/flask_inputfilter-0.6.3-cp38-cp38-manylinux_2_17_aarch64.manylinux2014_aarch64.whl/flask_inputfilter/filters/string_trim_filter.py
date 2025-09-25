cb9eb1ecdd8a5397457f6c81e98a97827d1029db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 06:06:14 +0000

    Add package flask-inputfilter/0.6.3/flask_inputfilter-0.6.3-cp38-cp38-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/flask-inputfilter/flask_inputfilter-0.6.3-cp38-cp38-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/flask_inputfilter/filters/string_trim_filter.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+from typing import Any, Union
+
+from flask_inputfilter.models import BaseFilter
+
+
+class StringTrimFilter(BaseFilter):
+    """
+    Removes leading and trailing whitespace from a string.
+
+    **Expected Behavior:**
+
+    If the input is a string, it returns the trimmed version. Otherwise,
+    the value remains unchanged.
+
+    **Example Usage:**
+
+    .. code-block:: python
+
+        class UserFilter(InputFilter):
+            def __init__(self):
+                super().__init__()
+
+                self.add('username', filters=[
+                    StringTrimFilter()
+                ])
+    """
+
+    __slots__ = ()
+
+    def apply(self, value: Any) -> Union[str, Any]:
+        return value.strip() if isinstance(value, str) else value
