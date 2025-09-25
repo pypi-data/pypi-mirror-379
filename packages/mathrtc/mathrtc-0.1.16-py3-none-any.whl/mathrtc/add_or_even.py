042ee4ed74b931ec747286a4b0524e33266d298d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 15:05:52 +0000

    Add package mathrtc/0.1.16/mathrtc-0.1.16-py3-none-any.whl

---
+++ packages/mathrtc/mathrtc-0.1.16-py3-none-any.whl/mathrtc/add_or_even.py
@@ -0,0 +1,16 @@
+# Odd or Even Program with Error Handling inside a Function
+
+def add_or_even():
+    try:
+        num = int(input("Enter a number: "))
+        
+        if num % 2 == 0:
+            print(f"{num} is Even")
+        else:
+            print(f"{num} is Odd")
+
+    except ValueError:
+        print("Invalid input! Please enter a valid integer.")
+
+# Call the function
+add_or_even()
