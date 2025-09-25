a35626c8769970492b3fad2af8a222c7aa20769b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 06:07:09 +0000

    Add package pytsql/1.5.2/pytsql-1.5.2-cp313-cp313-win32.whl

---
+++ packages/pytsql/pytsql-1.5.2-cp313-cp313-win32.whl/pytsql/grammar/helper_generate_grammar_targets.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -exuo pipefail
+
+ANTLR4_JAR_FILEPATH=$1
+OPTIONAL_DOWNLOAD_GRAMMAR=$2
+
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+antlr4="java -jar $ANTLR4_JAR_FILEPATH"
+
+# Optionally: Download latest grammar definition before generating grammar
+if [[ "$2" == "download" || "$2" == "-d" ]]; then
+  curl -o TSqlParser.g4 https://raw.githubusercontent.com/antlr/grammars-v4/master/sql/tsql/TSqlParser.g4
+  curl -o TSqlLexer.g4 https://raw.githubusercontent.com/antlr/grammars-v4/master/sql/tsql/TSqlLexer.g4
+fi
+
+# Adjust grammar for C++ target
+python adjust_antlrs_grammar.py rename-protected-rule-element-labels -f TSqlParser.g4
+
+# Generate C++ target with visitor
+$antlr4 -Dlanguage=Cpp -o cpp_src TSqlLexer.g4
+$antlr4 -Dlanguage=Cpp -visitor -no-listener -o cpp_src TSqlParser.g4
+
+# Generate Python target
+$antlr4 -Dlanguage=Python3 -o . TSqlLexer.g4
+$antlr4 -Dlanguage=Python3 -no-visitor -o . TSqlParser.g4
+
+# Run speedy-antlr-tool to generate parse accelerator
+python3 <<EOF
+from speedy_antlr_tool import generate
+
+generate(
+    py_parser_path="TSqlParser.py",
+    cpp_output_dir="cpp_src",
+    entry_rule_names=["tsql_file"],
+)
+EOF
+
+# Adjust auto-generated files for protected names on Windows
+python adjust_antlrs_grammar.py add-removal-of-specified-names-on-windows --filepath cpp_src/TSqlLexer.h
