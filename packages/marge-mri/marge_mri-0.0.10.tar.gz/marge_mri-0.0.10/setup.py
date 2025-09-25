42635600a08c73d87fe97110df9c088b18b1a320
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 15:06:17 +0000

    Add package marge-mri/0.0.10/marge_mri-0.0.10.tar.gz

---
+++ packages/marge-mri/marge_mri-0.0.10.tar.gz/marge_mri-0.0.10/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="marge-mri",
+    version="0.0.10",
+    packages=find_packages(),
+    install_requires=[],
+    author="José Miguel Algarín",
+    description="MaRCoS Graphical Environment (MaRGE)",
+)
+
