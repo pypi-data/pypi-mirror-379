7cac74a6c26d0afe46893e8f45c7d15d920f537d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 16:07:07 +0000

    Add package pyblebb/1.7/pyblebb-1.7-py3-none-any.whl

---
+++ packages/pyblebb/pyblebb-1.7-py3-none-any.whl/bitblock/motor.py
@@ -0,0 +1,28 @@
+from .bitblock import * 
+from .constants import *
+
+__all__ = [
+    "DCMotor",
+]
+
+class DCMotor():
+    def __init__(self, controler, pin=PIN.P16):
+        self.__controller = controler
+        self.__pin = pin
+         
+    def set(self, value):
+        command = NULL_COMMAND_PACKET[:]
+        command[BBPACKET.INDEX] = self.__controller.get_index()
+        command[BBPACKET.ACTION] = ACTION_CODE.ANALOG
+        command[BBPACKET.DATA0] = ACTION_MODE.ANALOG_OUTPUT
+        command[BBPACKET.DATA1] = self.__pin
+        # 0 ~ 1023
+        ah = (value >> 8) & 0xff      # 상위 바이트
+        al = value & 0xff
+        command[BBPACKET.DATA2] = al    # 펌웨어에서 readShort 함수를 사용할려면 상위와 하위를 조심
+        command[BBPACKET.DATA3] = ah
+        self.__controller.send(command)
+
+
+    
+
