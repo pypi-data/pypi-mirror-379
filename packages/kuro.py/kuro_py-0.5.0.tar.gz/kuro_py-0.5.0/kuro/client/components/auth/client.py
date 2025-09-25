c0cbe9e52d6a4e088262946accc807a76583eb64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 07:06:08 +0000

    Add package kuro.py/0.5.0/kuro_py-0.5.0.tar.gz

---
+++ packages/kuro.py/kuro_py-0.5.0.tar.gz/kuro_py-0.5.0/kuro/client/components/auth/client.py
@@ -0,0 +1,35 @@
+"""Auth component responsible for authentication."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from kuro.client.components.auth import subclients
+from kuro.utility import geetest
+
+if TYPE_CHECKING:
+    from kuro import models
+
+__all__ = ["AuthClient"]
+
+
+class AuthClient(subclients.GameAuthClient, subclients.WebAuthClient):
+    """Auth client."""
+
+    async def login(self, number: str, *, port: int = 5000) -> models.LoginResult:
+        """Login with a phone number.
+
+        ### Args:
+            number: Chinese phone number. With or without regional prefix (`+86`).
+            port: Port to use for geetest server.
+
+        ### Returns:
+            Login result.
+        """
+        success = await self._send_sms_code(number)
+        if not success:
+            mmt_result = await geetest.server.solve_geetest(lang=self.lang, port=port)
+            await self._send_sms_code(number, mmt_result=mmt_result)
+
+        code = input("Enter OTP code: ")  # noqa: ASYNC250
+        return await self._web_login(number, code)
---
+++ packages/kuro.py/kuro_py-0.5.0.tar.gz/kuro_py-0.5.0/kuro/client/components/auth/subclients/game.py
@@ -0,0 +1,163 @@
+"""Game auth client."""
+import typing
+
+from kuro import constants, errors, models, types
+from kuro.client import decorators, routes
+from kuro.client.components import base
+from kuro.utility import auth
+
+__all__ = ["GameAuthClient"]
+
+
+class GameAuthClient(base.BaseClient):
+    """Game auth client."""
+
+    @decorators.region_specific(types.Region.OVERSEAS)
+    async def game_login(self, email: str, password: str) -> models.GameLoginResult:
+        """Login with a username and password into game account.
+
+        ### Args:
+            email: Email (raw unencrypted string).
+            password: Password (raw unencrypted string).
+
+        ### Returns:
+            Login result.
+        """
+        data: dict[str, typing.Any] = {
+            "__e__": 1,
+            "email": email,
+            "client_id": "7rxmydkibzzsf12om5asjnoo",  # KR_PRODUCT_KEY in kr_sdk_config.json
+            "deviceNum": "A227C7BA-CB16-4E62-8355-BEABE3DE10F8",  # Can be any random string
+            "password": auth.encode_password(password),
+            "platform": "iOS",
+            "productId": "A1725",
+            "productKey": "01433708256c41838cda8ead20b64042",  # KR_PRODUCT_KEY in kr_sdk_config.json
+            "projectId": "G153",
+            "redirect_uri": 1,
+            "response_type": "code",
+            "sdkVersion": "1.9.8h",
+            "channelId": "171",
+        }
+        data["sign"] = auth.encode_md5_parameter(
+            data, constants.APP_KEYS[types.Game.WUWA][self.region]
+        )
+        rsp = await self.request(routes.GAME_LOGIN.get_url(), data=data)
+
+        if rsp["codes"] not in {0, None}:
+            errors.raise_from_data(rsp)
+
+        return models.GameLoginResult(**rsp)
+
+    @decorators.region_specific(types.Region.OVERSEAS)
+    async def get_game_token(self, code: str) -> models.GameTokenResult:
+        """Get game access token.
+
+        ### Args:
+            code: Authorization code.
+
+        ### Returns:
+            Game token result.
+        """
+        data: dict[str, typing.Any] = {
+            "client_id": "7rxmydkibzzsf12om5asjnoo",  # KR_PRODUCT_KEY in kr_sdk_config.json
+            "deviceNum": "A227C7BA-CB16-4E62-8355-BEABE3DE10F8",
+            "client_secret": "32gh5r0p35ullmxrzzwk40ly",
+            "code": code,
+            "productId": "A1725",
+            "projectId": "G153",
+            "redirect_uri": 1,
+            "grant_type": "authorization_code",
+        }
+        data["sign"] = auth.encode_md5_parameter(
+            data, constants.APP_KEYS[types.Game.WUWA][self.region]
+        )
+        rsp = await self.request(routes.GAME_TOKEN.get_url(), data=data)
+
+        if rsp["codes"] not in {0, None}:
+            errors.raise_from_data(rsp)
+
+        return models.GameTokenResult(**rsp)
+
+    @decorators.region_specific(types.Region.OVERSEAS)
+    async def check_game_token(self, access_token: str) -> int:
+        """Check game access token life time.
+
+        ### Args:
+            access_token: Game access token.
+
+        ### Returns:
+            Remaining life time of access token in seconds.
+        """
+        data = {
+            "deviceNum": "A227C7BA-CB16-4E62-8355-BEABE3DE10F8",
+            "access_token": access_token,
+            "productId": "A1725",
+            "projectId": "G153",
+            "channelId": "171",
+        }
+        data["sign"] = auth.encode_md5_parameter(
+            data, constants.APP_KEYS[types.Game.WUWA][self.region]
+        )
+        rsp = await self.request(routes.GAME_TOKEN_CHECK.get_url(), data=data)
+
+        if rsp["codes"] not in {0, None}:
+            errors.raise_from_data(rsp)
+
+        return rsp["expireSec"]
+
+    @decorators.region_specific(types.Region.OVERSEAS)
+    async def game_auto_login(self, auto_token: str) -> models.GameLoginResult:
+        """Login with an auto token.
+
+        ### Args:
+            auto_token: Auto login token.
+
+        ### Returns:
+            Login result.
+        """
+        data: dict[str, typing.Any] = {
+            "token": auto_token,
+            "client_id": "7rxmydkibzzsf12om5asjnoo",  # KR_PRODUCT_KEY in kr_sdk_config.json
+            "deviceNum": "A227C7BA-CB16-4E62-8355-BEABE3DE10F8",
+            "productId": "A1725",
+            "projectId": "G153",
+            "redirect_uri": 1,
+            "response_type": "code",
+            "channelId": "171",
+        }
+        data["sign"] = auth.encode_md5_parameter(
+            data, constants.APP_KEYS[types.Game.WUWA][self.region]
+        )
+        rsp = await self.request(routes.GAME_AUTO_LOGIN.get_url(), data=data)
+
+        if rsp["codes"] not in {0, None}:
+            errors.raise_from_data(rsp)
+
+        return models.GameLoginResult(**rsp)
+
+    @decorators.region_specific(types.Region.OVERSEAS)
+    async def generate_oauth_code(self, game_token: str) -> str:
+        """Generate OAuth code from game token.
+
+        ### Args:
+            game_token: Game access token.
+
+        ### Returns:
+            The generated OAuth code.
+        """
+        data: dict[str, int | str] = {
+            "client_id": "7rxmydkibzzsf12om5asjnoo",  # KR_PRODUCT_KEY in kr_sdk_config.json
+            "deviceNum": "A227C7BA-CB16-4E62-8355-BEABE3DE10F8",
+            "client_secret": "32gh5r0p35ullmxrzzwk40ly",
+            "access_token": game_token,
+            "productId": "A1725",
+            "projectId": "G153",
+            "redirect_uri": 1,
+            "scope": "launcher",
+        }
+        rsp = await self.request(routes.OAUTH_CODE.get_url(), data=data)
+
+        if rsp["codes"] not in {0, None}:
+            errors.raise_from_data(rsp)
+
+        return rsp["oauthCode"]
