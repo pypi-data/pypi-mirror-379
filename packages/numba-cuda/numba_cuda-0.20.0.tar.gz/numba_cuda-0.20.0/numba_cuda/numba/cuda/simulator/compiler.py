dbfeca50b44b90310275451e4fc965a0fcb5e497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 05:05:18 +0000

    Add package numba-cuda/0.20.0/numba_cuda-0.20.0.tar.gz

---
+++ packages/numba-cuda/numba_cuda-0.20.0.tar.gz/numba_cuda-0.20.0/numba_cuda/numba/cuda/simulator/compiler.py
@@ -0,0 +1,17 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: BSD-2-Clause
+
+"""
+The compiler is not implemented in the simulator. This module provides a stub
+to allow tests to import successfully.
+"""
+
+compile = None
+compile_for_current_device = None
+compile_ptx = None
+compile_ptx_for_current_device = None
+declare_device_function = None
+
+
+def run_frontend(func, inline_closures=False, emit_dels=False):
+    pass
