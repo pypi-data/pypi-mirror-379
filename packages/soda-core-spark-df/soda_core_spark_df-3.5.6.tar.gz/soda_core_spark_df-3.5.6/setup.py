66be05a31d0339b0d90aaecca09c74de30f6eb4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 03:17:43 +0000

    Add package soda-core-spark-df/3.5.6/soda_core_spark_df-3.5.6.tar.gz

---
+++ packages/soda-core-spark-df/soda_core_spark_df-3.5.6.tar.gz/soda_core_spark_df-3.5.6/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import find_namespace_packages, setup
+
+package_name = "soda-core-spark-df"
+package_version = "3.5.6"
+description = "Soda Core Spark Dataframe Package"
+
+requires = [
+    f"soda-core-spark=={package_version}",
+    "pyspark>=3.4.0,<4.0.0",  # 4.0.0 has breaking changes, drops java 11 support. Revisit as needed.",
+]
+# TODO Fix the params
+setup(
+    name=package_name,
+    version=package_version,
+    install_requires=requires,
+    packages=find_namespace_packages(include=["soda*"]),
+)
