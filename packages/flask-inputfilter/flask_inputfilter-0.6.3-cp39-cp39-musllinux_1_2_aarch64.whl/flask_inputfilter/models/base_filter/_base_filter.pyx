1a3b4e0a61739da1160ab1aef8e07eadc5abab7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 06:06:28 +0000

    Add package flask-inputfilter/0.6.3/flask_inputfilter-0.6.3-cp39-cp39-musllinux_1_2_aarch64.whl

---
+++ packages/flask-inputfilter/flask_inputfilter-0.6.3-cp39-cp39-musllinux_1_2_aarch64.whl/flask_inputfilter/models/base_filter/_base_filter.pyx
@@ -0,0 +1,23 @@
+# cython: language=c++
+
+cdef class BaseFilter:
+    """
+    BaseFilter-Class.
+
+    Every filter should inherit from it.
+    """
+
+    cpdef object apply(self, object value):
+        """
+        Apply the filter to the given value.
+
+        Args:
+            value: The value to apply the filter to.
+
+        Returns:
+            The filtered value.
+
+        Raises:
+            NotImplementedError: If the method is not implemented by subclasses.
+        """
+        raise NotImplementedError("Subclasses must implement the apply method")
