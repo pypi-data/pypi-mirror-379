a3286a522eedabbb7b3178737efc990e3c9cbebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 16:07:21 +0000

    Add package pyredis-manager/0.1.3/pyredis_manager-0.1.3.tar.gz

---
+++ packages/pyredis-manager/pyredis_manager-0.1.3.tar.gz/pyredis_manager-0.1.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="pyredis_manager",
+    version="0.1.3",
+    author="jack-cizon",
+    author_email="jack20021213cn@gmail.com",
+    description="A lightweight Redis client manager with sync/async support",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jackcizon/pyredis_manager",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    install_requires=[
+        "redis>=4.3.6",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+)
