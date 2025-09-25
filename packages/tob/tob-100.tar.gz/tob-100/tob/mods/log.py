ae5c306557a4ae28c9775513d8159ef3bf110a67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 14:06:52 +0000

    Add package tob/100/tob-100.tar.gz

---
+++ packages/tob/tob-100.tar.gz/tob-100/tob/mods/log.py
@@ -0,0 +1,35 @@
+# This file is placed in the Public Domain.
+
+
+"log text"
+
+
+import time
+
+
+from ..disk   import find, write
+from ..object import Object
+from ..utils  import elapsed, fntime
+
+
+class Log(Object):
+
+    def __init__(self):
+        super().__init__()
+        self.txt = ''
+
+
+def log(event):
+    if not event.rest:
+        nmr = 0
+        for fnm, obj in find('log'):
+            lap = elapsed(time.time() - fntime(fnm))
+            event.reply(f'{nmr} {obj.txt} {lap}')
+            nmr += 1
+        if not nmr:
+            event.reply('no log')
+        return
+    obj = Log()
+    obj.txt = event.rest
+    write(obj)
+    event.done()
---
+++ packages/tob/tob-100.tar.gz/tob-100/tob/mods/tbl.py
@@ -0,0 +1,45 @@
+# This file is placed in the Public Domain.
+
+
+"lookup tables"
+
+
+NAMES = {
+    "cfg": "irc",
+    "dne": "tdo",
+    "dpl": "rss",
+    "eml": "mbx",
+    "exp": "rss",
+    "flt": "flt",
+    "fnd": "fnd",
+    "imp": "rss",
+    "log": "log",
+    "ls": "lst",
+    "mbx": "mbx",
+    "mre": "irc",
+    "nme": "rss",
+    "pwd": "irc",
+    "rem": "rss",
+    "res": "rss",
+    "rss": "rss",
+    "syn": "rss",
+    "tdo": "tdo",
+    "thr": "thr",
+    "tmr": "tmr",
+    "upt": "upt"
+}
+
+MD5 = {
+    "flt": "123a0a6dbd005fdfa30466e121453bfd",
+    "fnd": "c295c5bd0ccb6916593f60e9099f76a1",
+    "irc": "f81889ff46708efe053e5460971ebd39",
+    "log": "fad2dc46758daa45c44354eee63041b5",
+    "lst": "7048f50ce66ab547552b4ee4fd250132",
+    "mbx": "3b19a2629870964e2d80948d8b9f4d5b",
+    "rss": "0016b3801d07d19504b1ffac0c96d49d",
+    "tbl": "d41d8cd98f00b204e9800998ecf8427e",
+    "tdo": "045fb38dfcf5154d64a29457b92e6b05",
+    "thr": "bb2ec6fc78468d320b2354499dff2e74",
+    "tmr": "c623a4f766cbac06484e52ae100fff6e",
+    "upt": "bb9b9d8d3bf62381a185ff7115db1c00",
+}
