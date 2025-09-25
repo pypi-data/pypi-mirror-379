58958868e02cfc6d8b8372527bfdf6db0a524760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 06:07:01 +0000

    Add package pyfaust/3.42.1a3/pyfaust-3.42.1a3-cp312-cp312-win_amd64.whl

---
+++ packages/pyfaust/pyfaust-3.42.1a3-cp312-cp312-win_amd64.whl/pyfaust/tests/run.py
@@ -0,0 +1,37 @@
+import unittest
+from pyfaust.tests.TestFaust import TestFaust
+from pyfaust.tests.TestPoly import TestPoly
+import sys
+
+dev = 'cpu'
+field = 'real'
+
+if __name__ == "__main__":
+    nargs = len(sys.argv)
+    if(nargs > 1):
+        dev = sys.argv[1]
+        if dev != 'cpu' and not dev.startswith('gpu'):
+            raise ValueError("dev argument must be cpu or gpu.")
+        if(nargs > 2):
+            field = sys.argv[2]
+            if field not in ['complex', 'real', 'float32', 'float64', 'double']:
+                raise ValueError("field must be 'complex', 'real', 'float32', 'float64', 'double'")
+        del sys.argv[2]  # deleted to avoid interfering with unittest
+        del sys.argv[1]
+    if(nargs > 1):
+        # it remains a method fully qualified method name to test
+        # e.g. TestFaust.test_transpose
+        class_name, meth_name = sys.argv[1].split('.')[:]
+        testloader = unittest.TestLoader()
+        test_names = eval("testloader.getTestCaseNames("+class_name+")")
+        if meth_name in test_names:
+            test = eval(""+class_name+"('"+meth_name+"', dev=dev, field=field)")
+        else:
+            raise ValueError(meth_name +" is not in "+class_name)
+        suite = unittest.TestSuite()
+        suite.addTest(test)
+        runner = unittest.TextTestRunner()
+        runner.run(suite)
+    else:
+        # run all tests
+        unittest.main()
