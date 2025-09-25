559b4f090955eadd2a22dbc98392d7b2d73ef8eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 06:06:07 +0000

    Add package odoo-addon-mgmtsystem-hazard/16.0.1.2.0/odoo_addon_mgmtsystem_hazard-16.0.1.2.0-py3-none-any.whl

---
+++ packages/odoo-addon-mgmtsystem-hazard/odoo_addon_mgmtsystem_hazard-16.0.1.2.0-py3-none-any.whl/odoo/addons/mgmtsystem_hazard/models/mgmtsystem_hazard_hazard.py
@@ -0,0 +1,16 @@
+# Copyright (C) 2010 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+# This model is deprecated and must be removed in future versions.
+class MgmtsystemHazardHazard(models.Model):
+    _name = "mgmtsystem.hazard.hazard"
+    _description = "Hazard"
+
+    company_id = fields.Many2one(
+        "res.company", "Company", required=True, default=lambda self: self.env.company
+    )
+    name = fields.Char("Hazard", required=True, translate=True)
+    description = fields.Text(translate=True)
