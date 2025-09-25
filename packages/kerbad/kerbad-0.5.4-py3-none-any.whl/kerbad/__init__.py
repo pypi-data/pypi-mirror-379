af97396490f13fac474a0d20a8a51ae826be1656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 06:06:20 +0000

    Add package kerbad/0.5.4/kerbad-0.5.4-py3-none-any.whl

---
+++ packages/kerbad/kerbad-0.5.4-py3-none-any.whl/kerbad/__init__.py
@@ -0,0 +1,22 @@
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+def getLogger():
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    class SymbolFormatter(logging.Formatter):
+        LEVEL_SYMBOLS = {
+            logging.DEBUG: '[*]',
+            logging.INFO: '[+]',
+            logging.WARNING: '[!]',
+            logging.ERROR: '[-]',
+            logging.CRITICAL: '[X]',
+        }
+        def format(self, record):
+            symbol = self.LEVEL_SYMBOLS.get(record.levelno, '[?]')
+            return f"{symbol} {record.getMessage()}"
+    handler.setFormatter(SymbolFormatter())
+    logger.addHandler(handler)
+    return logger
