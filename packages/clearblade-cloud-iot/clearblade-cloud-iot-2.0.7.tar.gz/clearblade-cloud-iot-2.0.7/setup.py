ef3c6dd8c78c193230e33b9058199a9535aa13ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 03:17:47 +0000

    Add package clearblade-cloud-iot/2.0.7/clearblade-cloud-iot-2.0.7.tar.gz

---
+++ packages/clearblade-cloud-iot/clearblade-cloud-iot-2.0.7.tar.gz/clearblade-cloud-iot-2.0.7/setup.py
@@ -0,0 +1,84 @@
+# Copyright 2023 ClearBlade Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import io
+import os
+
+import setuptools
+
+name = "clearblade-cloud-iot"
+description = "Cloud IoT API client library"
+version = "2.0.7"
+release_status = "Development Status :: 5 - Production/Stable"
+dependencies = ["httpx", "proto-plus"]
+
+package_root = os.path.abspath(os.path.dirname(__file__))
+
+readme_filename = os.path.join(package_root, "README.rst")
+with io.open(readme_filename, encoding="utf-8") as readme_file:
+    readme = readme_file.read()
+
+packages = [
+    package
+    for package in setuptools.PEP420PackageFinder.find()
+    if package.startswith("clearblade")
+]
+
+namespaces = ["clearblade"]
+if "clearblade.cloud" in packages:
+    namespaces.append("clearblade.cloud")
+
+setuptools.setup(
+    name=name,
+    version=version,
+    description=description,
+    long_description=readme,
+    author="Clearblade",
+    author_email="info@clearblade.com",
+    license="Apache 2.0",
+    url="https://github.com/clearblade/python-iot",
+    classifiers=[
+        release_status,
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+        "Topic :: Internet",
+    ],
+    platforms="Posix; MacOS X; Windows",
+    packages=packages,
+    namespace_packages=namespaces,
+    install_requires=dependencies,
+    python_requires=">=3.7",
+    include_package_data=True,
+    zip_safe=False,
+)
