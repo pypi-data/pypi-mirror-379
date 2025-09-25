d9727e39a5a6db6f7a3c145b4efd20df653bedaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 03:18:45 +0000

    Add package avocado-framework/112.0/avocado_framework-112.0.tar.gz

---
+++ packages/avocado-framework/avocado_framework-112.0.tar.gz/avocado_framework-112.0/avocado/utils/wait.py
@@ -0,0 +1,40 @@
+import logging
+import time
+
+LOG = logging.getLogger(__name__)
+
+
+# pylint: disable=R0913
+def wait_for(func, timeout, first=0.0, step=1.0, text=None, args=None, kwargs=None):
+    """
+    Wait until func() evaluates to True.
+
+    If func() evaluates to True before timeout expires, return the
+    value of func(). Otherwise return None.
+
+    :param timeout: Timeout in seconds
+    :param first: Time to sleep before first attempt
+    :param step: Time to sleep between attempts in seconds
+    :param text: Text to print while waiting, for debug purposes
+    :param args: Positional arguments to func
+    :param kwargs: Keyword arguments to func
+    """
+    args = args or []
+    kwargs = kwargs or {}
+
+    start_time = time.monotonic()
+    end_time = start_time + timeout
+
+    time.sleep(first)
+
+    while time.monotonic() < end_time:
+        if text:
+            LOG.debug("%s (%.9f secs)", text, (time.monotonic() - start_time))
+
+        output = func(*args, **kwargs)
+        if output:
+            return output
+
+        time.sleep(step)
+
+    return None
