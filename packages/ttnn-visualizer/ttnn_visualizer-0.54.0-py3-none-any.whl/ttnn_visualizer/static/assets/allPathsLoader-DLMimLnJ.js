4742e40cc5542b0db79100bef51bd0b850ef2235
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 06:06:00 +0000

    Add package ttnn-visualizer/0.54.0/ttnn_visualizer-0.54.0-py3-none-any.whl

---
+++ packages/ttnn-visualizer/ttnn_visualizer-0.54.0-py3-none-any.whl/ttnn_visualizer/static/assets/allPathsLoader-DLMimLnJ.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/allPaths-ClJTDoXf.js","assets/index-CnPrfHYh.js","assets/index-Cnc1EkDo.js","assets/index-eW1dMfMf.js","assets/index-DdmYzvVg.css"])))=>i.map(i=>d[i]);
+import{_ as e}from"./index-eW1dMfMf.js";const s=async(t,a)=>{const{getIconPaths:o}=await e(async()=>{const{getIconPaths:r}=await import("./allPaths-ClJTDoXf.js");return{getIconPaths:r}},__vite__mapDeps([0,1,2,3,4]));return o(t,a)};export{s as allPathsLoader};
