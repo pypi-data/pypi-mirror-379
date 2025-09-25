9f3540f7dccd27e519f8428712e6302dd2de3686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 16:07:05 +0000

    Add package snanpy/2.2.10/snanpy-2.2.10.tar.gz

---
+++ packages/snanpy/snanpy-2.2.10.tar.gz/snanpy-2.2.10/src/snanpy/functions.py
@@ -0,0 +1,27 @@
+from matplotlib import pyplot as plt
+import matplotlib as mpl
+
+mpl.rcParams.update(mpl.rcParamsDefault)
+plt.rcParams.update({'font.size': 19})
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+
+def figax(figsize=(8,6), xlim=None, ylim=None, xlabel=None, ylabel=None, title=None):
+    fig, ax = plt.subplots(figsize=figsize, dpi=200, constrained_layout=True)
+    ax.grid(linewidth=0.5) 
+    if xlim is not None:
+        ax.set_xlim(xlim)  
+    if ylim is not None:
+        ax.set_ylim(ylim) 
+    ax.set_title(title)
+    ax.set_ylabel(ylabel)
+    ax.set_xlabel(xlabel) 
+
+    return fig, ax
+
+def snadd(x,y):
+    return x+y
+
+def snantest():
+    print('Hi')
+    return
