c6bec04316d30dd0887b4d60f113dae9f174e1d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 03:18:25 +0000

    Add package scorebook/0.0.9/scorebook-0.0.9-py3-none-any.whl

---
+++ packages/scorebook/scorebook-0.0.9-py3-none-any.whl/scorebook/__init__.py
@@ -0,0 +1,18 @@
+"""
+Scorebook package.
+
+A Python project for scorebook functionality.
+"""
+
+import importlib.metadata
+
+# get version from pyproject.toml
+__version__ = importlib.metadata.version(__package__ or __name__)
+
+from scorebook.eval_dataset import EvalDataset
+from scorebook.evaluate import evaluate
+from scorebook.inference_pipeline import InferencePipeline
+from scorebook.trismik_services.login import login, whoami
+from scorebook.utils.build_prompt import build_prompt
+
+__all__ = ["EvalDataset", "evaluate", "build_prompt", "login", "whoami", "InferencePipeline"]
