5caf45fea5b8317d76adfcc013fd411f601b7b7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 03:17:54 +0000

    Add package togura/0.1.0a2/togura-0.1.0a2-py3-none-any.whl

---
+++ packages/togura/togura-0.1.0a2-py3-none-any.whl/togura/config.py
@@ -0,0 +1,27 @@
+import os
+import yaml
+from pathlib import Path
+
+
+class Config(object):
+    def __init__(self):
+        file = f"{Path.cwd()}/config.yaml"
+        if os.path.isfile(file):
+            with open(file, encoding="utf-8") as f:
+                self.entry = yaml.safe_load(f)
+
+    @property
+    def base_url(self):
+        return self.entry["base_url"]
+
+    @property
+    def site_name(self):
+        return self.entry["site_name"]
+
+    @property
+    def organization(self):
+        return self.entry["organization"]
+
+    @property
+    def jalc_site_id(self):
+        return self.entry["jalc_site_id"]
