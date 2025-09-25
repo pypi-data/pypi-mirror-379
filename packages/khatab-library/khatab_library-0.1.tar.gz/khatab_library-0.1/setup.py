a8b5d89dbfc3d9620bcf991aab32dfadc8bbead8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 06:06:54 +0000

    Add package khatab-library/0.1/khatab_library-0.1.tar.gz

---
+++ packages/khatab-library/khatab_library-0.1.tar.gz/khatab_library-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='khatab_library',          # اسم المكتبة
+    version='0.1',                  # الإصدار
+    packages=find_packages(),
+    description='مكتبة بسيطة تعرض رسالة ترحيب باسم خطاب',
+    author='Khatab',
+    author_email='example@email.com',
+    url='https://github.com/Khatab/khatab_library',
+    install_requires=[],
+)
