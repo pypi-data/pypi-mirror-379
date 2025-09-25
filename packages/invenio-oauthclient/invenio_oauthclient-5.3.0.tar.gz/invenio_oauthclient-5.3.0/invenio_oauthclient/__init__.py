72fc9a6cb4cb8ffb627dddf60ebfc3528dd594dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 04:06:03 +0000

    Add package invenio-oauthclient/5.3.0/invenio_oauthclient-5.3.0.tar.gz

---
+++ packages/invenio-oauthclient/invenio_oauthclient-5.3.0.tar.gz/invenio_oauthclient-5.3.0/invenio_oauthclient/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2015-2025 CERN.
+# Copyright (C) 2024 Graz University of Technology.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Advanced usage docs."""
+
+from .ext import InvenioOAuthClient, InvenioOAuthClientREST
+from .oauth import oauth_link_external_id, oauth_unlink_external_id
+from .proxies import current_oauthclient
+
+__version__ = "5.3.0"
+
+__all__ = (
+    "__version__",
+    "current_oauthclient",
+    "InvenioOAuthClient",
+    "InvenioOAuthClientREST",
+    "oauth_link_external_id",
+    "oauth_unlink_external_id",
+)
