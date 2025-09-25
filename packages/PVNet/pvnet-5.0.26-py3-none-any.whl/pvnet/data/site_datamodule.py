252e598d358b03d83f0e78221544b649efc92c9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 03:18:10 +0000

    Add package PVNet/5.0.26/pvnet-5.0.26-py3-none-any.whl

---
+++ packages/PVNet/pvnet-5.0.26-py3-none-any.whl/pvnet/data/site_datamodule.py
@@ -0,0 +1,29 @@
+""" Data module for pytorch lightning """
+
+from ocf_data_sampler.torch_datasets.datasets.site import SitesDataset
+from ocf_data_sampler.torch_datasets.sample.site import SiteSample
+from torch.utils.data import Dataset
+
+from pvnet.data.base_datamodule import (
+    BasePresavedDataModule,
+    BaseStreamedDataModule,
+    PresavedSamplesDataset,
+)
+
+
+class SitePresavedDataModule(BasePresavedDataModule):
+    """Datamodule for loading pre-saved samples."""
+
+    def _get_premade_samples_dataset(self, subdir: str) -> Dataset:
+        return PresavedSamplesDataset(f"{self.sample_dir}/{subdir}", SiteSample)
+
+
+class SiteStreamedDataModule(BaseStreamedDataModule):
+    """Datamodule which streams samples using sampler for ocf-data-sampler."""
+
+    def _get_streamed_samples_dataset(
+        self,
+        start_time: str | None,
+        end_time: str | None
+    ) -> Dataset:
+        return SitesDataset(self.configuration, start_time=start_time, end_time=end_time)
---
+++ packages/PVNet/pvnet-5.0.26-py3-none-any.whl/pvnet/data/uk_regional_datamodule.py
@@ -0,0 +1,29 @@
+""" Data module for pytorch lightning """
+
+from ocf_data_sampler.torch_datasets.datasets.pvnet_uk import PVNetUKRegionalDataset
+from ocf_data_sampler.torch_datasets.sample.uk_regional import UKRegionalSample
+from torch.utils.data import Dataset
+
+from pvnet.data.base_datamodule import (
+    BasePresavedDataModule,
+    BaseStreamedDataModule,
+    PresavedSamplesDataset,
+)
+
+
+class UKRegionalPresavedDataModule(BasePresavedDataModule):
+    """Datamodule for loading pre-saved samples."""
+
+    def _get_premade_samples_dataset(self, subdir: str) -> Dataset:
+        return PresavedSamplesDataset(f"{self.sample_dir}/{subdir}", UKRegionalSample)
+
+
+class UKRegionalStreamedDataModule(BaseStreamedDataModule):
+    """Datamodule which streams samples using sampler for ocf-data-sampler."""
+
+    def _get_streamed_samples_dataset(
+        self,
+        start_time: str | None,
+        end_time: str | None
+    ) -> Dataset:
+        return PVNetUKRegionalDataset(self.configuration, start_time=start_time, end_time=end_time)
