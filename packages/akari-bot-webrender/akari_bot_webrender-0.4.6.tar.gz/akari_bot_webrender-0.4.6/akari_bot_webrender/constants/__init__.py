c10b9cd5c377050f3de0d147c856a0c85a72ac4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 15:06:06 +0000

    Add package akari-bot-webrender/0.4.6/akari_bot_webrender-0.4.6.tar.gz

---
+++ packages/akari-bot-webrender/akari_bot_webrender-0.4.6.tar.gz/akari_bot_webrender-0.4.6/akari_bot_webrender/constants/__init__.py
@@ -0,0 +1,30 @@
+import os
+
+elements_to_disable = [".notifications-placeholder",
+                       ".top-ads-container",
+                       ".fandom-sticky-header",
+                       "div#WikiaBar",
+                       "aside.page__right-rail",
+                       ".n-modal-container",
+                       "div#moe-float-toc-container",
+                       "div#moe-draw-float-button",
+                       "div#moe-global-header",
+                       ".mys-wrapper",
+                       "div#moe-open-in-app",
+                       "div#age-gate",
+                       ".va-variant-prompt",
+                       ".va-variant-prompt-mobile",
+                       ".mw-cookiewarning-container",
+                       "div#summerNotificationOverlay"
+                       ]
+
+
+templates_path = os.path.abspath(os.path.join(
+    os.path.dirname(__file__), "../templates"))
+
+base_user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36"
+
+base_width = 720
+base_height = 1280
+
+max_screenshot_height = 8192
