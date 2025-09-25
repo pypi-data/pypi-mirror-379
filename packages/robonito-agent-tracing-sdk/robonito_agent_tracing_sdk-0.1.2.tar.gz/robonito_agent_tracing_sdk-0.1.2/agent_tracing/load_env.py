83367a0c16bf78a0fe1d4d584340dacfe9116904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 03:17:55 +0000

    Add package robonito-agent-tracing-sdk/0.1.2/robonito_agent_tracing_sdk-0.1.2.tar.gz

---
+++ packages/robonito-agent-tracing-sdk/robonito_agent_tracing_sdk-0.1.2.tar.gz/robonito_agent_tracing_sdk-0.1.2/agent_tracing/load_env.py
@@ -0,0 +1,11 @@
+import os
+
+def load_env(file_path=".env"):
+    with open(file_path) as f:
+        for line in f:
+            line = line.strip()
+            if not line or line.startswith("#"):
+                continue
+            key, value = line.split("=", 1)
+            os.environ[key] = value
+
