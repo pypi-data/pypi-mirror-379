ae340ce2ba9e7a2f7b904d4fdfe8643459ff9b80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 06:06:23 +0000

    Add package qrgb/1.0.2/qrgb-1.0.2.tar.gz

---
+++ packages/qrgb/qrgb-1.0.2.tar.gz/qrgb-1.0.2/main.py
@@ -0,0 +1,5 @@
+from qrgb import app
+
+
+if __name__ == "__main__":
+    app()
---
+++ packages/qrgb/qrgb-1.0.2.tar.gz/qrgb-1.0.2/src/qrgb/cli.py
@@ -0,0 +1,7 @@
+"""CLI interface for QRGB."""
+
+# Move your main CLI code here, importing from core.py
+from .qrgb import app
+
+if __name__ == "__main__":
+    app()
