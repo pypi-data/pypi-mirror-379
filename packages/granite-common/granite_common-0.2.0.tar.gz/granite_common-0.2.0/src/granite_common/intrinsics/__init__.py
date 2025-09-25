3fb8224ed5ad2f8326edb3d789b5b4c98943c93c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 14:07:15 +0000

    Add package granite-common/0.2.0/granite_common-0.2.0.tar.gz

---
+++ packages/granite-common/granite_common-0.2.0.tar.gz/granite_common-0.2.0/src/granite_common/intrinsics/__init__.py
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: Apache-2.0
+
+__doc__ = """
+Support for the intrinsics in the RAG Agent Library.
+"""
+
+# Local
+from .input import RagAgentLibRewriter
+from .output import RagAgentLibResultProcessor
+
+__all__ = ("RagAgentLibRewriter", "RagAgentLibResultProcessor")
