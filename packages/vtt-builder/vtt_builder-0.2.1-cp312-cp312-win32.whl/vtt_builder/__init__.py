dbf1c1ca54666f5c78dc08703c50c8a25d1d3809
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 15:05:44 +0000

    Add package vtt-builder/0.2.1/vtt_builder-0.2.1-cp312-cp312-win32.whl

---
+++ packages/vtt-builder/vtt_builder-0.2.1-cp312-cp312-win32.whl/vtt_builder/__init__.py
@@ -0,0 +1,13 @@
+from vtt_builder._lowlevel import (
+    build_transcript_from_json_files,
+    build_vtt_from_json_files,
+    build_vtt_from_records,
+    validate_vtt_file,
+)
+
+__all__ = [
+    "build_vtt_from_records",
+    "build_transcript_from_json_files",
+    "build_vtt_from_json_files",
+    "validate_vtt_file",
+]
