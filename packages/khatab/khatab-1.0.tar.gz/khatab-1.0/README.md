3ea836b5fc37b5718b0fdba933d5b236ad0cf8a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-25 06:06:54 +0000

    Add package khatab/1.0/khatab-1.0.tar.gz

---
+++ packages/khatab/khatab-1.0.tar.gz/khatab-1.0/README.md
@@ -0,0 +1,8 @@
+# khatab Library
+
+هذه مكتبة بسيطة بلغة بايثون تقوم بعرض رسالة ترحيب باسم خطاب العصري.
+
+## الاستخدام
+```python
+import khatab
+print(khatab.welcome_message())
---
+++ packages/khatab/khatab-1.0.tar.gz/khatab-1.0/setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+setup(
+    name="khatab",
+    version="1.0",
+    py_modules=["khatab"],
+    install_requires=[],
+    description="مكتبة بسيطة لعرض رسالة ترحيب باسم خطاب العصري",
+    author="خطاب العصري",
+    author_email="example@email.com",
+    url="https://github.com/yourusername/khatab",
+)
