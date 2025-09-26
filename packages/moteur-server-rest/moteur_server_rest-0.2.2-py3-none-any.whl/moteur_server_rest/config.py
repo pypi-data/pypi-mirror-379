6000aba15144e297e45fc614ab1f5afc3315fee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 05:06:36 +0000

    Add package moteur-server-rest/0.2.2/moteur_server_rest-0.2.2-py3-none-any.whl

---
+++ packages/moteur-server-rest/moteur_server_rest-0.2.2-py3-none-any.whl/moteur_server_rest/config.py
@@ -0,0 +1,12 @@
+import os
+
+def get_env_variable(var_name, default=None, required=True):
+    """Helper function to get environment variables with optional default and required flag."""
+    value = os.getenv(var_name, default)
+    if required and value is None:
+        raise EnvironmentError(f"{var_name} is not defined and is required.")
+    return value
+
+def get_workflow_filename():
+    """Helper function to get workflow file name"""
+    return os.getenv("WORKFLOW_FILE_NAME", "workflow.json")
---
+++ packages/moteur-server-rest/moteur_server_rest-0.2.2-py3-none-any.whl/moteur_server_rest/jvm_utils.py
@@ -0,0 +1,16 @@
+import os
+from moteur_server_rest.config import get_env_variable
+
+
+def load_classpath():
+    """Load and return the classpath from moteur_home environment variable."""
+    moteur_home = get_env_variable('MOTEUR_HOME')
+    moteur_type = get_env_variable('MOTEUR_TYPE')
+    
+    classpath = f"{moteur_home}/{moteur_type}"
+    for jar_file in filter(lambda x: x.endswith(".jar"), os.listdir(f"{moteur_home}/libs")):
+        classpath += f":{os.path.join(moteur_home, 'libs', jar_file)}"
+    for plugin_file in filter(lambda x: x.endswith(".jar"), os.listdir(f"{moteur_home}/plugins")):
+        classpath += f":{os.path.join(moteur_home, 'plugins', plugin_file)}"
+        
+    return classpath
