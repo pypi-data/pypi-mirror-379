00a5b96313ceddf8091f4f1d1f4b34b9ec14cb0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 04:06:46 +0000

    Add package forteenall-kit/0.1.40/forteenall_kit-0.1.40.tar.gz

---
+++ packages/forteenall-kit/forteenall_kit-0.1.40.tar.gz/forteenall_kit-0.1.40/forteenall_kit/invokers/django_simple_CRUD.py
@@ -0,0 +1,11 @@
+class DjangoModelField:
+    pass
+
+class DjangoModelData:
+    pass
+
+class Feature:
+    def __init__(self, name, manager, options, invokerType):
+        pass
+    def execute(self):
+        pass
