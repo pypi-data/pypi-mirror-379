776c3b2e00db8e3887ba005036b4788b90330aaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 04:06:32 +0000

    Add package pybes3/0.3.11/pybes3-0.3.11-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/pybes3/pybes3-0.3.11-cp310-cp310-macosx_11_0_arm64.whl/pybes3/typing.py
@@ -0,0 +1,8 @@
+from typing import TypeVar
+
+import awkward as ak
+import numpy as np
+
+IntLike = TypeVar("ak.Array | np.ndarray | int", ak.Array, np.ndarray, int)
+FloatLike = TypeVar("ak.Array | np.ndarray | float", ak.Array, np.ndarray, float)
+BoolLike = TypeVar("ak.Array | np.ndarray | bool", ak.Array, np.ndarray, bool)
