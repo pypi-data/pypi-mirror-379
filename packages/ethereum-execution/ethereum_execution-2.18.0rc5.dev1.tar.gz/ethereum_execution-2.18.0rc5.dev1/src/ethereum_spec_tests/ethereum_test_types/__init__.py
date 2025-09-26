40c9facc6a9c520a5c828285b80d89634daebcff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 06:06:11 +0000

    Add package ethereum-execution/2.18.0rc5.dev1/ethereum_execution-2.18.0rc5.dev1.tar.gz

---
+++ packages/ethereum-execution/ethereum_execution-2.18.0rc5.dev1.tar.gz/ethereum_execution-2.18.0rc5.dev1/src/ethereum_spec_tests/ethereum_test_types/__init__.py
@@ -0,0 +1,82 @@
+"""Common definitions and types."""
+
+from .account_types import EOA, Alloc
+from .blob_types import Blob
+from .block_access_list import (
+    BalAccountChange,
+    BalBalanceChange,
+    BalCodeChange,
+    BalNonceChange,
+    BalStorageChange,
+    BalStorageSlot,
+    BlockAccessList,
+    BlockAccessListExpectation,
+)
+from .block_types import (
+    Environment,
+    EnvironmentDefaults,
+    Withdrawal,
+)
+from .chain_config_types import ChainConfig, ChainConfigDefaults
+from .helpers import (
+    TestParameterGroup,
+    add_kzg_version,
+    ceiling_division,
+    compute_create2_address,
+    compute_create_address,
+    compute_eofcreate_address,
+)
+from .receipt_types import TransactionReceipt
+from .request_types import (
+    ConsolidationRequest,
+    DepositRequest,
+    Requests,
+    WithdrawalRequest,
+)
+from .transaction_types import (
+    AuthorizationTuple,
+    NetworkWrappedTransaction,
+    Transaction,
+    TransactionDefaults,
+    TransactionTestMetadata,
+    TransactionType,
+)
+from .utils import Removable, keccak256
+
+__all__ = (
+    "Alloc",
+    "AuthorizationTuple",
+    "BalAccountChange",
+    "BalBalanceChange",
+    "BalCodeChange",
+    "BalNonceChange",
+    "BalStorageChange",
+    "BalStorageSlot",
+    "Blob",
+    "BlockAccessList",
+    "BlockAccessListExpectation",
+    "ChainConfig",
+    "ChainConfigDefaults",
+    "ConsolidationRequest",
+    "DepositRequest",
+    "Environment",
+    "EnvironmentDefaults",
+    "EOA",
+    "NetworkWrappedTransaction",
+    "Removable",
+    "Requests",
+    "TestParameterGroup",
+    "Transaction",
+    "TransactionDefaults",
+    "TransactionReceipt",
+    "TransactionTestMetadata",
+    "TransactionType",
+    "Withdrawal",
+    "WithdrawalRequest",
+    "add_kzg_version",
+    "ceiling_division",
+    "compute_create_address",
+    "compute_create2_address",
+    "compute_eofcreate_address",
+    "keccak256",
+)
