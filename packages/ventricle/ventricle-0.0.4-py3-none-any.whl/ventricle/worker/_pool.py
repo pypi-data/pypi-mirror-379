9cd59076f10417164aa34664a375f0f760448ee5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 04:07:05 +0000

    Add package ventricle/0.0.4/ventricle-0.0.4-py3-none-any.whl

---
+++ packages/ventricle/ventricle-0.0.4-py3-none-any.whl/ventricle/worker/_pool.py
@@ -0,0 +1,43 @@
+from functools import wraps
+from typing import Callable, Awaitable, Any
+
+from ._worker import Worker
+from ._task_queue import TaskQueue
+from ._task import Task
+
+class WorkerPool:
+
+    def __init__(self, concurrency:int) -> None:
+        """
+        The main worker pool class.
+        :param concurrency: The number of concurrency workers.
+        """
+
+        # initialize the worker queue
+        self._worker_queue = TaskQueue()
+
+
+        # initialize the worker
+        self._workers = [Worker(self._worker_queue) for _ in range(concurrency)]
+
+    def start(self):
+        for worker in self._workers:
+            worker.start()
+
+    def task(self, func:Callable[..., Awaitable[Any]]):
+        """
+        Task decorator.
+        :param func: The function to be executed.
+        :return: None
+        """
+
+        @wraps(func)
+        async def wrapper(*args, **kwargs):
+
+            # create the task object and put it in the execution queue
+            task = Task(func=func, args=args, kwargs=kwargs)
+            await self._worker_queue.put(task)
+
+            return None
+
+        return wrapper
