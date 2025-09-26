ae4a32f6dd4bcfac64a18ffd1dde9b3c5f4be806
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 03:18:35 +0000

    Add package forteenall-kit/0.1.35/forteenall_kit-0.1.35.tar.gz

---
+++ packages/forteenall-kit/forteenall_kit-0.1.35.tar.gz/forteenall_kit-0.1.35/forteenall_kit/invokers/nextjs_layout_create.py
@@ -0,0 +1,16 @@
+class ComponentDefinition:
+    pass
+
+class ProviderDefinition:
+    pass
+
+class LayoutData:
+    pass
+
+class Feature:
+    def __init__(self, name, manager, options, invokerType):
+        pass
+    def execute(self):
+        pass
+    def _generate_page_content(self):
+        pass
