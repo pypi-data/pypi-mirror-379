9cda566140e2286095636b98eed2f41df61de283
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 03:18:38 +0000

    Add package sfst-transduce/1.3.0/sfst_transduce-1.3.0-cp311-cp311-macosx_10_9_universal2.whl

---
+++ packages/sfst-transduce/sfst_transduce-1.3.0-cp311-cp311-macosx_10_9_universal2.whl/sfst_transduce/__init__.pyi
@@ -0,0 +1,20 @@
+from typing import List
+
+class Transducer:
+    def __init__(self, filename: str):
+        pass
+    def analyse(self, s: str) -> List[str]:
+        pass
+    def generate(self, s: str) -> List[str]:
+        pass
+
+class CompactTransducer:
+    both_layers: bool
+    simplest_only: bool
+
+    def __init__(self, filename: str):
+        pass
+    def analyse(self, s: str) -> List[str]:
+        pass
+    def generate(self, s: str) -> List[str]:
+        pass
