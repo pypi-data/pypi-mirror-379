fa5ff2a4d5df7640d6345401d10cc2ae1a8da564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 05:06:08 +0000

    Add package done2334/1.0/done2334-1.0.tar.gz

---
+++ packages/done2334/done2334-1.0.tar.gz/done2334-1.0/done2334/done2334.py
@@ -0,0 +1,5 @@
+print("This is my module\n use sm(a,b)")
+
+def sm(a,b):
+    c = a + b
+    return c
---
+++ packages/done2334/done2334-1.0.tar.gz/done2334-1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="done2334",
+    version="1.0",
+    author="Noman",
+    author_email="mnomanansari99@gmail.com",
+    description="This is just a test package",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    entry_point={
+        "console_script" :[
+            "mnomanansari99@gmail.com"
+            ],
+        },
+    
+    
+    
+    )
