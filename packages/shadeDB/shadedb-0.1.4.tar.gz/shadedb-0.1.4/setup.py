f614f4f3aaf955e9a76537bdcde457cb80a987be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 03:18:10 +0000

    Add package shadeDB/0.1.4/shadedb-0.1.4.tar.gz

---
+++ packages/shadeDB/shadedb-0.1.4.tar.gz/shadedb-0.1.4/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="shadeDB",
+    version="0.1.4",
+    description="A class oriented lightweight database with a cli wrapper for any device: store, update, copy, and remove structured data with a single command. Perfect for embedded devices, mobiles, devtools, and quick local services.",
+    author="Shade",
+    author_email="adesolasherifdeen3@gmail.com",
+    entry_points={
+        "console_scripts": [
+          "shadecrypt=shadecrypt.cli:main",
+          "scdb=shadecrypt.cli:main"
+        ]
+    },
+    include_package_data=True,
+    python_requires='>=3.8',
+    license="GPL-3.0",
+    classifiers=[
+      "Programming Language :: Python :: 3",
+      "License :: OSI Approved :: MIT License",
+      "Environment :: Console",
+      "Intended Audience :: Developers",
+      "Topic :: Database :: Database Engines/Servers",
+    ],
+    project_urls={
+        "GitHub": "https://github.com/harkerbyte",
+        "Facebook": "https://facebook.com/harkerbyte",
+        "Whatsapp" : "https://whatsapp.com/channel/0029Vb5f98Z90x2p6S1rhT0S",
+        "Youtube" : "https://youtube.com/@harkerbyte",
+        "Instagram": "https://instagram.com/harkerbyte"
+    },
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+)
