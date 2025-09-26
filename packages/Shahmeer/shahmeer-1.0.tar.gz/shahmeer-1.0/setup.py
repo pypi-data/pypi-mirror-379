06688e7bc2ecb21e0a32443cc401e721d6c82158
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 05:06:32 +0000

    Add package Shahmeer/1.0/shahmeer-1.0.tar.gz

---
+++ packages/Shahmeer/shahmeer-1.0.tar.gz/shahmeer-1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Shahmeer",
+    version = "1.0",
+    author = "Shahmeer khan",
+    author_email = "khanshahmeer200@gmail.com",
+    description = "This is just a test package",
+    packages = find_packages(),
+    python_requires = ">=3.6",
+    entry_point = {
+        "console_script" : [
+            "khanshahmeer200@gmail.com"
+            ],
+        },
+    )
