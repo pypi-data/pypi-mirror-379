0275ccb4c8c30b9571e8afd6fa6c86b72295eec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 15:07:39 +0000

    Add package mgo-prwrkpi/5.4.1/mgo_prwrkpi-5.4.1.tar.gz

---
+++ packages/mgo-prwrkpi/mgo_prwrkpi-5.4.1.tar.gz/mgo_prwrkpi-5.4.1/mgo_processor/controllers/processor/__init__.py
@@ -0,0 +1,2 @@
+#from .process_manager import ProcessManager
+#from .entry_points import *
---
+++ packages/mgo-prwrkpi/mgo_prwrkpi-5.4.1.tar.gz/mgo_prwrkpi-5.4.1/mgo_processor/tp_processing/__init__.py
@@ -0,0 +1,3 @@
+from .tp_base import send, send2
+from .config import init
+from mgo_processor.tp_processing.method_cols import method_cols
