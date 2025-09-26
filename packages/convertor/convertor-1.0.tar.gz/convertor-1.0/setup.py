1396e0f9d44ce3ee5a7550c50a068e502fcd557f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 05:06:10 +0000

    Add package convertor/1.0/convertor-1.0.tar.gz

---
+++ packages/convertor/convertor-1.0.tar.gz/convertor-1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="convertor",
+    version="1.0",
+    author="Fakiya Siddiqui",
+    author_email="fakiyasiddiqui25@gmail.com",
+    description="This is just a test package",
+    packages= find_packages(),
+    python_requires=">=3.6",
+    entry_point={
+        "console_script":["fakiyasiddiqui25@gmail.com"],
+        },
+    )
+    
