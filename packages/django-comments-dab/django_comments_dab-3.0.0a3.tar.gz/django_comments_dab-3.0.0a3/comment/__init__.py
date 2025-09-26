467a0b83e588c58402236edbf74d57d887f3d7b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 06:07:04 +0000

    Add package django-comments-dab/3.0.0a3/django_comments_dab-3.0.0a3.tar.gz

---
+++ packages/django-comments-dab/django_comments_dab-3.0.0a3.tar.gz/django_comments_dab-3.0.0a3/comment/__init__.py
@@ -0,0 +1,23 @@
+import os
+
+__version__ = '3.0.0a3'
+
+
+def _get_version():
+    parent_project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    with open(os.path.join(parent_project_dir, 'VERSION')) as version_file:
+        version = version_file.read().strip().strip('v')
+
+    return version
+
+
+def check_release():
+    try:
+        release = _get_version()
+    except FileNotFoundError:
+        return
+
+    assert release == __version__, 'Current version does not match with manifest VERSION'
+
+
+check_release()
