d4461633f04deed938f7c91b24dea19f5fe0f7de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 04:06:45 +0000

    Add package forteenall-kit/0.1.39/forteenall_kit-0.1.39.tar.gz

---
+++ packages/forteenall-kit/forteenall_kit-0.1.39.tar.gz/forteenall_kit-0.1.39/forteenall_kit/_features.py
@@ -0,0 +1,12 @@
+# This file is auto-generated. Do not edit manually.
+# For upadte this file pls run 'bash update.sh' on main forteenall project
+
+# Invokers import
+from forteenall_kit.invokers.djangoModel import Feature as Feature_djangoModel
+from forteenall_kit.invokers.spaceDup import Feature as Feature_spaceDup
+
+features = {
+    'djangoModel': Feature_djangoModel,
+    'spaceDup': Feature_spaceDup,
+}
+
