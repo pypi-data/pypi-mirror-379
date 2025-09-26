2fb7cd88a86e98163d7d87e472222159bcf375af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 03:18:30 +0000

    Add package autowebx/1.10.1/autowebx-1.10.1-py3-none-any.whl

---
+++ packages/autowebx/autowebx-1.10.1-py3-none-any.whl/autowebx/communications.py
@@ -0,0 +1,27 @@
+"""Very small localhost message-passing helpers.
+
+Note: `send(msg)` opens a Listener and writes to the first client. Use
+`receive()` to connect as a Client and read the message. Run the sender
+first so the listener is available.
+"""
+
+from multiprocessing.connection import Listener
+from multiprocessing.connection import Client
+from threading import Lock
+
+__lock = Lock()
+
+
+def send(msg):
+    with Listener(('localhost', 3011)) as listener:
+        with listener.accept() as conn:
+            conn.send(msg)
+
+
+def receive():
+    with __lock:
+        try:
+            with Client(('localhost', 3011)) as conn:
+                return conn.recv()
+        except ConnectionRefusedError:
+            raise ConnectionRefusedError("Run the sender first")
