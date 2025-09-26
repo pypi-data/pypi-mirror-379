42a18fee846d5e4b7728faff524f2819ed9c744d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 07:06:01 +0000

    Add package ciounreal/1.0.0a2/ciounreal-1.0.0a2.tar.gz

---
+++ packages/ciounreal/ciounreal-1.0.0a2.tar.gz/ciounreal-1.0.0a2/Plugins/Conductor/Source/Conductor/Private/ConductorSettings.cpp
@@ -0,0 +1,46 @@
+// Copyright 2024 CONDUCTOR TECHNOLOGIES. All Rights Reserved.
+
+
+#include "ConductorSettings.h"
+
+#include "ConductorSettingsLibrary.h"
+
+UConductorSettings::UConductorSettings()
+{
+	if (UConductorSettingsLibrary::Get())
+	{
+		SettingsContainer.GeneralSettings.JobTitle = UConductorSettingsLibrary::Get()->GetJobTitle();
+		SettingsContainer.AdvancedSettings.Template = UConductorSettingsLibrary::Get()->GetDefaultTaskTemplate();
+		SettingsContainer.PerforceSettings.PerforceServer = UConductorSettingsLibrary::Get()->GetPerforceServer();
+	}
+}
+
+TArray<FString> UConductorSettings::GetProjects()
+{
+	return UConductorSettingsLibrary::Get()->GetProjects();
+}
+
+TArray<FString> UConductorSettings::GetInstanceTypes()
+{
+	return UConductorSettingsLibrary::Get()->GetInstanceTypes();
+}
+
+TArray<FString> UConductorSettings::GetEnvMergePolicy()
+{
+	return UConductorSettingsLibrary::Get()->GetEnvMergePolicy();
+}
+
+FText UConductorPluginSettings::GetSectionText() const
+{
+	return NSLOCTEXT("ConductorPluginSettings", "ConductorPluginSettingsSection", "Conductor");
+}
+
+FName UConductorPluginSettings::GetSectionName() const
+{
+	return TEXT("Conductor");
+}
+
+UConductorPluginSettings::UConductorPluginSettings()
+{
+}
+
