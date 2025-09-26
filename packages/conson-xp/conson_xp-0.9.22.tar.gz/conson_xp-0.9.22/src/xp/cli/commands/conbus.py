90a4ae3b45c8850e55fd04b82963f1e0033151c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 17:05:46 +0000

    Add package conson-xp/0.9.22/conson_xp-0.9.22.tar.gz

---
+++ packages/conson-xp/conson_xp-0.9.22.tar.gz/conson_xp-0.9.22/src/xp/cli/commands/conbus.py
@@ -0,0 +1,32 @@
+"""Shared conbus CLI group definition."""
+
+import click
+from click_help_colors import HelpColorsGroup
+
+from .conbus_datapoint_commands import conbus_datapoint_group
+
+@click.group(cls=HelpColorsGroup, help_headers_color='yellow', help_options_color='green')
+def conbus() -> None:
+    """
+    Conbus client operations for sending telegrams to remote servers
+    """
+    pass
+
+@click.group(name="blink", cls=HelpColorsGroup, help_headers_color='yellow', help_options_color='green')
+def conbus_blink() -> None:
+    """
+    Conbus client operations for sending blink telegrams to remote servers
+    """
+    pass
+
+
+@click.group(name="output", cls=HelpColorsGroup, help_headers_color='yellow', help_options_color='green')
+def conbus_output() -> None:
+    """
+    Conbus input operations to remote servers
+    """
+    pass
+
+conbus.add_command(conbus_blink)
+conbus.add_command(conbus_output)
+conbus.add_command(conbus_datapoint_group)
