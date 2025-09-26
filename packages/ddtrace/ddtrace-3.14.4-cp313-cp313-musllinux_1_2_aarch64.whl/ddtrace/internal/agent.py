c82f0e048b18cd570d4f0575ce0b3c0179cb1304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 16:08:26 +0000

    Add package ddtrace/3.14.4/ddtrace-3.14.4-cp313-cp313-musllinux_1_2_aarch64.whl

---
+++ packages/ddtrace/ddtrace-3.14.4-cp313-cp313-musllinux_1_2_aarch64.whl/ddtrace/internal/agent.py
@@ -0,0 +1,31 @@
+import json
+
+from ddtrace.internal.logger import get_logger
+from ddtrace.settings._agent import config
+
+from .utils.http import get_connection
+
+
+log = get_logger(__name__)
+
+
+def info(url=None):
+    agent_url = config.trace_agent_url if url is None else url
+    timeout = config.trace_agent_timeout_seconds
+    _conn = get_connection(agent_url, timeout=timeout)
+    try:
+        _conn.request("GET", "info", headers={"content-type": "application/json"})
+        resp = _conn.getresponse()
+        data = resp.read()
+    finally:
+        _conn.close()
+
+    if resp.status == 404:
+        # Remote configuration is not enabled or unsupported by the agent
+        return None
+
+    if resp.status < 200 or resp.status >= 300:
+        log.warning("Unexpected error: HTTP error status %s, reason %s", resp.status, resp.reason)
+        return None
+
+    return json.loads(data)
