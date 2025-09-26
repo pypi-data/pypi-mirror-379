75f44f2da61ba7f85b7de5bd19b128cc57076810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 14:09:21 +0000

    Add package ppca-py/1.0.3/ppca_py-1.0.3-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/ppca-py/ppca_py-1.0.3-cp310-cp310-macosx_11_0_arm64.whl/ppca/__init__.py
@@ -0,0 +1,9 @@
+# Copyright 2025 brdav
+
+# Use of this source code is governed by an MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT.
+
+from ._ppca import PPCA
+
+__all__ = ["PPCA"]
