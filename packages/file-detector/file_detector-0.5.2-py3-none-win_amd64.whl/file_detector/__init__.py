2c8ce5e86f81eaf31d613c747de601a44445e190
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 05:06:08 +0000

    Add package file-detector/0.5.2/file_detector-0.5.2-py3-none-win_amd64.whl

---
+++ packages/file-detector/file_detector-0.5.2-py3-none-win_amd64.whl/file_detector/__init__.py
@@ -0,0 +1,21 @@
+from .detect import (
+    detect_file,
+    detect_buffer,
+    detect_file_mime_and_charset,
+    detect_buffer_mime_and_charset,
+    FileCategory,
+    FileSubtype,
+    Kind,
+)
+
+__version__ = "0.5.2"
+__version_info__ = tuple(int(i) for i in __version__.split('.'))
+__all__ = [
+    "detect_file",
+    "detect_buffer",
+    "detect_file_mime_and_charset",
+    "detect_buffer_mime_and_charset",
+    "FileCategory",
+    "FileSubtype",
+    "Kind",
+]
