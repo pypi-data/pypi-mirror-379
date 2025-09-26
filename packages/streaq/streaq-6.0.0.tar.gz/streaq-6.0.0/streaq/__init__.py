f9927df4f68f0df3d8211917d82d5cf1f32f764a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 07:06:05 +0000

    Add package streaq/6.0.0/streaq-6.0.0.tar.gz

---
+++ packages/streaq/streaq-6.0.0.tar.gz/streaq-6.0.0/streaq/__init__.py
@@ -0,0 +1,27 @@
+import logging
+
+import coredis
+
+VERSION = "6.0.0"
+__version__ = VERSION
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+
+# disable some runtime checks
+coredis.Config.optimized = True
+
+# ruff: noqa: E402
+
+from .task import StreaqRetry, TaskStatus
+from .types import TaskContext
+from .utils import StreaqError
+from .worker import Worker
+
+__all__ = [
+    "StreaqError",
+    "StreaqRetry",
+    "TaskContext",
+    "TaskStatus",
+    "Worker",
+]
