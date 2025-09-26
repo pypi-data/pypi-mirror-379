6071c3029f83dfe49e38d6de2b8fdf655591043d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 05:06:47 +0000

    Add package plain.toolbar/0.3.0/plain_toolbar-0.3.0-py3-none-any.whl

---
+++ packages/plain.toolbar/plain_toolbar-0.3.0-py3-none-any.whl/plain/toolbar/CHANGELOG.md
@@ -0,0 +1,42 @@
+# plain-toolbar changelog
+
+## [0.3.0](https://github.com/dropseed/plain/releases/plain-toolbar@0.3.0) (2025-09-25)
+
+### What's changed
+
+- Updated toolbar module autodiscovery to use the new `packages_registry.autodiscover_modules()` method ([b0b610d](https://github.com/dropseed/plain/commit/b0b610d461))
+
+### Upgrade instructions
+
+- No changes required
+
+## [0.2.0](https://github.com/dropseed/plain/releases/plain-toolbar@0.2.0) (2025-09-19)
+
+### What's changed
+
+- Minimum Python version raised to 3.13 ([d86e307](https://github.com/dropseed/plain/commit/d86e307efb))
+
+### Upgrade instructions
+
+- Upgrade your Python environment to Python 3.13 or later
+
+## [0.1.1](https://github.com/dropseed/plain/releases/plain-toolbar@0.1.1) (2025-08-28)
+
+### What's changed
+
+- Improved null safety when checking user and impersonator attributes in toolbar rendering ([90568bd](https://github.com/dropseed/plain/commit/90568bdfa4))
+
+### Upgrade instructions
+
+- No changes required
+
+## [0.1.0](https://github.com/dropseed/plain/releases/plain-toolbar@0.1.0) (2025-08-27)
+
+### What's changed
+
+- Initial release of plain-toolbar as a standalone package ([e49d54b](https://github.com/dropseed/plain/commit/e49d54bfea162424c73e54bf7ed87e93442af899))
+- Fixed URL pattern and name display to include quotes in the request toolbar ([aa759c7](https://github.com/dropseed/plain/commit/aa759c72cae987ed8b6dd07c2e70f5fb97b6fd09))
+
+### Upgrade instructions
+
+- No changes required
---
+++ packages/plain.toolbar/plain_toolbar-0.3.0-py3-none-any.whl/plain/toolbar/toolbar.py
@@ -0,0 +1,97 @@
+import sys
+import traceback
+
+from plain.runtime import settings
+from plain.templates import Template
+from plain.urls.exceptions import Resolver404
+from plain.utils.safestring import mark_safe
+
+from .registry import register_toolbar_panel, registry
+
+
+class Toolbar:
+    def __init__(self, request):
+        self.request = request
+        self.version = settings.APP_VERSION
+
+    def should_render(self):
+        if settings.DEBUG:
+            return True
+
+        if impersonator := getattr(self.request, "impersonator", None):
+            return getattr(impersonator, "is_admin", False)
+
+        if user := getattr(self.request, "user", None):
+            return getattr(user, "is_admin", False)
+
+        return False
+
+    def request_exception(self):
+        # We can capture the exception currently being handled here, if any.
+        exception = sys.exception()
+
+        if exception and not isinstance(exception, Resolver404):
+            exception._traceback_string = "".join(
+                traceback.format_tb(exception.__traceback__)
+            )
+            return exception
+
+    def get_panels(self):
+        panels = [panel(self.request) for panel in registry.get_panels()]
+
+        if self.request_exception():
+            exception = self.request_exception()
+            panels = [
+                _ExceptionToolbarPanel(self.request, exception),
+            ] + panels
+
+        return panels
+
+
+class ToolbarPanel:
+    name: str
+    template_name: str
+    button_template_name: str = ""
+
+    def __init__(self, request):
+        self.request = request
+
+    def get_template_context(self):
+        return {
+            "request": self.request,
+            "panel": self,
+        }
+
+    def render(self):
+        template = Template(self.template_name)
+        context = self.get_template_context()
+        return mark_safe(template.render(context))
+
+    def render_button(self):
+        """Render the toolbar button for the minimized state."""
+        if not self.button_template_name:
+            return ""
+        template = Template(self.button_template_name)
+        context = self.get_template_context()
+        return mark_safe(template.render(context))
+
+
+class _ExceptionToolbarPanel(ToolbarPanel):
+    name = "Exception"
+    template_name = "toolbar/exception.html"
+    button_template_name = "toolbar/exception_button.html"
+
+    def __init__(self, request, exception):
+        super().__init__(request)
+        self.exception = exception
+
+    def get_template_context(self):
+        context = super().get_template_context()
+        context["exception"] = self.exception
+        return context
+
+
+@register_toolbar_panel
+class _RequestToolbarPanel(ToolbarPanel):
+    name = "Request"
+    template_name = "toolbar/request.html"
