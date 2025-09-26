e5bcbfee0fcf72028cb87972beb25c7bd80f1827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 03:18:25 +0000

    Add package respark/0.0.2/respark-0.0.2.tar.gz

---
+++ packages/respark/respark-0.0.2.tar.gz/respark-0.0.2/respark/__init__.py
@@ -0,0 +1,10 @@
+from .schema_inspector import (
+    SchemaInspector,
+    SchemaModel,
+    TableInfo,
+    ColumnInfo,
+    TypeNormaliser,
+)
+
+__all__ = ["SchemaInspector", "SchemaModel",
+           "TableInfo", "ColumnInfo", "TypeNormaliser"]
