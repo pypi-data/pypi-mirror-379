878d5752f674f42a81aeea3b90a52e2abc3123dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 06:06:15 +0000

    Add package ombott/2.5/ombott-2.5.tar.gz

---
+++ packages/ombott/ombott-2.5.tar.gz/ombott-2.5/README.md
@@ -0,0 +1,2 @@
+# ombott
+This is [bottle.py](https://bottlepy.org) spin-off (One More BOTTle)
---
+++ packages/ombott/ombott-2.5.tar.gz/ombott-2.5/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import ombott
+
+setup(
+    name="ombott",
+    version=ombott.__version__,
+    url="https://github.com/valq7711/ombott",
+    license=ombott.__license__,
+    author=ombott.__author__,
+    author_email="valq7711@gmail.com",
+    maintainer=ombott.__author__,
+    maintainer_email="valq7711@gmail.com",
+    description="One More BOTTle",
+    platforms="any",
+    keywords='python webapplication',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Internet :: WWW/HTTP :: HTTP Servers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires='>=3.7',
+    packages=find_packages('.'),
+    package_data={'ombott': ['error.html']}
+)
