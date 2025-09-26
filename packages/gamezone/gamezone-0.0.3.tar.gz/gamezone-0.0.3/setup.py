075f62b5b25cffd4bf39bf8d2156f1a75224d2e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 03:18:12 +0000

    Add package gamezone/0.0.3/gamezone-0.0.3.tar.gz

---
+++ packages/gamezone/gamezone-0.0.3.tar.gz/gamezone-0.0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gamezone',
+    version='0.0.3',
+    author='Shreeya Singh',
+    author_email='shreeyasingh2404@gmail.com',
+    description='A collection of classic arcade games built with Python and Turtle Graphics.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/shreeya2424/pygamezone',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        
+    ],
+    # Corrected license file path
+    # license_file='LICENSE',
+)
