0245a32b8a1c33fdcf9808a122f57b8f107b3537
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 14:06:30 +0000

    Add package dsf-label-sdk/1.1.2/dsf_label_sdk-1.1.2.tar.gz

---
+++ packages/dsf-label-sdk/dsf_label_sdk-1.1.2.tar.gz/dsf_label_sdk-1.1.2/dsf_label_sdk/exceptions.py
@@ -0,0 +1,27 @@
+# dsf_label_sdk/exceptions.py
+"""Custom exceptions for the SDK"""
+
+from typing import Optional 
+
+class LabelSDKError(Exception):
+    """Base exception for SDK errors."""
+    pass
+
+
+class ValidationError(LabelSDKError):
+    """Raised when input validation fails."""
+    pass
+
+
+class LicenseError(LabelSDKError):
+    """Raised when license validation fails."""
+    pass
+
+
+class APIError(LabelSDKError):
+    """Raised when API request fails."""
+    
+    def __init__(self, message: str, status_code: Optional[int] = None):
+        super().__init__(message)
+        self.message = message
+        self.status_code = status_code
