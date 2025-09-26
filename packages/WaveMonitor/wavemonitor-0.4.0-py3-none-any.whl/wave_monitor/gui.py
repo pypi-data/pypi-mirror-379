76dab7b9cc1c95421b5d35c5451a6b749188c56c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 16:07:54 +0000

    Add package WaveMonitor/0.4.0/wavemonitor-0.4.0-py3-none-any.whl

---
+++ packages/WaveMonitor/wavemonitor-0.4.0-py3-none-any.whl/wave_monitor/gui.py
@@ -0,0 +1,26 @@
+import argparse
+import sys
+
+from PySide6.QtWidgets import QApplication
+
+from . import MonitorWindow, config_log
+
+
+def start():
+    """Console entry point.
+
+    Accepts --log=<LEVEL> and passes it to config_log. Uses parse_known_args so
+    Qt/other args are preserved for QApplication.
+    """
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument("--log", help="Set log level (DEBUG, INFO, WARNING, ERROR)", default=None)
+    args, _ = parser.parse_known_args()
+
+    if args.log:
+        config_log(args.log)
+    else:
+        config_log()
+
+    app = QApplication(sys.argv)
+    _ = MonitorWindow()
+    sys.exit(app.exec())
