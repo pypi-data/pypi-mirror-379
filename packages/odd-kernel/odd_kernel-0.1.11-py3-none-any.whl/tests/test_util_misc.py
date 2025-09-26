dd03bb2e0ffa63a2161a3db8462da12336cc4cf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 03:18:19 +0000

    Add package odd-kernel/0.1.11/odd_kernel-0.1.11-py3-none-any.whl

---
+++ packages/odd-kernel/odd_kernel-0.1.11-py3-none-any.whl/tests/test_util_misc.py
@@ -0,0 +1,8 @@
+from odd_kernel.util.misc import ding
+from odd_kernel.util.general import wait_some_time
+from odd_kernel.util.charts import get_colors
+
+for i in range(3):
+    ding(2000)
+
+print(get_colors(100))
