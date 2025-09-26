d717192c1409b23023e1c5232756513123e2a3bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 05:06:39 +0000

    Add package odoo-image-fetcher/0.1.0/odoo_image_fetcher-0.1.0.tar.gz

---
+++ packages/odoo-image-fetcher/odoo_image_fetcher-0.1.0.tar.gz/odoo_image_fetcher-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='odoo-image-fetcher',
+    version='0.1.0',
+    author='Onyekelu Chukwuebuka',
+    author_email='conyekelu@yahoo.com',
+    description='Fetch images from an Odoo backend using session-based authentication',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/cloudtechy/odoo-image-fetcher',
+    download_url='https://github.com/cloudtechy/odoo-image-fetcher/archive/refs/tags/v0.1.0.tar.gz',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'python-dotenv'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
