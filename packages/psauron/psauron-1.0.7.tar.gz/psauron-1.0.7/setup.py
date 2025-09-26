ef75d5def74d52b9c454654ba01d9de8986f359b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 04:06:42 +0000

    Add package psauron/1.0.7/psauron-1.0.7.tar.gz

---
+++ packages/psauron/psauron-1.0.7.tar.gz/psauron-1.0.7/setup.py
@@ -0,0 +1,44 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+    
+setup(
+    name="psauron",
+    version="1.0.7",
+    description="A tool to assess protein coding gene annotation",
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Markusjsommer/psauron",
+    author="markus",
+    author_email = "markusjsommer@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS :: MacOS X",
+    ],
+    install_requires=["torch>=2.1.2",
+                      "torchvision>=0.16.2",
+                      "torchaudio>=2.1.2",
+                      "typing-extensions>=4.9.0",
+                      "tqdm>=4.66.1",
+                      "scipy>=1.10.1",
+                      "numpy>=1.24.4, <2",
+                      "pandas",
+                      "setuptools<81"],
+    extras_require={
+        "dev": ["pytest>=7.0", "twine>=4.0.2", "pytest-cov>=4.0", "wheel"],
+    },
+    python_requires=">=3.9, <3.13",
+    entry_points={
+        'console_scripts': [
+            'psauron = psauron.psauron:eye_of_psauron',
+        ],
+    },
+    include_package_data=True,
+    package_data={'': ['/data/model_state_dict.pt']},
+    
+)
---
+++ packages/psauron/psauron-1.0.7.tar.gz/psauron-1.0.7/tests/test_load_model.py
@@ -0,0 +1,4 @@
+from psauron.psauron import load_model
+
+def test_load_model_default():
+    load_model(True)
