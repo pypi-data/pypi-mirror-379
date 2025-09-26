f8e7ff7dd09093b05fc83e4fbc55b4a522fcc80e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 03:18:08 +0000

    Add package sfst-transduce/1.3.1/sfst_transduce-1.3.1.tar.gz

---
+++ packages/sfst-transduce/sfst_transduce-1.3.1.tar.gz/sfst_transduce-1.3.1/src/fst-print.cpp
@@ -0,0 +1,51 @@
+/*******************************************************************/
+/*                                                                 */
+/*  FILE     fst-print.cpp                                           */
+/*  MODULE   fst-print                                             */
+/*  PROGRAM  SFST                                                  */
+/*  AUTHOR   Helmut Schmid, IMS, University of Stuttgart           */
+/*                                                                 */
+/*******************************************************************/
+
+#include "fst.h"
+
+using std::cerr;
+using std::cout;
+
+using namespace SFST;
+
+/*******************************************************************/
+/*                                                                 */
+/*  main                                                           */
+/*                                                                 */
+/*******************************************************************/
+
+int main(int argc, char **argv)
+
+{
+  FILE *file;
+
+  if (argc > 1 && (!strcmp(argv[1], "-h") || !strcmp(argv[1], "-help") ||
+                   !strcmp(argv[1], "-?"))) {
+    fprintf(stderr, "\nUsage: %s [file]\n\n", argv[0]);
+    exit(1);
+  }
+
+  if (argc == 1)
+    file = stdin;
+  else if ((file = fopen(argv[1], "rb")) == NULL) {
+    fprintf(stderr, "\nError: Cannot open transducer file %s\n\n", argv[1]);
+    exit(1);
+  }
+
+  try {
+    Transducer a(file);
+    fclose(file);
+    cout << a;
+  } catch (const char *p) {
+    cerr << p << "\n";
+    return 1;
+  }
+
+  return 0;
+}
