56227d9ab98e90c87307b6ea99186978ea2b8dc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 16:08:15 +0000

    Add package gateau/0.1.5/gateau-0.1.5.tar.gz

---
+++ packages/gateau/gateau-0.1.5.tar.gz/gateau-0.1.5/scripts/clean.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+rm -rf \
+	.ruff_cache \
+	.skbuild-info.json \
+	CMakeCache.txt \
+	CMakeFiles \
+	CMakeInit.txt \
+	dist \
+	*.cmake \
+	.cmake \
+	testfile \
+	docs \
+	gsl \
+	Makefile \
+	gateau_test
+
