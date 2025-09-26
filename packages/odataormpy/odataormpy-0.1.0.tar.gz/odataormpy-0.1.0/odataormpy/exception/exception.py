7d07a85ed4a3c1dd764ac6924f0c5b79c3e60220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 03:18:21 +0000

    Add package odataormpy/0.1.0/odataormpy-0.1.0.tar.gz

---
+++ packages/odataormpy/odataormpy-0.1.0.tar.gz/odataormpy-0.1.0/odataormpy/exception/exception.py
@@ -0,0 +1,13 @@
+"""
+Author: Diego Vaccher <dvaccher99@gmail.com>
+Created on: 2025-09-24
+
+Description:
+    Provides the base class for ORM Session Exception
+
+Change Log:
+    2025-09-24 - Diego Vaccher - Initial creation
+"""
+
+class ORMSessionException(Exception):
+    pass
