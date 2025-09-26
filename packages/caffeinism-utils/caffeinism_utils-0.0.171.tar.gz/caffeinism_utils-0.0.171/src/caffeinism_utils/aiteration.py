89b7fcef3d6a078b639090ea04d08b50dc388cbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 16:08:35 +0000

    Add package caffeinism-utils/0.0.171/caffeinism_utils-0.0.171.tar.gz

---
+++ packages/caffeinism-utils/caffeinism_utils-0.0.171.tar.gz/caffeinism_utils-0.0.171/src/caffeinism_utils/aiteration.py
@@ -0,0 +1,60 @@
+import asyncio
+import time
+from typing import AsyncGenerator, AsyncIterable, Generic, TypeVar
+
+from .asyncio import run_in_threadpool
+from .utils import DummyStopIteration, next_without_stop_iteration
+
+
+async def to_aiterator(iterator):
+    iterator = await run_in_threadpool(iter, iterator)
+    while True:
+        try:
+            yield await run_in_threadpool(next_without_stop_iteration, iterator)
+        except DummyStopIteration:
+            break
+
+
+async def rate_limit_iterator(aiterator, iters_per_second):
+    start = time.time()
+    i = 0
+    async for it in aiterator:
+        yield it
+        await asyncio.sleep((i / iters_per_second) - (time.time() - start))
+        i += 1
+
+
+T = TypeVar("T")
+
+__CLOSE = object()
+
+
+class StreamQueue(Generic[T]):
+    def __init__(self):
+        self.queue = asyncio.Queue[T]()
+
+    def put(self, data: T) -> None:
+        return self.queue.put(data)
+
+    async def __aiter__(self) -> AsyncGenerator[T]:
+        while (it := await self.queue.get()) is not __CLOSE:
+            yield it
+
+    async def close(self):
+        await self.queue.put(__CLOSE)
+
+
+class StreamIteratorQueue(Generic[T]):
+    def __init__(self):
+        self.queue = asyncio.Queue[AsyncIterable[T]]()
+
+    def put(self, data: AsyncIterable[T]) -> None:
+        return self.queue.put(data)
+
+    async def __aiter__(self) -> AsyncGenerator[T]:
+        while (iterator := await self.queue.get()) is not __CLOSE:
+            async for it in iterator:
+                yield it
+
+    async def close(self):
+        await self.queue.put(__CLOSE)
