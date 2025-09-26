6efc1a20522ab5a1792941f98c2df5a9280a3062
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 05:06:10 +0000

    Add package kaqing/2.0.42/kaqing-2.0.42.tar.gz

---
+++ packages/kaqing/kaqing-2.0.42.tar.gz/kaqing-2.0.42/adam/commands/postgres/pg_utils.py
@@ -0,0 +1,18 @@
+import functools
+
+from adam.commands.postgres.postgres_session import PostgresSession
+
+def pg_database_names(ns: str, pg_path: str):
+    pg = PostgresSession(ns, pg_path)
+    return [db['name'] for db in pg.databases() if db['owner'] == PostgresSession.default_owner()]
+
+@functools.lru_cache()
+def pg_table_names(ns: str, pg_path: str):
+    return [table['name'] for table in pg_tables(ns, pg_path) if table['schema'] == PostgresSession.default_schema()]
+
+def pg_tables(ns: str, pg_path: str):
+    pg = PostgresSession(ns, pg_path)
+    if pg.db:
+        return pg.tables()
+
+    return []
