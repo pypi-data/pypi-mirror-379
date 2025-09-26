3b5bc3250c85952d67e97c8ab1050b627587e2b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 05:06:02 +0000

    Add package hussain/1.0/hussain-1.0.tar.gz

---
+++ packages/hussain/hussain-1.0.tar.gz/hussain-1.0/hussain/hussain1.py
@@ -0,0 +1,15 @@
+def a(a,b):
+    add=a+b
+    return add
+
+def b(a,b):
+    sub=a-b
+    return sub
+
+def c(a,b):
+    mul=a*b
+    return mul
+
+def d(a,b):
+    div=a/b
+    return div
---
+++ packages/hussain/hussain-1.0.tar.gz/hussain-1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hussain',
+    version='1.0',
+    author='hussain',
+    author_email='hussain8900.shs@gmail.com',
+    description='this is just a test package',
+    packages=find_packages(),
+    python_requires='>=3.6',
+    entry_point={
+        'console_script':['hussain8900.shs@gmail.com'],
+                },
+     )
