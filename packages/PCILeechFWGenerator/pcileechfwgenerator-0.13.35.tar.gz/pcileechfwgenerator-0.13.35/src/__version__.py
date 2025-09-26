bc4c9c36e2ad187fb43be5f8778424f762450f64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 15:07:14 +0000

    Add package PCILeechFWGenerator/0.13.35/pcileechfwgenerator-0.13.35.tar.gz

---
+++ packages/PCILeechFWGenerator/pcileechfwgenerator-0.13.35.tar.gz/pcileechfwgenerator-0.13.35/src/__version__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Version information for PCILeech Firmware Generator."""
+
+__version__ = "0.13.35"
+__version_info__ = (0, 13, 35)
+
+# Release information
+__title__ = "PCILeech Firmware Generator"
+__description__ = "Generate spoofed PCIe DMA firmware from real donor hardware"
+__author__ = "Ramsey McGrath"
+__author_email__ = "ramsey@voltcyclone.info"
+__license__ = "MIT"
+__url__ = "https://github.com/voltcyclone/PCILeechFWGenerator"
+
+# Build metadata
+__build_date__ = "2025-09-26T04:48:04.521897"
+__commit_hash__ = "632f1bc"
