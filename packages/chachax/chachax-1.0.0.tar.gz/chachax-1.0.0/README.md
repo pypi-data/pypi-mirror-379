13422a77ae85e94ca4f5ce6271274f797e95e62e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 07:06:14 +0000

    Add package chachax/1.0.0/chachax-1.0.0.tar.gz

---
+++ packages/chachax/chachax-1.0.0.tar.gz/chachax-1.0.0/README.md
@@ -0,0 +1,27 @@
+# chachax
+
+Decrypt ChaCha
+
+## Features
+
+- Supports Python 3.6+
+
+## Installation
+
+Install the latest version of `chachax` from PyPI:
+
+```bash
+pip install chachax
+```
+
+## Usage
+Here's how to use `chachax` for decryption:
+
+```python
+import chachax
+
+encrypted_data = b'...'
+key = b'...' # 32 bytes
+nonce = b'...' # 12 bytes
+rounds = 8 # 8/12/20
+dec = chachax.decrypt(encrypted_data, key, Nonce, rounds)
---
+++ packages/chachax/chachax-1.0.0.tar.gz/chachax-1.0.0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, Extension
+
+extension = Extension('chachax', ['chachax.c'])
+
+try:
+    with open('README.md', 'r', encoding='utf-8') as f:
+        long_description = f.read()
+except Exception:
+    long_description = ''
+
+setup(
+    name="chachax",
+    version="1.0.0",
+    author='wood',
+    author_email='miraclerinwood@gmail.com',
+    url='https://github.com/Rin-Wood/chachax',
+    description="Decrypt ChaCha",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license="BSD",
+    keywords="chachax",
+    ext_modules=[extension],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+    ],
+    python_requires=">=3.6",
+)
