fc3ff604f50fa5cc8e0ef7169b379bee46ba0af5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 05:06:12 +0000

    Add package open-edison/0.1.116/open_edison-0.1.116-py3-none-any.whl

---
+++ packages/open-edison/open_edison-0.1.116-py3-none-any.whl/src/__init__.py
@@ -0,0 +1,8 @@
+"""
+Open Edison Source Package.
+
+Note: Avoid importing heavy submodules at package import time to keep
+packaging/import of light utilities (e.g., mcp_importer) side‑effect free.
+"""
+
+__all__: list[str] = []
