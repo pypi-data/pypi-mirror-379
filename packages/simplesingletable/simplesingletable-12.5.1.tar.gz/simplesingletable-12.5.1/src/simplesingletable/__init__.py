2339afacd5768bda15200fd2fba10e56ca4b26d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 15:07:17 +0000

    Add package simplesingletable/12.5.1/simplesingletable-12.5.1.tar.gz

---
+++ packages/simplesingletable/simplesingletable-12.5.1.tar.gz/simplesingletable-12.5.1/src/simplesingletable/__init__.py
@@ -0,0 +1,15 @@
+from .dynamodb_memory import (
+    DynamoDbMemory,
+    DynamoDbResource,
+    DynamoDbVersionedResource,
+    PaginatedList,
+    exhaust_pagination,
+)
+
+package_version = "12.5.1"
+
+_ = DynamoDbMemory
+_ = DynamoDbResource
+_ = DynamoDbVersionedResource
+_ = PaginatedList
+_ = exhaust_pagination
