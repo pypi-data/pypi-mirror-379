4058877febe88a0a3f220966e92120fcd622ec49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 06:06:37 +0000

    Add package ventricle/0.0.6/ventricle-0.0.6.tar.gz

---
+++ packages/ventricle/ventricle-0.0.6.tar.gz/ventricle-0.0.6/src/ventricle/worker/_head.py
@@ -0,0 +1,13 @@
+import asyncio
+from ._task_queue import TaskQueue
+from ._worker import Worker
+
+
+class HeadWorker(Worker):
+
+    def __init__(self, queue: TaskQueue) -> None:
+        super().__init__(queue)
+
+    async def _run_async(self):
+        await self.queue.start()
+        await asyncio.Event().wait()
