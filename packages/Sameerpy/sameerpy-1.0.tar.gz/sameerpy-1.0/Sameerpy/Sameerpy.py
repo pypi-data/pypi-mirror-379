27047ac5cb9ea291e099f15230ac920de82c02ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 05:06:05 +0000

    Add package Sameerpy/1.0/sameerpy-1.0.tar.gz

---
+++ packages/Sameerpy/sameerpy-1.0.tar.gz/sameerpy-1.0/Sameerpy/Sameerpy.py
@@ -0,0 +1,39 @@
+def plus(a,b):
+    add=a+b
+    return add
+
+
+
+def minus(a,b):
+    sub=a-b
+    return sub
+
+
+def x(a,b):
+    mul=a*b
+    return mul
+
+
+def div(a,b):
+    division=a/b
+    return division
+
+
+def currency_exchangedollar():
+    
+    convert=int(input("\nEnter your Value in PKR to convert : "))
+    
+    
+    print(f"Converted PKR into Dollar is {convert/280}")
+
+    
+
+
+
+
+
+
+
+
+
+
---
+++ packages/Sameerpy/sameerpy-1.0.tar.gz/sameerpy-1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Sameerpy",
+    version="1.0",
+    author="Syed Sameer Ali",
+    author_email="syedsameerali.official@gmail.com",
+    description="Test Package",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    entry_point={
+        "console_script" : [
+            "syedsameerali.official@gmail.com"
+            ],
+        },
+    )
