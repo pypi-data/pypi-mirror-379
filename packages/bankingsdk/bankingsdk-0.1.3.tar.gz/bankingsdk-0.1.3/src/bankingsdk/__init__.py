61320664f83026899544c5fe9b4645e253f724c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 06:06:53 +0000

    Add package bankingsdk/0.1.3/bankingsdk-0.1.3.tar.gz

---
+++ packages/bankingsdk/bankingsdk-0.1.3.tar.gz/bankingsdk-0.1.3/src/bankingsdk/__init__.py
@@ -0,0 +1,9 @@
+from .demandes import DemandeSimple, DemandeDetailed
+from .agences import AgenceSimple
+from .situations_pro import SituationProSimple
+from .situations_famille import SituationFamilialeSimple
+from .apports import ApportSimple
+from .analytics import AnalyticsResponse
+
+
+__all__ = ["DemandeClient", "BankConfig"]
