16838aebf2bc161750ba00da2eb5cc1eabcd9845
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 04:06:44 +0000

    Add package PYield/0.38.15/pyield-0.38.15-py3-none-any.whl

---
+++ packages/PYield/pyield-0.38.15-py3-none-any.whl/pyield/__init__.py
@@ -0,0 +1,32 @@
+import logging
+
+from pyield import anbima, b3, bc, bday, ibge, tn
+from pyield.__about__ import __version__
+from pyield.b3 import di1, futures
+from pyield.fwd import forward, forwards
+from pyield.interpolator import Interpolator
+from pyield.tn import lft, ltn, ntnb, ntnc, ntnf, pre
+
+__all__ = [
+    "__version__",
+    "anbima",
+    "bc",
+    "b3",
+    "ibge",
+    "tn",
+    "bday",
+    "di1",
+    "forwards",
+    "forward",
+    "futures",
+    "Interpolator",
+    "lft",
+    "ltn",
+    "ntnb",
+    "ntnc",
+    "ntnf",
+    "pre",
+]
+
+# Configura o logger do pacote principal com um NullHandler
+logging.getLogger(__name__).addHandler(logging.NullHandler())
