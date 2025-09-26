e30f34472fff4aeac29b301ce33ebdb0d7ec5c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 04:07:13 +0000

    Add package kakaomail/1.0.0/kakaomail-1.0.0.tar.gz

---
+++ packages/kakaomail/kakaomail-1.0.0.tar.gz/kakaomail-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kakaomail',
+    version='1.0.0',
+    description='send simple text using kakao mail',
+    license='MIT',
+    author='Kyunghonn',
+    author_email='aloecandy@gmail.com',
+    url='https://github.com/aloecandy/kakaomail',
+    keywords=['kakao', 'mail','korean'],
+    install_requires=[
+        'email>=6.0.0a1',
+        'python-dotenv'
+    ],
+    packages=find_packages(exclude=['tests'])
+)
