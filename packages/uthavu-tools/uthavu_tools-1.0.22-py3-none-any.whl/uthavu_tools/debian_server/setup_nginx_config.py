dd9b4797caa040cce1513641ea7cb931dfcdf7b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 14:09:19 +0000

    Add package uthavu-tools/1.0.22/uthavu_tools-1.0.22-py3-none-any.whl

---
+++ packages/uthavu-tools/uthavu_tools-1.0.22-py3-none-any.whl/uthavu_tools/debian_server/setup_nginx_config.py
@@ -0,0 +1,30 @@
+import subprocess
+import sys
+
+# Server details
+HOST = "72.60.97.244"      # e.g. 192.168.1.10 or domain
+USER = "root"   # e.g. root, debian, jawahar
+REMOTE_SCRIPT = "/etc/nginx/sites-available/create_nginx_config.py"  # Path to the script on the server
+#/etc/nginx/sites-available/create_nginx_config.py
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: python run_remote_nginx.py <domain> <port>")
+        sys.exit(1)
+
+    domain = sys.argv[1]
+    port = sys.argv[2]
+
+    # Run the script remotely
+    cmd = f"ssh {USER}@{HOST} 'sudo python3 {REMOTE_SCRIPT} {domain} {port}'"
+    print(f"👉 Running on {HOST}: {cmd}")
+
+    result = subprocess.run(cmd, shell=True, text=True)
+    if result.returncode != 0:
+        print(f"❌ Remote execution failed with exit code {result.returncode}")
+        sys.exit(result.returncode)
+
+    print("✅ Remote script executed successfully")
+
+if __name__ == "__main__":
+    main()
