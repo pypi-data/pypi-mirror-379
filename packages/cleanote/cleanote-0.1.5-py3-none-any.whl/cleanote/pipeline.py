059b675a87ad78d511fbf263e56eda7088b5d9c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 03:18:12 +0000

    Add package cleanote/0.1.5/cleanote-0.1.5-py3-none-any.whl

---
+++ packages/cleanote/cleanote-0.1.5-py3-none-any.whl/cleanote/pipeline.py
@@ -0,0 +1,21 @@
+class Pipeline:
+    def __init__(self, dataset, model_h, model_v):
+        self.dataset = dataset
+        self.model_h = model_h
+        self.model_v = model_v
+
+    def apply(self):
+        print("[Pipeline] Starting pipeline...")
+
+        prompt_h = "please count the number of words in the following text:"
+        print("[Pipeline] Start Homogenization...")
+        dataset_h = self.model_h.run(self.dataset, prompt_h)
+        print("[Pipeline] Homogenization completed.")
+
+        prompt_v = "please verify the following text is not empty:"
+        print("[Pipeline] Start Verification...")
+        dataset_v = self.model_v.run(dataset_h, prompt_v)
+        print("[Pipeline] Verification completed.")
+
+        print("[Pipeline] Pipeline completed.")
+        return dataset_v
