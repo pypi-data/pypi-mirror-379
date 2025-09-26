24a83f55d192458830abc59b7ca0c4371fd77712
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 05:06:38 +0000

    Add package dekho-cli/1.0.3/dekho_cli-1.0.3.tar.gz

---
+++ packages/dekho-cli/dekho_cli-1.0.3.tar.gz/dekho_cli-1.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dekho-cli",
+    version="1.0.3",
+    packages=find_packages(),
+    install_requires=["requests"],
+    entry_points={
+        "console_scripts": [
+            "dekho-cli = dekho_cli.main:main"
+        ]
+    },
+    author="dev1abhi",
+    author_email="opguddu21@gmail.com",
+    description="CLI tool to fetch and play sports channels via mpv.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
