07576be1d036b1a8adf6d742d84d80ff1116d0ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 06:07:01 +0000

    Add package rrai/1.0.0/rrai-1.0.0.tar.gz

---
+++ packages/rrai/rrai-1.0.0.tar.gz/rrai-1.0.0/rrai/__init__.py
@@ -0,0 +1,27 @@
+from .case_studies import (
+    get_case_study,
+    print_case_study,
+    get_all_case_studies,
+    AMAZON_AI_HIRING_BIAS,
+    APPLE_CARD_GENDER_BIAS,
+    GOOGLE_PHOTOS_TAGGING,
+    HEALTHCARE_ALGORITHM_BIAS,
+    AI_DEEPFAKE_SCAMS,
+    CHATGPT_HALLUCINATIONS
+)
+
+__version__ = "1.0.0"
+__author__ = "Manny"
+__email__ = "rrai@rrai.com"
+
+__all__ = [
+    'get_case_study',
+    'print_case_study', 
+    'get_all_case_studies',
+    'AMAZON_AI_HIRING_BIAS',
+    'APPLE_CARD_GENDER_BIAS',
+    'GOOGLE_PHOTOS_TAGGING',
+    'HEALTHCARE_ALGORITHM_BIAS',
+    'AI_DEEPFAKE_SCAMS',
+    'CHATGPT_HALLUCINATIONS'
+]
---
+++ packages/rrai/rrai-1.0.0.tar.gz/rrai-1.0.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rrai",
+    version="1.0.0",
+    description="A Python library",
+    long_description="Yes you are thinking right!",
+    long_description_content_type="text/plain",
+    author="Manny",
+    author_email="rrai@example.com",
+    url="https://github.com/rrai",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Education",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research", 
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Education",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires=">=3.6",
+    install_requires=[],
+    project_urls={
+        "Bug Reports": "https://github.com/rrai/issues",
+        "Source": "https://github.com/rrai",
+    },
+)
