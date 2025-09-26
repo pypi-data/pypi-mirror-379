267bc4a9cec404e385fbb293dffdcf2d44e31f0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 04:06:45 +0000

    Add package forteenall-kit/0.1.39/forteenall_kit-0.1.39-py3-none-any.whl

---
+++ packages/forteenall-kit/forteenall_kit-0.1.39-py3-none-any.whl/forteenall_kit/invokers/nextjs_hero_component.py
@@ -0,0 +1,13 @@
+class CTAButtonData:
+    pass
+
+class HeroFeatureData:
+    pass
+
+class Feature:
+    def __init__(self, name, manager, options, invokerType):
+        pass
+    def generate_tsx_content(self):
+        pass
+    def execute(self):
+        pass
