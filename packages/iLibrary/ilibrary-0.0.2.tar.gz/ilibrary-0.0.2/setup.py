3b42a4547429d730fbe7147dfed7d5673e6c3605
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 05:06:13 +0000

    Add package iLibrary/0.0.2/ilibrary-0.0.2.tar.gz

---
+++ packages/iLibrary/ilibrary-0.0.2.tar.gz/ilibrary-0.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setup(  # Note: no 'setuptools.' prefix here
+    name = "iLibrary",
+    version = "0.0.2",
+    author = "Andreas Legner",
+    author_email = "iLibrary@legner.beer",
+    description = "iLibrary - Tools for IBM i",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://legner.beer",
+    project_urls = {
+        "Bug Tracker": "https://legner.beer/bugs",
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir = {"": "app"},
+    packages = find_packages(where="app"),
+    python_requires = ">=3.6"
+)
