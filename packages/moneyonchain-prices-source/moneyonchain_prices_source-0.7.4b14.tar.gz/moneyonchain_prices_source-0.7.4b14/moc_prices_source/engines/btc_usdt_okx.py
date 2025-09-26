eef5c2427f763f281c5463b36d3725f72cfe8b00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 04:06:24 +0000

    Add package moneyonchain-prices-source/0.7.4b14/moneyonchain_prices_source-0.7.4b14.tar.gz

---
+++ packages/moneyonchain-prices-source/moneyonchain_prices_source-0.7.4b14.tar.gz/moneyonchain_prices_source-0.7.4b14/moc_prices_source/engines/btc_usdt_okx.py
@@ -0,0 +1,25 @@
+from engine_base import Base, BTC_USDT, Decimal
+
+
+class Engine(Base):
+
+    _name        = Base._name_from_file(__file__)
+    _description = "OKX"
+    _uri         = "https://www.okx.com/api/v5/market/ticker?instId=BTC-USDT"
+    _coinpair    = BTC_USDT
+
+    def _map(self, data):
+        data = data['data'][0]        
+        return {
+            'price': (Decimal(data['askPx']) +
+                      Decimal(data['bidPx'])) / Decimal('2')
+        }
+
+
+if __name__ == '__main__':
+    print("File: {}, Ok!".format(repr(__file__)))
+    engine = Engine()
+    engine()
+    print(engine)
+    if engine.error:
+        print(engine.error)
