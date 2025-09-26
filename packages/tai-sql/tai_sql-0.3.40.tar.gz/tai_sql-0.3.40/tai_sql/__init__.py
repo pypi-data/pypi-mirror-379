50c53d4289755d9958d5558d630c5dc97f7bc87f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 05:06:05 +0000

    Add package tai-sql/0.3.40/tai_sql-0.3.40.tar.gz

---
+++ packages/tai-sql/tai_sql-0.3.40.tar.gz/tai_sql-0.3.40/tai_sql/__init__.py
@@ -0,0 +1,44 @@
+"""
+Declarative models for SQLAlchemy.
+This module provides the base classes and utilities to define
+models using SQLAlchemy's declarative system.
+"""
+from __future__ import annotations
+from sqlalchemy.types import (
+    BigInteger as bigint,
+    Text as text,
+    Numeric as numeric,
+    LargeBinary as largebinary,
+)
+
+from datetime import datetime, date, time
+from typing import List, Optional
+
+from .project import ProjectManager as pm
+from .core import datasource, generate, env, connection_string, params, query
+from .orm import Table, View, Enum, column, relation
+from .drivers import postgresql, mysql, sqlserver
+
+# Exportar los elementos principales
+__all__ = [
+    'datasource', 
+    'generate',
+    'env',
+    'connection_string',
+    'params',
+    'Table',
+    'column',
+    'relation',
+    'List',
+    'Optional',
+    'datetime',
+    'date',
+    'time',
+    'bigint',
+    'text',
+    'numeric',
+    'largebinary',
+    'query',
+    'View',
+    'Enum',
+]
