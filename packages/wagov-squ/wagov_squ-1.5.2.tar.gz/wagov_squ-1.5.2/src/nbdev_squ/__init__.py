6fa3c1df562ac586f446df99b1983dd5244c19ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 15:06:54 +0000

    Add package wagov-squ/1.5.2/wagov_squ-1.5.2.tar.gz

---
+++ packages/wagov-squ/wagov_squ-1.5.2.tar.gz/wagov_squ-1.5.2/src/nbdev_squ/__init__.py
@@ -0,0 +1,20 @@
+"""
+Compatibility module for backwards compatibility.
+This allows existing code to continue using 'from nbdev_squ import ...'
+"""
+
+import sys
+import warnings
+
+import wagov_squ
+
+# Issue a deprecation warning
+warnings.warn(
+    "The 'nbdev_squ' import name is deprecated. Please use 'wagov_squ' instead. "
+    "Support for 'nbdev_squ' will be removed in a future version.",
+    DeprecationWarning,
+    stacklevel=2,
+)
+
+# Make nbdev_squ act as an alias to wagov_squ
+sys.modules[__name__] = wagov_squ
