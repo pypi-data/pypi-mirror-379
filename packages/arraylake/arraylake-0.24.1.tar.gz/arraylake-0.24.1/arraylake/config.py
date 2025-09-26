807543aa889c0b1bb853e6e50d562d4868242eab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 05:06:46 +0000

    Add package arraylake/0.24.1/arraylake-0.24.1.tar.gz

---
+++ packages/arraylake/arraylake-0.24.1.tar.gz/arraylake-0.24.1/arraylake/config.py
@@ -0,0 +1,21 @@
+import os
+from pathlib import Path
+
+import yaml
+from donfig import Config
+
+new_user_config_dir = Path("~/.config/arraylake").expanduser()
+
+user_config_dir = Path(os.getenv("ARRAYLAKE_CONFIG", new_user_config_dir)).expanduser()
+user_config_file = user_config_dir / "config.yaml"
+
+fn = Path(__file__).resolve().parent / "config.yaml"
+with fn.open() as f:
+    defaults = yaml.safe_load(f)
+config = Config("arraylake", paths=[user_config_dir], defaults=[defaults])
+
+# maybe move a copy of the defaults to the user config space
+config.ensure_file(
+    source=fn,
+    comment=True,
+)
