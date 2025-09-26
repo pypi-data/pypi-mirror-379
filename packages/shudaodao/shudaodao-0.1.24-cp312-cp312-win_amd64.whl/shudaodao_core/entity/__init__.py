c3f46065f1b3451a988943b0542c4f4516bd9659
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 05:06:43 +0000

    Add package shudaodao/0.1.24/shudaodao-0.1.24-cp312-cp312-win_amd64.whl

---
+++ packages/shudaodao/shudaodao-0.1.24-cp312-cp312-win_amd64.whl/shudaodao_core/entity/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# @License  ：(C)Copyright 2025, 数道智融科技
+# @Author   ：李锋
+# @Software ：PyCharm
+# @Date     ：2025/9/21 上午11:39
+# @Desc     ：
+
+def get_schema_name():
+    return "shudaodao_core"
