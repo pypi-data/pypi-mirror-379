32461e9c7d32b7f88233d6a2a6b37d45dfc0ac5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 04:06:43 +0000

    Add package sycommon-python-lib/0.1.13/sycommon_python_lib-0.1.13-py3-none-any.whl

---
+++ packages/sycommon-python-lib/sycommon_python_lib-0.1.13-py3-none-any.whl/sycommon/middleware/cors.py
@@ -0,0 +1,14 @@
+from fastapi.middleware.cors import CORSMiddleware
+
+
+def setup_cors_handler(app):
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+        expose_headers=["*"],
+    )
+
+    return app
