ba9944a6e10c8a5395ac36d12070e9c8aef7756f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 03:18:04 +0000

    Add package invenio-records-resources/8.6.1/invenio_records_resources-8.6.1-py2.py3-none-any.whl

---
+++ packages/invenio-records-resources/invenio_records_resources-8.6.1-py2.py3-none-any.whl/invenio_records_resources/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020-2025 CERN.
+# Copyright (C) 2024-2025 Graz University of Technology.
+#
+# Invenio-Records-Resources is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Invenio Resources module to create REST APIs."""
+
+from .ext import InvenioRecordsResources
+
+__version__ = "8.6.1"
+
+__all__ = ("__version__", "InvenioRecordsResources")
