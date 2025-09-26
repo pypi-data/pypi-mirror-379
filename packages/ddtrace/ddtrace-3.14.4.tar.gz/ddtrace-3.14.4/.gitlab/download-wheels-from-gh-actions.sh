34d351b6cb820ef4a101198d2afbbbcb8a8d09e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 16:07:53 +0000

    Add package ddtrace/3.14.4/ddtrace-3.14.4.tar.gz

---
+++ packages/ddtrace/ddtrace-3.14.4.tar.gz/ddtrace-3.14.4/.gitlab/download-wheels-from-gh-actions.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+set -eo pipefail
+
+get_run_id() {
+    RUN_ID=$(
+        gh run ls \
+        --repo DataDog/dd-trace-py \
+        --commit="$CI_COMMIT_SHA" \
+        $([ -z "$TRIGGERING_EVENT" ] && echo "" || echo "--event=$TRIGGERING_EVENT") \
+        --workflow=build_deploy.yml \
+        --json databaseId \
+        --jq "first (.[]) | .databaseId"
+    )
+}
+
+if [ -z "$CI_COMMIT_SHA" ]; then
+  echo "Error: CI_COMMIT_SHA was not provided"
+  exit 1
+fi
+
+if [ -v "$CI_COMMIT_TAG" ]; then
+    TRIGGERING_EVENT="release"
+fi
+
+get_run_id
+
+if [ -z "$RUN_ID" ]; then
+  echo "No RUN_ID found waiting for job to start"
+  # The job has not started yet. Give it time to start
+  sleep 180 # 3 minutes
+
+  echo "Querying for RUN_ID"
+
+  timeout=600 # 10 minutes
+  start_time=$(date +%s)
+  end_time=$((start_time + timeout))
+  # Loop for 10 minutes waiting for run to appear in github
+  while [ $(date +%s) -lt $end_time ]; do
+    get_run_id
+    if [ -n "$RUN_ID" ]; then
+      break;
+    fi
+    echo "Waiting for RUN_ID"
+    sleep 60
+  done
+fi
+
+if [ -z "$RUN_ID" ]; then
+  echo "RUN_ID not found. Check if the GitHub build jobs were successfully triggered on your PR. Usually closing and re-opening your PR will resolve this issue."
+  exit 1
+fi
+
+echo "Found RUN_ID: $RUN_ID"
+
+mkdir pywheels
+cd pywheels
+
+if [[ $(gh run view $RUN_ID --exit-status --json status --jq .status) != "completed" ]]; then
+  echo "Waiting for workflow to finish"
+
+  # Give time to the job to finish
+  sleep 300 # 5 minutes
+
+  # wait for run to finish
+  gh run watch $RUN_ID --interval 60 --exit-status 1 --repo DataDog/dd-trace-py
+fi
+
+echo "Github workflow finished. Downloading wheels"
+# download all wheels
+gh run download $RUN_ID --repo DataDog/dd-trace-py --pattern "wheels-*" --pattern "source-dist*"
+
+cd ..
+
+echo "Finished downloading wheels. Fixing directory structure"
+
+# Flatten directory structure so all wheels are top level
+find pywheels -type f -exec mv {} pywheels \;
+
+echo "Done"
---
+++ packages/ddtrace/ddtrace-3.14.4.tar.gz/ddtrace-3.14.4/tests/appsec/appsec_utils.py
@@ -0,0 +1,283 @@
+from contextlib import contextmanager
+import os
+from pathlib import Path
+import signal
+import subprocess
+import sys
+
+from requests.exceptions import ConnectionError  # noqa: A004
+
+from ddtrace.appsec._constants import IAST
+from ddtrace.internal.compat import PYTHON_VERSION_INFO
+from ddtrace.internal.utils.retry import RetryError
+from ddtrace.vendor import psutil
+from tests.utils import _build_env
+from tests.webclient import Client
+
+
+FILE_PATH = Path(__file__).resolve().parent
+
+
+@contextmanager
+def gunicorn_server(
+    use_ddtrace_cmd=True,
+    appsec_enabled="true",
+    iast_enabled="false",
+    remote_configuration_enabled="true",
+    tracer_enabled="true",
+    apm_tracing_enabled="true",
+    token=None,
+    port=8000,
+    workers="1",
+    use_threads=False,
+    use_gevent=False,
+    assert_debug=False,
+    env=None,
+):
+    cmd = ["gunicorn", "-w", workers, "--log-level", "debug"]
+    if use_ddtrace_cmd:
+        cmd = ["python", "-m", "ddtrace.commands.ddtrace_run"] + cmd
+    if use_threads:
+        cmd += ["--threads", "1"]
+    if use_gevent:
+        cmd += ["-k", "gevent"]
+    cmd += ["-b", "0.0.0.0:%s" % port, "tests.appsec.app:app"]
+    yield from appsec_application_server(
+        cmd,
+        appsec_enabled=appsec_enabled,
+        iast_enabled=iast_enabled,
+        apm_tracing_enabled=apm_tracing_enabled,
+        remote_configuration_enabled=remote_configuration_enabled,
+        tracer_enabled=tracer_enabled,
+        token=token,
+        env=env,
+        port=port,
+        assert_debug=assert_debug,
+    )
+
+
+@contextmanager
+def flask_server(
+    python_cmd="python",
+    appsec_enabled="false",
+    remote_configuration_enabled="true",
+    iast_enabled="false",
+    tracer_enabled="true",
+    apm_tracing_enabled=None,
+    token=None,
+    app="tests/appsec/app.py",
+    env=None,
+    port=8000,
+    assert_debug=False,
+    manual_propagation_debug=False,
+    use_ddtrace_cmd=True,
+):
+    cmd = [python_cmd, app, "--no-reload"]
+    if use_ddtrace_cmd:
+        cmd = [python_cmd, "-m", "ddtrace.commands.ddtrace_run"] + cmd
+    yield from appsec_application_server(
+        cmd,
+        appsec_enabled=appsec_enabled,
+        apm_tracing_enabled=apm_tracing_enabled,
+        remote_configuration_enabled=remote_configuration_enabled,
+        iast_enabled=iast_enabled,
+        tracer_enabled=tracer_enabled,
+        token=token,
+        env=env,
+        port=port,
+        assert_debug=assert_debug,
+        manual_propagation_debug=manual_propagation_debug,
+    )
+
+
+@contextmanager
+def django_server(
+    python_cmd="python",
+    appsec_enabled="false",
+    remote_configuration_enabled="true",
+    iast_enabled="false",
+    tracer_enabled="true",
+    apm_tracing_enabled=None,
+    token=None,
+    port=8000,
+    env=None,
+    assert_debug=False,
+    manual_propagation_debug=False,
+):
+    """
+    Context manager that runs a Django test server in a subprocess.
+
+    This server uses the Django test application located in tests/appsec/integrations/django_tests/django_app.
+    The server is started when entering the context and stopped when exiting.
+    """
+    manage_py = "tests/appsec/integrations/django_tests/django_app/manage.py"
+    cmd = [
+        python_cmd,
+        "-m",
+        "ddtrace.commands.ddtrace_run",
+        python_cmd,
+        manage_py,
+        "runserver",
+        f"0.0.0.0:{port}",
+        "--noreload",
+    ]
+    yield from appsec_application_server(
+        cmd,
+        appsec_enabled=appsec_enabled,
+        apm_tracing_enabled=apm_tracing_enabled,
+        remote_configuration_enabled=remote_configuration_enabled,
+        iast_enabled=iast_enabled,
+        tracer_enabled=tracer_enabled,
+        token=token,
+        port=port,
+        env=env,
+        assert_debug=assert_debug,
+        manual_propagation_debug=manual_propagation_debug,
+    )
+
+
+@contextmanager
+def uvicorn_server(
+    python_cmd="python",
+    appsec_enabled="false",
+    remote_configuration_enabled="true",
+    iast_enabled="false",
+    tracer_enabled="true",
+    apm_tracing_enabled=None,
+    token=None,
+    app="tests.appsec.integrations.fastapi_tests.app:app",
+    env=None,
+    port=8000,
+    assert_debug=False,
+    manual_propagation_debug=False,
+):
+    """
+    Context manager that runs a FastAPI test server in a subprocess using Uvicorn.
+
+    This server uses the FastAPI test application located in tests/appsec/integrations/fastapi_tests.
+    The server is started when entering the context and stopped when exiting.
+    """
+    cmd = [
+        python_cmd,
+        "-m",
+        "ddtrace.commands.ddtrace_run",
+        "uvicorn",
+        app,
+        "--host",
+        "0.0.0.0",
+        "--port",
+        str(port),
+        "--no-access-log",
+    ]
+    yield from appsec_application_server(
+        cmd,
+        appsec_enabled=appsec_enabled,
+        apm_tracing_enabled=apm_tracing_enabled,
+        remote_configuration_enabled=remote_configuration_enabled,
+        iast_enabled=iast_enabled,
+        tracer_enabled=tracer_enabled,
+        token=token,
+        port=port,
+        env=env,
+        assert_debug=assert_debug,
+        manual_propagation_debug=manual_propagation_debug,
+    )
+
+
+def appsec_application_server(
+    cmd,
+    appsec_enabled="true",
+    remote_configuration_enabled="true",
+    iast_enabled="false",
+    tracer_enabled="true",
+    apm_tracing_enabled=None,
+    token=None,
+    env=None,
+    port=8000,
+    assert_debug=False,
+    manual_propagation_debug=False,
+):
+    env = _build_env(env, file_path=FILE_PATH)
+    env["DD_REMOTE_CONFIG_POLL_INTERVAL_SECONDS"] = "0.5"
+    env["DD_REMOTE_CONFIGURATION_ENABLED"] = remote_configuration_enabled
+    if token:
+        env["_DD_REMOTE_CONFIGURATION_ADDITIONAL_HEADERS"] = "X-Datadog-Test-Session-Token:%s," % (token,)
+        env["_DD_TRACE_WRITER_ADDITIONAL_HEADERS"] = "X-Datadog-Test-Session-Token:{}".format(token)
+    if appsec_enabled is not None:
+        env["DD_APPSEC_ENABLED"] = appsec_enabled
+    if apm_tracing_enabled is not None:
+        env["DD_APM_TRACING_ENABLED"] = apm_tracing_enabled
+    if iast_enabled is not None and iast_enabled != "false":
+        env[IAST.ENV] = iast_enabled
+        env[IAST.ENV_REQUEST_SAMPLING] = "100"
+        env["DD_IAST_DEDUPLICATION_ENABLED"] = "false"
+        env["_DD_IAST_PATCH_MODULES"] = "tests.appsec."
+        env[IAST.ENV_NO_DIR_PATCH] = "false"
+        if assert_debug:
+            env["_" + IAST.ENV_DEBUG] = iast_enabled
+            env["_" + IAST.ENV_PROPAGATION_DEBUG] = iast_enabled
+            env["DD_TRACE_DEBUG"] = iast_enabled
+    if tracer_enabled is not None:
+        env["DD_TRACE_ENABLED"] = tracer_enabled
+    env["DD_TRACE_AGENT_URL"] = os.environ.get("DD_TRACE_AGENT_URL", "")
+    env["FLASK_RUN_PORT"] = str(port)
+
+    subprocess_kwargs = {
+        "env": env,
+        "start_new_session": True,
+        "stdout": sys.stdout,
+        "stderr": sys.stderr,
+    }
+    if assert_debug:
+        if not manual_propagation_debug:
+            subprocess_kwargs["stdout"] = subprocess.PIPE
+            subprocess_kwargs["stderr"] = subprocess.PIPE
+        subprocess_kwargs["text"] = True
+
+    server_process = subprocess.Popen(cmd, **subprocess_kwargs)
+    try:
+        client = Client("http://0.0.0.0:%s" % port)
+
+        try:
+            print("Waiting for server to start")
+            client.wait(max_tries=120, delay=0.1, initial_wait=1.0)
+            print("Server started")
+        except RetryError:
+            raise AssertionError(
+                "Server failed to start, see stdout and stderr logs"
+                "\n=== Captured STDOUT ===\n%s=== End of captured STDOUT ==="
+                "\n=== Captured STDERR ===\n%s=== End of captured STDERR ==="
+                % (server_process.stdout, server_process.stderr)
+            )
+        except Exception:
+            raise AssertionError(
+                "Server FAILED, see stdout and stderr logs"
+                "\n=== Captured STDOUT ===\n%s=== End of captured STDOUT ==="
+                "\n=== Captured STDERR ===\n%s=== End of captured STDERR ==="
+                % (server_process.stdout, server_process.stderr)
+            )
+
+        # If we run a Gunicorn application, we want to get the child's pid, see test_flask_remoteconfig.py
+        parent = psutil.Process(server_process.pid)
+        children = parent.children(recursive=True)
+
+        yield server_process, client, (children[1].pid if len(children) > 1 else None)
+        try:
+            client.get_ignored("/shutdown")
+        except ConnectionError:
+            pass
+        except Exception:
+            raise AssertionError(
+                "\n=== Captured STDOUT ===\n%s=== End of captured STDOUT ==="
+                "\n=== Captured STDERR ===\n%s=== End of captured STDERR ==="
+                % (server_process.stdout, server_process.stderr)
+            )
+    finally:
+        os.killpg(os.getpgid(server_process.pid), signal.SIGTERM)
+        server_process.terminate()
+        server_process.wait()
+        if (assert_debug and PYTHON_VERSION_INFO >= (3, 10)) and (iast_enabled is not None and iast_enabled != "false"):
+            process_output = server_process.stderr.read()
+            assert "Return from " in process_output
+            assert "Return value is tainted" in process_output
+            assert "Tainted arguments:" in process_output
