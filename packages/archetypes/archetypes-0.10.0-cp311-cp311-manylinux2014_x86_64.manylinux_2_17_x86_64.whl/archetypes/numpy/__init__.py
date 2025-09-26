012be2968a642dd6b9cae336351c49fbfc32beb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 16:08:30 +0000

    Add package archetypes/0.10.0/archetypes-0.10.0-cp311-cp311-manylinux2014_x86_64.manylinux_2_17_x86_64.whl

---
+++ packages/archetypes/archetypes-0.10.0-cp311-cp311-manylinux2014_x86_64.manylinux_2_17_x86_64.whl/archetypes/numpy/__init__.py
@@ -0,0 +1,17 @@
+from ._aa import AA
+from ._ada import ADA
+from ._biaa import BiAA
+from ._fair_aa import FairAA
+from ._fair_kernel_aa import FairKernelAA
+from ._kernel_aa import KernelAA
+from ._sym_biaa import SymmetricBiAA
+
+__all__ = [
+    "AA",
+    "FairAA",
+    "ADA",
+    "BiAA",
+    "SymmetricBiAA",
+    "KernelAA",
+    "FairKernelAA",
+]
