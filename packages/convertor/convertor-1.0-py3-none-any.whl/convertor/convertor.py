c39b7269420fc60574d6db946323324877f9830e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 05:06:10 +0000

    Add package convertor/1.0/convertor-1.0-py3-none-any.whl

---
+++ packages/convertor/convertor-1.0-py3-none-any.whl/convertor/convertor.py
@@ -0,0 +1,12 @@
+print("Function for currency convertor\n use rtodo(a),rtodi(b),rtoli(c) ")
+
+def rtodo(a):
+    do=a*280
+    return do
+def rtodi(b):
+    di=b*76
+    return di
+def rtoli(c):
+    li=c*7
+    return li
+    
