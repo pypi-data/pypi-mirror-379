9914466922ecbc7b9d7e5035a4e95fb6079d4a59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 14:07:57 +0000

    Add package Uw-Django-Oidc/1.0.1/uw_django_oidc-1.0.1-py3-none-any.whl

---
+++ packages/Uw-Django-Oidc/uw_django_oidc-1.0.1-py3-none-any.whl/uw_oidc/logger.py
@@ -0,0 +1,16 @@
+# Copyright 2025 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+from django.conf import settings
+
+enable_logging = getattr(settings, 'UW_OIDC_ENABLE_LOGGING', None)
+
+
+def log_err(logger, msg):
+    if enable_logging:
+        logger.error(msg)
+
+
+def log_info(logger, msg):
+    if enable_logging:
+        logger.info(msg)
