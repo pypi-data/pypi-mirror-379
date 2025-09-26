49ade2387aae5e912b33a54b9ebde00fbedd5b9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 05:06:01 +0000

    Add package rafay1/1.0/rafay1-1.0.tar.gz

---
+++ packages/rafay1/rafay1-1.0.tar.gz/rafay1-1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rafay1",
+    version="1.0",
+    author="M.Rafay Shaikh",
+    author_email="rafayshaikh633@gmail.com",
+    description="This is just a test package",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    entry_point={
+        "console_script" : [
+            "rafayshaikh633@gmail.com"
+            ],
+        },
+    
+    )
