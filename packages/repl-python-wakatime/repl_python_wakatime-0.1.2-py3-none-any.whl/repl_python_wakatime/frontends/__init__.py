8547f7714a14af7fbe3c8fd4eab8552f822f58de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 16:08:40 +0000

    Add package repl-python-wakatime/0.1.2/repl_python_wakatime-0.1.2-py3-none-any.whl

---
+++ packages/repl-python-wakatime/repl_python_wakatime-0.1.2-py3-none-any.whl/repl_python_wakatime/frontends/__init__.py
@@ -0,0 +1,19 @@
+r"""Frontends
+=============
+"""
+
+import asyncio
+from dataclasses import dataclass
+
+from ..backends import Hook
+
+
+@dataclass
+class Repl:
+    hook: Hook
+
+    def __post_init__(self) -> None:
+        self.hook.frontend = self.__class__.__name__.lower()
+
+    def __call__(self):
+        asyncio.run(self.hook())
