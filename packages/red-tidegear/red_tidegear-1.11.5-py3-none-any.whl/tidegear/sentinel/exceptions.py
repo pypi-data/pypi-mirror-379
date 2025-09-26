69c719e5aa37b5bf279102bb432df659617e89c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 04:06:48 +0000

    Add package red-tidegear/1.11.5/red_tidegear-1.11.5-py3-none-any.whl

---
+++ packages/red-tidegear/red_tidegear-1.11.5-py3-none-any.whl/tidegear/sentinel/exceptions.py
@@ -0,0 +1,27 @@
+# This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+# If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+# © 2025 cswimr
+
+"""This module contains exceptions used within Sentinel and consuming cogs."""
+
+from tidegear.exceptions import ShowToEndUserError, TidegearError
+
+
+class HandlerError(ShowToEndUserError):
+    """Raised whenever a moderation handler wants to show an error message to the end user."""
+
+
+class LoggedHandlerError(ShowToEndUserError):
+    """Raised whenever a moderation handler wants to show an error message to the end user, while still logging that error for bot owners to see."""
+
+
+class UpsertError(TidegearError):
+    """Raised whenever an upsert operation falis."""
+
+
+class UnsetError(TidegearError):
+    """Raised when attempting to access a database entry that is unset."""
+
+
+class NotReadyError(TidegearError):
+    """Raised when attempting to expire a moderation case that isn't ready to expire."""
