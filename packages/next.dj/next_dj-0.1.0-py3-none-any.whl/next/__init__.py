42a13642f29be5cca85398142e3e3a41d8f367b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-26 14:09:19 +0000

    Add package next.dj/0.1.0/next_dj-0.1.0-py3-none-any.whl

---
+++ packages/next.dj/next_dj-0.1.0-py3-none-any.whl/next/__init__.py
@@ -0,0 +1,19 @@
+r"""                  __           __
+                     /\ \__       /\ \    __
+  ___      __   __  _\ \ ,_\      \_\ \  /\_\
+/' _ `\  /'__`\/\ \/'\\ \ \/      /'_` \ \/\ \
+/\ \/\ \/\  __/\/>  </ \ \ \_  __/\ \L\ \ \ \ \
+\ \_\ \_\ \____\/\_/\_\ \ \__\/\_\ \___,_\_\ \ \
+ \/_/\/_/\/____/\//\/_/  \/__/\/_/\/__,_ /\ \_\ \
+                                         \ \____/
+                                          \/___/
+
+Django next generation framework without the tears: tweak UI, bind data, and animate in 2 lines.
+Ditch tons of JS — grab ready-made and roll!
+"""
+
+__title__ = "Next Django Framework"
+__version__ = "0.1.0"
+__author__ = "paqstd-dev"
+
+VERSION = __version__
