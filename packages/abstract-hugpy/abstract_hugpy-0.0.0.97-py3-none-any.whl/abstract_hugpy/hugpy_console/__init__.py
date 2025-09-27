42202aed7f8fd681195af91f3301bc2be7e2dd71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 06:07:20 +0000

    Add package abstract-hugpy/0.0.0.97/abstract_hugpy-0.0.0.97-py3-none-any.whl

---
+++ packages/abstract-hugpy/abstract_hugpy-0.0.0.97-py3-none-any.whl/abstract_hugpy/hugpy_console/__init__.py
@@ -0,0 +1,5 @@
+from .hugging_face_models import *
+from .video_console import *
+from .hugpy_flasks import *
+
+
---
+++ packages/abstract-hugpy/abstract_hugpy-0.0.0.97-py3-none-any.whl/abstract_hugpy/hugpy_proxyvideo_wsgi.py
@@ -0,0 +1,4 @@
+from hugpy_console.hugpy_flasks.hugpy_proxyvideo_flask_app import hugpy_proxyvideo_app
+
+app = hugpy_proxyvideo_app()
+
