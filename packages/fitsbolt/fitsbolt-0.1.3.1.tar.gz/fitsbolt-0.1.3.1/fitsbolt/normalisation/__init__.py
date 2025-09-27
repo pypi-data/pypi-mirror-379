7338b060fc62ffad9bcabd30f0adebb675c366aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 03:13:40 +0000

    Add package fitsbolt/0.1.3.1/fitsbolt-0.1.3.1.tar.gz

---
+++ packages/fitsbolt/fitsbolt-0.1.3.1.tar.gz/fitsbolt-0.1.3.1/fitsbolt/normalisation/__init__.py
@@ -0,0 +1,23 @@
+# fitsbolt - A Python package for image loading and processing
+# Copyright (C) <2025>  <Ruhberg>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from .NormalisationMethod import NormalisationMethod
+from .normalisation import normalise_images
+
+__all__ = [
+    "NormalisationMethod",
+    "normalise_images",
+]
