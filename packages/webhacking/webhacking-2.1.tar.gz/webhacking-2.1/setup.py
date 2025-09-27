a0e3ba5dbfc90acb4fdc2a8933e533b1de9600da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 03:13:26 +0000

    Add package webhacking/2.1/webhacking-2.1.tar.gz

---
+++ packages/webhacking/webhacking-2.1.tar.gz/webhacking-2.1/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='webhacking',
+    version='2.1',
+    author='vova2f',
+    author_email='vovk4756@gmail.com',
+    description='взломай сайт',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/vova2f/webhacking',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=2.0',
+    install_requires=[
+        'somepackage>=1.1.0',
+        'ipaddress==1.0.23',
+        'socket',
+        'trio-websocket==0.12.2',
+        'colorama==0.4.2',
+        'requests==2.32.5',
+        'threading',
+        'websocket-client==1.8.0'
+    ]
+)
