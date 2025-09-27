4a7d1e26b83fc85c72d967adac5b6e66576cf589
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 07:05:46 +0000

    Add package pythonic-fp/3.1.0/pythonic_fp-3.1.0-py3-none-any.whl

---
+++ packages/pythonic-fp/pythonic_fp-3.1.0-py3-none-any.whl/pythonic_fp/homepage/__init__.py
@@ -0,0 +1,38 @@
+"""
+Pythonic FP Project
+===================
+
+PyPI project pythonic-fp itself consists of a PyPI project and GitHub repo
+both named pythonic-fp.
+
+It has several purposes.
+
+- claims the pythonic-fp name on PyPI for the overall Pythonic FP effort
+- hosts on its GitHub Pages
+
+  - detailed Sphinx based documentation for each pythonic_fp project
+  - a homepage for the effort
+
+- provides a location on GitHub for
+
+  - CHANGELOG.rst for the overall Pythonic FP effort
+
+    - each individual Pythonic FP project has its on CHANGELOG.rst
+
+  - README.md for the overall Pythonic FP effort
+
+    - produces list of links to all the Pythonic FP PyPI projects
+    - populates the description on PyPI for pythonic-fp
+
+      - overall describing the effort
+      - naming the individual Pythonic FP projects on PyPI
+
+The pythonic_fp.homepage module is used to construct the projects homepage.
+
+There is no need to install it, but does not hurt anything if it is installed.
+"""
+
+__author__ = 'Geoffrey R. Scheller'
+__copyright__ = 'Copyright (c) 2023-2025 Geoffrey R. Scheller'
+__license__ = 'Apache License 2.0'
+
