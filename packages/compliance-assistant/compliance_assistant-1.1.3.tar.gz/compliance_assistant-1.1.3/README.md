61df2539d8b73a4ff3a87dcb2bb0a27880d33dde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 04:06:55 +0000

    Add package compliance-assistant/1.1.3/compliance_assistant-1.1.3.tar.gz

---
+++ packages/compliance-assistant/compliance_assistant-1.1.3.tar.gz/compliance_assistant-1.1.3/README.md
@@ -0,0 +1,177 @@
+<!--
+SPDX-FileCopyrightText: 2024 DB Systel GmbH
+
+SPDX-License-Identifier: Apache-2.0
+-->
+
+# Compliance Assistant
+
+![OpenRail Administrative Project](https://openrailassociation.org/badges/openrail-project-admin.svg)
+[![Test suites](https://github.com/OpenRailAssociation/compliance-assistant/actions/workflows/test.yaml/badge.svg)](https://github.com/OpenRailAssociation/compliance-assistant/actions/workflows/test.yaml)
+[![REUSE status](https://api.reuse.software/badge/github.com/OpenRailAssociation/compliance-assistant)](https://api.reuse.software/info/github.com/OpenRailAssociation/compliance-assistant)
+[![The latest version of Compliance Assistant can be found on PyPI.](https://img.shields.io/pypi/v/compliance-assistant.svg)](https://pypi.org/project/compliance-assistant/)
+[![Information on what versions of Python Compliance Assistant supports can be found on PyPI.](https://img.shields.io/pypi/pyversions/compliance-assistant.svg)](https://pypi.org/project/compliance-assistant/)
+
+**Compliance Assistant** is a comprehensive toolset designed to assist with creating and managing Software Bill of Materials (SBOMs). It helps in enriching SBOMs with licensing and copyright information and checks for Open Source license compliance using data from [ClearlyDefined](https://clearlydefined.io/).
+
+<!-- TOC -->
+- [Compliance Assistant](#compliance-assistant)
+  - [Features](#features)
+  - [Requirements](#requirements)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Development and Contribution](#development-and-contribution)
+  - [License](#license)
+<!-- /TOC -->
+
+## Features
+
+- **SBOM Generation**: Automatically generate a CycloneDX SBOM from a specified code repository.
+- **SBOM Enrichment**: Enhance an existing SBOM with detailed licensing and copyright information using ClearlyDefined data.
+- **SBOM Parsing**: Extract specific information from a CycloneDX SBOM.
+- **License and Copyright Information Retrieval**: Fetch licensing and copyright details for a single package from ClearlyDefined.
+- **License compliance support**: Extract and unify licenses from SBOM, suggest possible license outbound candidates
+
+Some of these features are made possible by excellent programs such as [flict](https://github.com/vinland-technology/flict), [cdxgen](https://github.com/CycloneDX/cdxgen) and [syft](https://github.com/anchore/syft/).
+
+## Requirements
+
+- Python 3.10+
+- Internet connection for accessing ClearlyDefined services
+- At least one SBOM generator:
+  - [syft](https://github.com/anchore/syft/)
+  - [cdxgen](https://github.com/CycloneDX/cdxgen)
+  - [Docker](https://www.docker.com/) for generating SBOMs with the dockerized cdxgen
+
+## Installation
+
+### Install and run via pipx (Recommended)
+
+[pipx](https://pypa.github.io/pipx/) makes installing and running Python programs easier and avoids conflicts with other packages. Install it with
+
+```sh
+pip3 install pipx
+```
+
+The following one-liner both installs and runs this program from [PyPI](https://pypi.org/project/compliance-assistant/):
+
+```sh
+pipx run compliance-assistant
+```
+
+If you want to be able to use compliance-assistant without prepending it with `pipx run` every time, install it globally like so:
+
+```sh
+pipx install compliance-assistant
+```
+
+compliance-assistant will then be available in `~/.local/bin`, which must be added to your `$PATH`.
+
+After this, make sure that `~/.local/bin` is in your `$PATH`. On Windows, the required path for your environment may look like `%USERPROFILE%\AppData\Roaming\Python\Python310\Scripts`, depending on the Python version you have installed.
+
+To upgrade compliance-assistant to the newest available version, run this command:
+
+```sh
+pipx upgrade compliance-assistant
+```
+
+
+### Other installation methods
+
+You may also use pure `pip` or `poetry` to install this package.
+
+
+## Usage
+
+The Compliance Assistant provides multiple commands to facilitate different tasks. Each command is invoked through the `compliance-assistant` command-line interface with specific options.
+
+Depending on your exact installation method, this may be one of
+
+```sh
+# Run via pipx
+pipx run compliance-assistant
+# Installation via pipx or pip
+compliance-assistant
+# Run via poetry
+poetry run compliance-assistant
+```
+
+In the following, we will just use `compliance-assistant`.
+
+### Command Structure
+
+```bash
+compliance-assistant <command> [<subcommand>] [subcommand-options]
+```
+
+### Commands
+
+Please run `compliance-assistant --help` to get an overview of the commands and global options.
+
+For each command, you can get detailed options, e.g., `compliance-assistant sbom enrich --help`.
+
+### Examples
+
+* Create an SBOM for the current directory using [syft](https://github.com/anchore/syft/): `compliance-assistant sbom generate -g syft -d . -o /tmp/my-sbom.json`
+* Enrich an SBOM with ClearlyDefined data: `compliance-assistant sbom enrich -f /tmp/my-sbom.json -o /tmp/my-enriched-sbom.json`
+* Extract certain data from an SBOM: `compliance-assistant sbom parse -f /tmp/my-enriched-sbom.json -e purl,copyright,name`
+* Gather ClearlyDefined licensing/copyright information for one package: `compliance-assistant clearlydefined fetch -p pkg:pypi/inwx-dns-recordmaster@0.3.1`
+* Get all licenses found in the enriched SBOM: `compliance-assistant licensing list -f /tmp/my-enriched-sbom.json -o plain`
+* Get license outbound candidate based on licenses from SBOM: `compliance-assistant licensing outbound -f /tmp/my-enriched-sbom.json`
+
+### Run as GitHub workflow
+
+You may also use GitHub workflows to generate an SBOM regularly, e.g., on each published release:
+
+```yaml
+name: Generate and enrich SBOM
+
+on:
+  release:
+    types: [published]
+
+jobs:
+  # Generate the SBOM with syft and enrich the generated SBOM
+  sbom-generate-and-enrich:
+    runs-on: ubuntu-22.04
+    needs: sbom-gen
+    steps:
+      # Install compliance-assistant
+      - name: Set up Python
+        uses: actions/setup-python@v5
+        with:
+          python-version: "3.12"
+          cache: "pip"
+      - name: Install compliance-assistant
+        run: pip install compliance-assistant
+      # Install syft
+      - run: mkdir -p ~/.local/bin
+      - name: Install syft
+        run: curl -sSfL https://raw.githubusercontent.com/anchore/syft/main/install.sh | sh -s -- -b ~/.local/bin
+      # Generate SBOM with syft via compliance-assistant
+      - name: Generate SBOM with syft
+        run: poetry run compliance-assistant sbom generate -g syft -d . -o ${{ runner.temp }}/sbom-raw.json
+      # Enrich SBOM with compliance-assistant
+      - name: Enrich SBOM
+        run: compliance-assistant sbom enrich -f ${{ runner.temp }}/sbom-raw.json -o ${{ runner.temp }}/sbom-enriched.json
+      # Upload enriched SBOM as artifact
+      - name: Store enriched SBOM as artifact
+        uses: actions/upload-artifact@v4
+        with:
+          name: sbom-enriched
+          path: ${{ runner.temp }}/sbom-enriched.json
+```
+
+
+## Development and Contribution
+
+We welcome contributions to improve Compliance Assistant. Please read [CONTRIBUTING.md](./CONTRIBUTING.md) for all information.
+
+
+## License
+
+The content of this repository is licensed under the [Apache 2.0 license](https://www.apache.org/licenses/LICENSE-2.0).
+
+There may be components under different, but compatible licenses or from different copyright holders. The project is REUSE compliant which makes these portions transparent. You will find all used licenses in the [LICENSES](./LICENSES/) directory.
+
+The project has been started by the [OpenRail Association](https://openrailassociation.org). You are welcome to [contribute](./CONTRIBUTING.md)!
---
+++ packages/compliance-assistant/compliance_assistant-1.1.3.tar.gz/compliance_assistant-1.1.3/complassist/_clearlydefined.py
@@ -0,0 +1,251 @@
+# SPDX-FileCopyrightText: 2024 DB Systel GmbH
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""Functions concerning working with ClearlyDefined"""
+
+import logging
+from os.path import join as pathjoin
+from urllib.parse import urljoin
+
+from purltools import purl2clearlydefined
+from requests.exceptions import JSONDecodeError
+
+from ._helpers import make_request_with_retry
+
+
+def _cdapi_call(
+    path: str,
+    method: str = "GET",
+    api_url: str = "https://api.clearlydefined.io",
+    basepath: str = "definitions",
+    json_dict: dict | list | None = None,
+    **params: str,
+) -> dict | None:
+    """
+    Makes a request to the ClearlyDefined API.
+
+    Constructs and sends a request to the ClearlyDefined API, either with query
+    parameters or a JSON payload, and returns the JSON response if available.
+
+    Args:
+        path (str): The API endpoint path relative to the basepath.
+
+        method (str, optional): The HTTP method to use for the request (e.g.,
+        "GET", "POST"). Defaults to "GET".
+
+        api_url (str, optional): The base URL of the ClearlyDefined API.
+        Defaults to "https://api.clearlydefined.io".
+
+        basepath (str, optional): The base path for the API endpoint. Defaults
+        to "definitions".
+
+        json_dict (dict | None, optional): A dictionary to be sent as a JSON
+        payload in the request body. Defaults to None.
+
+        **params (str): Additional query parameters to include in the request.
+
+    Returns:
+        dict: The JSON response from the API, or a dictionary containing the
+        response text if JSON decoding fails.
+    """
+    url = urljoin(api_url, pathjoin(basepath, path))
+    if json_dict:
+        result = make_request_with_retry(method=method, url=url, json=json_dict, params=params)
+    else:
+        result = make_request_with_retry(method=method, url=url, params=params)
+
+    # Return JSON response if possible
+    try:
+        return result.json()
+    except (JSONDecodeError, AttributeError):
+        logging.debug("JSON return is no valid JSON")
+        if basepath != "harvest":
+            try:
+                error_msg = result.content.decode("UTF-8")
+            except:  # pylint: disable=bare-except
+                error_msg = result.content
+            logging.warning(
+                "Unexpected JSON decoding error as result from %s: %s",
+                url,
+                error_msg,
+            )
+        return None
+
+
+def _extract_license_copyright(cd_api_response: dict) -> tuple[str, str]:
+    """
+    Extracts the declared license and detected copyright attributions from a
+    ClearlyDefined API response.
+
+    Args:
+        cd_api_response (dict): The JSON response from the ClearlyDefined API.
+
+    Returns:
+        tuple[str, str]: A tuple containing:
+            - The declared license as a string, or an empty string if not found.
+            - The detected copyright attributions as a single string, with each
+              attribution separated by a newline, or an empty string if not
+              found.
+    """
+    package_name = cd_api_response.get("coordinates", {}).get("name", "")
+    license_declared = ""
+    copyrights: list[str] = []
+    if licensed := cd_api_response.get("licensed"):
+        # Get license
+        license_declared = licensed.get("declared", "")
+
+        # Get copyright attributions
+        if facets := licensed.get("facets"):
+            try:
+                copyrights = facets.get("core", {}).get("attribution", {}).get("parties", [])
+            except (TypeError, AttributeError):
+                pass
+
+    if not license_declared:
+        logging.debug("No results for declared license from ClearlyDefined for %s", package_name)
+    if not copyrights:
+        logging.debug(
+            "No results for copyright attributions from ClearlyDefined for %s", package_name
+        )
+
+    return license_declared, "\n".join(copyrights).strip()
+
+
+def _handle_missing_license_and_request_harvest(coordinates: str) -> None:
+    """
+    Handles the case when a declared license is not found and triggers a harvest
+    request.
+
+    Logs the event of a missing license and sends a harvest request to
+    ClearlyDefined for the given coordinates.
+
+    Args:
+        coordinates (str): The ClearlyDefined coordinates or Package URL for
+        which the license is missing.
+    """
+    logging.info(
+        "Adding %s to be harvested by ClearlyDefined. "
+        "Make sure the package and this version actually exists, and try again later.",
+        coordinates,
+    )
+    _cdapi_call(
+        path="",
+        method="POST",
+        basepath="harvest",
+        json_dict={"tool": "package", "coordinates": coordinates},
+    )
+
+
+def get_clearlydefined_license_and_copyright(coordinates: str) -> tuple[str, str]:
+    """
+    Retrieves the declared license for the specified coordinates from ClearlyDefined.
+
+    Queries the ClearlyDefined API to get the declared license for the provided
+    coordinates or Package URL (purl). If no license is found, it initiates a
+    harvest request.
+
+    Args:
+        coordinates (str): The ClearlyDefined coordinates or Package URL for
+        which to retrieve the license.
+
+    Returns:
+        tuple[str, str]: A tuple containing:
+            - The declared license as a string, or an empty string if not found.
+            - The detected copyright attributions as a single string, with each
+              attribution separated by a newline, or an empty string if not
+              found.
+    """
+    api_return = _cdapi_call(coordinates, expand="-files")
+
+    if api_return:
+        declared_license, copyrights = _extract_license_copyright(api_return)
+
+        # Declared license couldn't be extracted. Add to harvest
+        if not declared_license:
+            _handle_missing_license_and_request_harvest(coordinates)
+
+        return declared_license, copyrights
+
+    # If no valid API result, return empty license and copyright
+    return "", ""
+
+
+def get_clearlydefined_license_and_copyright_in_batches(
+    purls: list[str],
+) -> dict[str, tuple[str, str]]:
+    """
+    Retrieves the declared license and detected copyright for multiple Package
+    URLs from ClearlyDefined.
+
+    Queries the ClearlyDefined API to retrieve both the declared license and the
+    detected copyright attributions for multiple packages specified via Package
+    URLs. If no declared license is found for a package, a harvest request is
+    initiated.
+
+    Args:
+        purls (list[str]): A list of Package URLs (purls) for which to retrieve
+        the license and copyright information.
+
+    Returns:
+        tuple[str, str]: A tuple containing:
+            - The declared license as a string, or an empty string if not found.
+            - The detected copyright attributions as a single string, with each
+              attribution separated by a newline, or an empty string if none are
+              found.
+
+            Returns a dict of the provided purls and empty tuples if the
+            ClearlyDefined API did not return valid data.
+    """
+    # Create connections between coordinates <-> purl
+    coordinates_purls = {purl2clearlydefined(purl): purl for purl in purls}
+    # Request the CD API for the coordinates
+    api_return = _cdapi_call(
+        path="", method="POST", json_dict=list(coordinates_purls.keys()), expand="-files"
+    )
+
+    if api_return:
+        result: dict[str, tuple[str, str]] = {}
+        for pkg_coordinates, cd_data in api_return.items():
+            # Fetch the corresponding PURL for the coordinates
+            pkg_purl = coordinates_purls[pkg_coordinates]
+
+            # Extract license and copyright data from the CD API return
+            declared_license, copyrights = _extract_license_copyright(cd_data)
+
+            # Declared license couldn't be extracted. Add to harvest
+            if not declared_license:
+                _handle_missing_license_and_request_harvest(pkg_coordinates)
+
+            result[pkg_purl] = (declared_license, copyrights)
+
+        return result
+
+    logging.warning(
+        "No valid data from ClearlyDefined received for the following packages: %s",
+        ", ".join(purls),
+    )
+    return {purl: ("", "") for purl in purls}
+
+
+def print_clearlydefined_result(results: tuple[str, str]) -> None:
+    """
+    Pretty-print the results for declared license and copyright attributions
+    retrieved from the ClearlyDefined API for a package.
+
+    Args:
+        results (tuple[str, str]): A tuple containing:
+            - The declared license as a string, or an empty string if not found.
+            - The detected copyright attributions as a single string, with each
+              attribution separated by a newline, or an empty string if not
+              found.
+
+    Returns:
+        str: A pretty-printed, human-readable output of both data points
+    """
+    output = f"Declared license: {results[0]}\n\n"
+    output += "Detected copyright attributions:"
+    if attribs := results[1]:
+        output += f"\n{attribs}"
+
+    print(output)
