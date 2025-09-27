31b0817f33782e409d68ee240f6dcfdd966e3dbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 14:06:37 +0000

    Add package dyff-schema/0.35.2/dyff_schema-0.35.2.tar.gz

---
+++ packages/dyff-schema/dyff_schema-0.35.2.tar.gz/dyff_schema-0.35.2/dyff/schema/v0/r1/dataset/vision.py
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2024 UL Research Institutes
+# SPDX-License-Identifier: Apache-2.0
+
+import pydantic
+
+from ..base import DyffSchemaBaseModel
+
+
+class Image(DyffSchemaBaseModel):
+    """A binary-formatted image."""
+
+    bytes_: bytes = pydantic.Field(alias="bytes", description="The image data")
+    format: str = pydantic.Field(description="The data MIME type")
