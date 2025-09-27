ba427768c9b433c4a27b93024fe930a2f9437e0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 03:13:59 +0000

    Add package django-mantle/25.0/django_mantle-25.0.tar.gz

---
+++ packages/django-mantle/django_mantle-25.0.tar.gz/django_mantle-25.0/src/mantle/__init__.py
@@ -0,0 +1,7 @@
+"""
+django-mantle: Your type-safe wrapper around Django's liquid core.
+
+ORM-to-Wire. Efficient querying and serialisation. Type-safe. Immutable. Fast.
+"""
+
+__version__ = "25.0"
