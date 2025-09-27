e558ee4609f86d4977e2e015e65c3d0380c5f1a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 05:06:05 +0000

    Add package cmomy/1.0.0/cmomy-1.0.0-py3-none-any.whl

---
+++ packages/cmomy/cmomy-1.0.0-py3-none-any.whl/cmomy/core/typing_compat.py
@@ -0,0 +1,39 @@
+"""Typing compatibility."""
+# pyright: reportUnreachable=false
+
+import sys
+from typing import Any
+
+from typing_extensions import TypedDict
+
+if sys.version_info >= (3, 10):
+    from types import EllipsisType
+    from typing import TypeAlias, TypeGuard
+else:
+    EllipsisType = Any
+    from typing_extensions import TypeAlias, TypeGuard
+
+
+if sys.version_info >= (3, 11):
+    from typing import Required, Self, Unpack
+else:
+    from typing_extensions import Required, Self, Unpack
+
+
+if sys.version_info >= (3, 13):  # pragma: no cover
+    from typing import TypeIs, TypeVar
+else:
+    from typing_extensions import TypeIs, TypeVar
+
+
+__all__ = [
+    "EllipsisType",
+    "Required",
+    "Self",
+    "TypeAlias",
+    "TypeGuard",
+    "TypeIs",
+    "TypeVar",
+    "TypedDict",
+    "Unpack",
+]
