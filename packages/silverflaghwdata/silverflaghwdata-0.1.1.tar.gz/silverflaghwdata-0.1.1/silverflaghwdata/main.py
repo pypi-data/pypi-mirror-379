1ec24bbd027b7d4db62198c6deecf9af80b5fd58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 14:06:37 +0000

    Add package silverflaghwdata/0.1.1/silverflaghwdata-0.1.1.tar.gz

---
+++ packages/silverflaghwdata/silverflaghwdata-0.1.1.tar.gz/silverflaghwdata-0.1.1/silverflaghwdata/main.py
@@ -0,0 +1,16 @@
+import sys
+from states import stateScrapers, run_all
+
+def main()
+    if len(sys.argv) > 1:
+        state = sys.argv[1].lower()
+        if state in stateScrapers:
+            stateScrapers[state]()
+        else:
+            print("Unknown state:", state)
+    else:
+        run_all()
+
+
+if __name__ == "__main__":
+    main()
