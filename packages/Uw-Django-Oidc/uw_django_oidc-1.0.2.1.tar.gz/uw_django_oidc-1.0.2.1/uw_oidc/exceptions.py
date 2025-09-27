92df8e1b3e5e4b783857039e822705aa9b05a9e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 05:07:33 +0000

    Add package Uw-Django-Oidc/1.0.2.1/uw_django_oidc-1.0.2.1.tar.gz

---
+++ packages/Uw-Django-Oidc/uw_django_oidc-1.0.2.1.tar.gz/uw_django_oidc-1.0.2.1/uw_oidc/exceptions.py
@@ -0,0 +1,34 @@
+# Copyright 2025 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Custom exceptions for uw_oidc.
+All of them are returned with the HTTP response code 401.
+
+If it is a Jwks related exception, check Jwks the site.
+Otherwise the error is caused by the ID token.
+"""
+
+
+class InvalidTokenError(Exception):
+    pass
+
+    def __str__(self):
+        return "{}: {}".format(self.__class__.__name__,
+                               super().__str__())
+
+
+class JwksFetchError(InvalidTokenError):
+    pass
+
+
+class JwksDataError(InvalidTokenError):
+    pass
+
+
+class InvalidTokenHeader(InvalidTokenError):
+    pass
+
+
+class NoMatchingPublicKey(InvalidTokenError):
+    pass
