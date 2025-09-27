90479f82c99b137b2021f7918f0d6f3253e16ebd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 03:13:28 +0000

    Add package cwl-loader/0.11.0/cwl_loader-0.11.0.tar.gz

---
+++ packages/cwl-loader/cwl_loader-0.11.0.tar.gz/cwl_loader-0.11.0/tests/__init__.py
@@ -0,0 +1,9 @@
+"""
+CWL Loader (c) 2025
+
+CWL Loader is licensed under
+Creative Commons Attribution-ShareAlike 4.0 International.
+
+You should have received a copy of the license along with this work.
+If not, see <https://creativecommons.org/licenses/by-sa/4.0/>.
+"""
