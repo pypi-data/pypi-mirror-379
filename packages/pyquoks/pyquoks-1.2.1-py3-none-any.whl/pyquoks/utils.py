eadefa3cc020fe47f008e6e65d2b6383411a8433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 14:05:54 +0000

    Add package pyquoks/1.2.1/pyquoks-1.2.1-py3-none-any.whl

---
+++ packages/pyquoks/pyquoks-1.2.1-py3-none-any.whl/pyquoks/utils.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+import sys, os
+
+
+def get_path(relative_path: str, only_abspath: bool = False) -> str:
+    try:
+        base_path = sys._MEIPASS
+    except:
+        base_path = os.path.abspath(".")
+    finally:
+        if only_abspath:
+            base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
