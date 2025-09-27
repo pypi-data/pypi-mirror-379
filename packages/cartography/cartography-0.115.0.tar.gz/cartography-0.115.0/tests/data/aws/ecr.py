7d91072260a3b631ec0c5d32ee6bd578eef14d8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 06:06:34 +0000

    Add package cartography/0.115.0/cartography-0.115.0.tar.gz

---
+++ packages/cartography/cartography-0.115.0.tar.gz/cartography-0.115.0/tests/data/aws/ecr.py
@@ -0,0 +1,117 @@
+import datetime
+
+DESCRIBE_REPOSITORIES = {
+    "repositories": [
+        {
+            "repositoryArn": "arn:aws:ecr:us-east-1:000000000000:repository/example-repository",
+            "registryId": "000000000000",
+            "repositoryName": "example-repository",
+            "repositoryUri": "000000000000.dkr.ecr.us-east-1.amazonaws.com/example-repository",
+            "createdAt": datetime.datetime(2019, 1, 1, 0, 0, 1),
+        },
+        {
+            "repositoryArn": "arn:aws:ecr:us-east-1:000000000000:repository/sample-repository",
+            "registryId": "000000000000",
+            "repositoryName": "sample-repository",
+            "repositoryUri": "000000000000.dkr.ecr.us-east-1.amazonaws.com/sample-repository",
+            "createdAt": datetime.datetime(2019, 1, 1, 0, 0, 1),
+        },
+        {
+            "repositoryArn": "arn:aws:ecr:us-east-1:000000000000:repository/test-repository",
+            "registryId": "000000000000",
+            "repositoryName": "test-repository",
+            "repositoryUri": "000000000000.dkr.ecr.us-east-1.amazonaws.com/test-repository",
+            "createdAt": datetime.datetime(2019, 1, 1, 0, 0, 1),
+        },
+    ],
+}
+DESCRIBE_IMAGES = {
+    "imageDetails": {
+        "registryId": "000000000000",
+        "imageSizeInBytes": 1024,
+        "imagePushedAt": "2025-01-01T00:00:00.000000-00:00",
+        "imageScanStatus": {
+            "status": "COMPLETE",
+            "description": "The scan was completed successfully.",
+        },
+        "imageScanFindingsSummary": {
+            "imageScanCompletedAt": "2025-01-01T00:00:00-00:00",
+            "vulnerabilitySourceUpdatedAt": "2025-01-01T00:00:00-00:00",
+            "findingSeverityCounts": {
+                "CRITICAL": 1,
+                "HIGH": 1,
+                "MEDIUM": 1,
+                "INFORMATIONAL": 1,
+                "LOW": 1,
+            },
+        },
+        "imageManifestMediaType": "application/vnd.docker.distribution.manifest.v2+json",
+        "artifactMediaType": "application/vnd.docker.container.image.v1+json",
+        "lastRecordedPullTime": "2025-01-01T01:01:01.000000-00:00",
+    },
+}
+
+LIST_REPOSITORY_IMAGES = {
+    "000000000000.dkr.ecr.us-east-1.amazonaws.com/example-repository": [
+        {
+            "imageDigest": "sha256:0000000000000000000000000000000000000000000000000000000000000000",
+            "imageTag": "1",
+            "repositoryName": "example-repository",
+            **DESCRIBE_IMAGES["imageDetails"],
+        },
+        {
+            "imageDigest": "sha256:0000000000000000000000000000000000000000000000000000000000000000",
+            "imageTag": "latest",
+            "repositoryName": "example-repository",
+            **DESCRIBE_IMAGES["imageDetails"],
+        },
+        {
+            "imageDigest": "sha256:0000000000000000000000000000000000000000000000000000000000000001",
+            "imageTag": "2",
+            "repositoryName": "example-repository",
+            **DESCRIBE_IMAGES["imageDetails"],
+        },
+    ],
+    "000000000000.dkr.ecr.us-east-1.amazonaws.com/sample-repository": [
+        {
+            # NOTE same digest and tag as image in example-repository
+            "imageDigest": "sha256:0000000000000000000000000000000000000000000000000000000000000000",
+            "imageTag": "1",
+            "repositoryName": "sample-repository",
+            **DESCRIBE_IMAGES["imageDetails"],
+        },
+        {
+            "imageDigest": "sha256:0000000000000000000000000000000000000000000000000000000000000011",
+            "imageTag": "2",
+            "repositoryName": "sample-repository",
+            **DESCRIBE_IMAGES["imageDetails"],
+        },
+    ],
+    "000000000000.dkr.ecr.us-east-1.amazonaws.com/test-repository": [
+        {
+            # NOTE same digest but different tag from image in example-repository
+            "imageDigest": "sha256:0000000000000000000000000000000000000000000000000000000000000000",
+            "imageTag": "1234567890",
+            "repositoryName": "test-repository",
+            **DESCRIBE_IMAGES["imageDetails"],
+        },
+        {
+            "imageDigest": "sha256:0000000000000000000000000000000000000000000000000000000000000021",
+            "imageTag": "1",
+            "repositoryName": "test-repository",
+            **DESCRIBE_IMAGES["imageDetails"],
+        },
+        # Item without an imageDigest: will get filtered out and not ingested.
+        {
+            "imageTag": "1",
+            "repositoryName": "test-repository",
+            **DESCRIBE_IMAGES["imageDetails"],
+        },
+        # Item without an imageTag
+        {
+            "imageDigest": "sha256:0000000000000000000000000000000000000000000000000000000000000031",
+            "repositoryName": "test-repository",
+            **DESCRIBE_IMAGES["imageDetails"],
+        },
+    ],
+}
