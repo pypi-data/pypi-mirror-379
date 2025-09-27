68788e2e02832ddbb1d73ff7877d9eee5ff0429e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 03:13:37 +0000

    Add package dune-client/1.9.0/dune_client-1.9.0.tar.gz

---
+++ packages/dune-client/dune_client-1.9.0.tar.gz/dune_client-1.9.0/tests/e2e/test_custom_endpoints.py
@@ -0,0 +1,23 @@
+import os
+import unittest
+
+import dotenv
+
+from dune_client.client import DuneClient
+
+dotenv.load_dotenv()
+
+
+@unittest.skip("endpoint no longer exists - {'error': 'Custom endpoint not found'}")
+class TestCustomEndpoints(unittest.TestCase):
+    def setUp(self) -> None:
+        self.valid_api_key = os.environ["DUNE_API_KEY"]
+
+    def test_getting_custom_endpoint_results(self):
+        dune = DuneClient(self.valid_api_key)
+        results = dune.get_custom_endpoint_result("dune", "new-test")
+        assert len(results.get_rows()) == 10
+
+
+if __name__ == "__main__":
+    unittest.main()
