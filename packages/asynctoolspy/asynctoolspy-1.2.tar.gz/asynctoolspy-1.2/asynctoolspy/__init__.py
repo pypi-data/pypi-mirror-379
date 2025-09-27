93b36052ccd01fd78d1f3f3acba35a5c4212bbd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 05:07:02 +0000

    Add package asynctoolspy/1.2/asynctoolspy-1.2.tar.gz

---
+++ packages/asynctoolspy/asynctoolspy-1.2.tar.gz/asynctoolspy-1.2/asynctoolspy/__init__.py
@@ -0,0 +1,6 @@
+from .appoint_tool import appoint_limit_async
+from .asynciter_tool import AsyncIterWrapper
+
+__version__ = '1.0'
+__author__ = 'Aleksei Surnov'
+__all__ = ['appoint_limit_async', 'AsyncIterWrapper']
