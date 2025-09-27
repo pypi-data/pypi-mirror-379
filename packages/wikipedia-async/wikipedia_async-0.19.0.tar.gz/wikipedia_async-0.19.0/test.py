9f4480ccec6ff817ea312686b45994b7695cdb0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 04:06:19 +0000

    Add package wikipedia-async/0.19.0/wikipedia_async-0.19.0.tar.gz

---
+++ packages/wikipedia-async/wikipedia_async-0.19.0.tar.gz/wikipedia_async-0.19.0/test.py
@@ -0,0 +1,50 @@
+from pprint import pprint
+from wikipedia_async import WikipediaClient, ClientConfig
+import asyncio
+import time
+
+section = None
+
+
+async def main():
+    global section
+    # Initialize client with optimal defaults
+    client = WikipediaClient(
+        ClientConfig(
+            cache_type="file",
+            cache_ttl=3600 * 24,
+            cache_serializer="json",
+            # enable_cache=False,
+        )
+    )
+    # s = time.time()
+    # await client.set_language("bn")
+    titles = [
+        "Python (programming language)",
+        "Java (programming language)",
+        "C (programming language)",
+    ]
+
+    s = time.time()
+    title = titles[0]
+    page = await client.get_page(title, lang="en")
+    print("Time taken:", time.time() - s)
+    await client.close()
+
+    # pprint(page.helper.tree_view_json(50))
+
+    # pprint(
+    #     page.helper.to_json(
+    #         keep_links=False,
+    #         table_limit=1,
+    #         content_limit=500,
+    #         show_children=False,
+    #     )
+    # )
+
+    
+
+
+# if __name__ == "__main__":
+
+asyncio.run(main())
