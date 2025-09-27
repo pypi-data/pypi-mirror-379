5063e678815c8053215be43340d836c528c4cd58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 06:06:56 +0000

    Add package python-ics/921.16/python_ics-921.16-cp314-cp314-manylinux_2_24_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/python-ics/python_ics-921.16-cp314-cp314-manylinux_2_24_aarch64.manylinux_2_28_aarch64.whl/ics/structures/st_cm_iso157652_rx_message.py
@@ -0,0 +1,53 @@
+# This file was auto generated; Do not modify, if you value your sanity!
+import ctypes
+import enum
+
+
+
+class Nameless40028(ctypes.Structure):
+    _pack_ = 2
+    _fields_ = [
+        ('id_29_bit_enable', ctypes.c_uint, 1),
+        ('fc_id_29_bit_enable', ctypes.c_uint, 1),
+        ('ext_address_enable', ctypes.c_uint, 1),
+        ('fc_ext_address_enable', ctypes.c_uint, 1),
+        ('enableFlowControlTransmission', ctypes.c_uint, 1),
+        ('paddingEnable', ctypes.c_uint, 1),
+        ('iscanFD', ctypes.c_uint, 1),
+        ('isBRSEnabled', ctypes.c_uint, 1),
+    ]
+
+
+
+class Nameless59525(ctypes.Union):
+    _pack_ = 2
+    _anonymous_  = ('Nameless40028',)
+    _fields_ = [
+        ('Nameless40028', Nameless40028),
+        ('flags', ctypes.c_uint32),
+    ]
+
+
+
+class st_cm_iso157652_rx_message(ctypes.Structure):
+    _pack_ = 2
+    _anonymous_  = ('Nameless59525',)
+    _fields_ = [
+        ('vs_netid', ctypes.c_uint16),
+        ('padding', ctypes.c_uint8),
+        ('id', ctypes.c_uint32),
+        ('id_mask', ctypes.c_uint32),
+        ('fc_id', ctypes.c_uint32),
+        ('flowControlExtendedAddress', ctypes.c_uint8),
+        ('extendedAddress', ctypes.c_uint8),
+        ('blockSize', ctypes.c_uint8),
+        ('stMin', ctypes.c_uint8),
+        ('cf_timeout', ctypes.c_uint16),
+        ('Nameless59525', Nameless59525),
+        ('reserved', ctypes.c_uint8 * 16),
+    ]
+
+
+_stCM_ISO157652_RxMessage = st_cm_iso157652_rx_message
+stCM_ISO157652_RxMessage = st_cm_iso157652_rx_message
+
