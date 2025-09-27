fa1a22ce521c5a9ff9d36d81af7d1919db96c7c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 05:07:22 +0000

    Add package mayan-edms/4.7.6/mayan_edms-4.7.6-py2.py3-none-any.whl

---
+++ packages/mayan-edms/mayan_edms-4.7.6-py2.py3-none-any.whl/mayan/apps/storage/dependencies.py
@@ -0,0 +1,17 @@
+from mayan.apps.dependencies.classes import PythonDependency
+
+PythonDependency(
+    module=__name__, name='boto3', version_string='==1.33.7'
+)
+PythonDependency(
+    module=__name__, name='django-storages', version_string='==1.14.6'
+)
+PythonDependency(
+    module=__name__, name='extract-msg', version_string='==0.48.7'
+)
+PythonDependency(
+    module=__name__, name='google-cloud-storage', version_string='==2.16.0'
+)
+PythonDependency(
+    module=__name__, name='pycryptodome', version_string='==3.21.0'
+)
