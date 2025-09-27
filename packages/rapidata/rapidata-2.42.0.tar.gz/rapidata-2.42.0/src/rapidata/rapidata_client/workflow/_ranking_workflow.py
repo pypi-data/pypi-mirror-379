16c170f98d6d94ee29715ad362d9fcbea08dc25e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 03:13:52 +0000

    Add package rapidata/2.42.0/rapidata-2.42.0.tar.gz

---
+++ packages/rapidata/rapidata-2.42.0.tar.gz/rapidata-2.42.0/src/rapidata/rapidata_client/workflow/_ranking_workflow.py
@@ -0,0 +1,80 @@
+from rapidata.api_client import (
+    CompareWorkflowModelPairMakerConfig,
+    OnlinePairMakerConfigModel,
+    EloConfigModel,
+)
+from rapidata.api_client.models.compare_workflow_model import CompareWorkflowModel
+from rapidata.rapidata_client.workflow._base_workflow import Workflow
+from rapidata.api_client import ComparePayload
+from rapidata.rapidata_client.datapoints._datapoint import Datapoint
+from rapidata.api_client.models.rapid_modality import RapidModality
+from rapidata.rapidata_client.datapoints.metadata import Metadata
+from rapidata.api_client.models.create_datapoint_from_files_model_metadata_inner import (
+    CreateDatapointFromFilesModelMetadataInner,
+)
+
+
+class RankingWorkflow(Workflow):
+    modality = RapidModality.COMPARE
+
+    def __init__(
+        self,
+        criteria: str,
+        total_comparison_budget: int,
+        random_comparisons_ratio,
+        elo_start: int = 1200,
+        elo_k_factor: int = 40,
+        elo_scaling_factor: int = 400,
+        metadatas: list[Metadata] = [],
+    ):
+        super().__init__(type="CompareWorkflowConfig")
+
+        self.metadatas = metadatas
+
+        self.criteria = criteria
+        self.total_comparison_budget = total_comparison_budget
+        self.random_comparisons_ratio = random_comparisons_ratio
+        self.elo_start = elo_start
+        self.elo_k_factor = elo_k_factor
+        self.elo_scaling_factor = elo_scaling_factor
+
+        self.pair_maker_config = CompareWorkflowModelPairMakerConfig(
+            OnlinePairMakerConfigModel(
+                _t="OnlinePairMaker",
+                totalComparisonBudget=total_comparison_budget,
+                randomMatchesRatio=random_comparisons_ratio,
+            )
+        )
+
+        self.elo_config = EloConfigModel(
+            startingElo=elo_start,
+            kFactor=elo_k_factor,
+            scalingFactor=elo_scaling_factor,
+        )
+
+    def _to_model(self) -> CompareWorkflowModel:
+
+        return CompareWorkflowModel(
+            _t="CompareWorkflow",
+            criteria=self.criteria,
+            eloConfig=self.elo_config,
+            pairMakerConfig=self.pair_maker_config,
+            metadata=[
+                CreateDatapointFromFilesModelMetadataInner(metadata.to_model())
+                for metadata in self.metadatas
+            ],
+        )
+
+    def _to_payload(self, datapoint: Datapoint) -> ComparePayload:
+        return ComparePayload(
+            _t="ComparePayload",
+            criteria=self.criteria,
+        )
+
+    def __str__(self) -> str:
+        return (
+            f"RankingWorkflow(criteria='{self.criteria}', metadatas={self.metadatas})"
+        )
+
+    def __repr__(self) -> str:
+        return f"RankingWorkflow(criteria={self.criteria!r}, total_comparison_budget={self.total_comparison_budget!r}, random_comparisons_ratio={self.random_comparisons_ratio!r}, elo_start={self.elo_start!r}, elo_k_factor={self.elo_k_factor!r}, elo_scaling_factor={self.elo_scaling_factor!r}, metadatas={self.metadatas!r})"
