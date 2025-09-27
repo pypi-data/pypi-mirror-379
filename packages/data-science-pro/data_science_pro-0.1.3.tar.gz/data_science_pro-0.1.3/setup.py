01d494d3df98404ea07f1f662a12e7c8ef4bc3bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 03:14:00 +0000

    Add package data-science-pro/0.1.3/data_science_pro-0.1.3.tar.gz

---
+++ packages/data-science-pro/data_science_pro-0.1.3.tar.gz/data_science_pro-0.1.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='data_science_pro',
+    version='0.1.3',
+    description='LLM-powered modular data science pipeline',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Your Name',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'scikit-learn',
+        'langchain',
+        'openai',
+        'python-dotenv',
+        'joblib',
+        'imbalanced-learn'
+    ],
+    entry_points={
+        'console_scripts': [
+            'data-science-pro=pipeline:main'
+        ]
+    },
+    python_requires='>=3.8',
+)
