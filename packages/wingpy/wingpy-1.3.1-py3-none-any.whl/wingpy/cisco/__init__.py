63f4300effb1d68f9ed10388ac31d9061e1f93bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 03:13:58 +0000

    Add package wingpy/1.3.1/wingpy-1.3.1-py3-none-any.whl

---
+++ packages/wingpy/wingpy-1.3.1-py3-none-any.whl/wingpy/cisco/__init__.py
@@ -0,0 +1,25 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# Copyright (C) 2025 Wingmen Solutions ApS
+# This file is part of wingpy, distributed under the terms of the GNU GPLv3.
+# See the LICENSE, NOTICE, and AUTHORS files for more information.
+
+from wingpy.cisco.apic import CiscoAPIC
+from wingpy.cisco.catalystcenter import CiscoCatalystCenter
+from wingpy.cisco.fmc import CiscoFMC
+from wingpy.cisco.hyperfabric import CiscoHyperfabric
+from wingpy.cisco.ise import CiscoISE
+from wingpy.cisco.merakidashboard import CiscoMerakiDashboard
+from wingpy.cisco.nexusdashboard import CiscoNexusDashboard
+from wingpy.cisco.vmanage import CiscoVmanage
+
+__all__ = [
+    "CiscoAPIC",
+    "CiscoCatalystCenter",
+    "CiscoFMC",
+    "CiscoISE",
+    "CiscoHyperfabric",
+    "CiscoMerakiDashboard",
+    "CiscoNexusDashboard",
+    "CiscoVmanage",
+]
