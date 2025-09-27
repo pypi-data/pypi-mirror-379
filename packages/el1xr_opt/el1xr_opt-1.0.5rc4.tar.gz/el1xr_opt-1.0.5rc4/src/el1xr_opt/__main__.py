1c64adcdf5698c04ff45e6e2061ca115c6bc7589
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 03:13:49 +0000

    Add package el1xr_opt/1.0.5rc4/el1xr_opt-1.0.5rc4.tar.gz

---
+++ packages/el1xr_opt/el1xr_opt-1.0.5rc4.tar.gz/el1xr_opt-1.0.5rc4/src/el1xr_opt/__main__.py
@@ -0,0 +1,7 @@
+from .el1xr_Main import main
+from .Modules.oM_SolverSetup import ensure_ampl_solvers
+
+if __name__ == "__main__":
+    # Making sure that the solvers are correctly set up
+    ensure_ampl_solvers()
+    raise SystemExit(main())
