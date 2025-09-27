1639a3824b7931cbb10d13519f3d9035e9a77b38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 07:05:43 +0000

    Add package mp-api/0.45.11/mp_api-0.45.11.tar.gz

---
+++ packages/mp-api/mp_api-0.45.11.tar.gz/mp_api-0.45.11/tests/materials/test_electrodes.py
@@ -0,0 +1,55 @@
+import os
+from core_function import client_search_testing
+
+import pytest
+from pymatgen.core.periodic_table import Element
+
+from mp_api.client.routes.materials.electrodes import ElectrodeRester
+
+
+@pytest.fixture
+def rester():
+    rester = ElectrodeRester()
+    yield rester
+    rester.session.close()
+
+
+excluded_params = [
+    "sort_fields",
+    "chunk_size",
+    "num_chunks",
+    "all_fields",
+    "fields",
+]
+
+sub_doc_fields = []  # type: list
+
+alt_name_dict = {
+    "battery_ids": "battery_id",
+    "formula": "battery_id",
+    "exclude_elements": "battery_id",
+    "num_elements": "nelements",
+    "num_sites": "nsites",
+}  # type: dict
+
+custom_field_tests = {
+    "battery_ids": ["mp-16722_Al"],
+    "working_ion": Element("Li"),
+    "formula": "CoO2",
+    "chemsys": "Co-O",
+    "elements": ["Co", "O"],
+    "exclude_elements": ["Co"],
+}  # type: dict
+
+
+@pytest.mark.skipif(os.getenv("MP_API_KEY", None) is None, reason="No API key found.")
+def test_client(rester):
+    search_method = rester.search
+
+    client_search_testing(
+        search_method=search_method,
+        excluded_params=excluded_params,
+        alt_name_dict=alt_name_dict,
+        custom_field_tests=custom_field_tests,
+        sub_doc_fields=sub_doc_fields,
+    )
