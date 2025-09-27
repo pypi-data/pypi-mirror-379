900f610cd210b07ef9d6da023820c9f03cb6555f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 03:13:55 +0000

    Add package ecodev-front/0.0.97/ecodev_front-0.0.97-py3-none-any.whl

---
+++ packages/ecodev-front/ecodev_front-0.0.97-py3-none-any.whl/ecodev_front/app_layout.py
@@ -0,0 +1,103 @@
+"""
+Base app component, utilising dmc.AppShell.
+"""
+from typing import Any
+
+import dash
+import dash_mantine_components as dmc
+from dash import dcc
+from dash import html
+
+from ecodev_front.constants import NOTIFICATION_ID
+from ecodev_front.ids import APPSHELL
+from ecodev_front.ids import ASIDE
+from ecodev_front.ids import FOOTER_ID
+from ecodev_front.ids import HEADER_ID
+from ecodev_front.ids import MAIN_CONTENT_ID
+from ecodev_front.ids import NAVBAR
+from ecodev_front.ids import NOTIFICATION
+from ecodev_front.ids import TOKEN
+from ecodev_front.ids import URL
+
+
+def dash_base_layout(stores: list[dcc.Store] = [],
+                     main_color: str = '#0066A1',
+                     theme: dict[str, dict[str, Any] | list[str]] | None = None,
+                     header_height: int = 55,
+                     footer_height: int = 40,
+                     main_content_style: dict[str, str] | None = None
+                     ) -> dmc.MantineProvider:
+    """
+    Returns a base layout for any Dash application
+    """
+    return dmc.MantineProvider(
+        forceColorScheme='light',
+        theme=theme,
+        children=dmc.AppShell([
+            dcc.Location(id=URL, refresh='callback-nav'),
+            dcc.Store(id=TOKEN, data={TOKEN: None}, storage_type='local'),
+            *stores,
+
+            html.Div(id=NOTIFICATION_ID),
+            dmc.NotificationContainer(id=NOTIFICATION),
+
+            dmc.AppShellHeader(id=HEADER_ID,
+                               style={'background-color': main_color},
+                               zIndex=100,
+                               children=[],
+                               ),
+
+            dmc.AppShellMain(
+                id=MAIN_CONTENT_ID,
+                children=html.Div(style={'width': '100%'},
+                                  children=dash.page_container),
+                style=main_content_style or {'width': '100%',
+                                             'justifyContent': 'center',
+                                             'display': 'flex',
+                                             'overflow': 'hidden',
+                                             },
+            ),
+
+            dmc.AppShellFooter(id=FOOTER_ID, zIndex=100, children=[],
+                               style={'paddingBottom': '50px',
+                                      'backgroundColor': main_color,
+                                      'color': 'white',
+                                      'textAlign': 'center',
+                                      'alignContent': 'center',
+                                      'justifyContent': 'center'}
+                               ),
+
+            dmc.AppShellNavbar(
+                id=NAVBAR,
+                children=[],
+                zIndex=90,
+                withBorder=True,
+                visibleFrom='md',
+                className='my-navbar'
+            ),
+
+            dmc.AppShellAside(
+                id=ASIDE,
+                children=[],
+                zIndex=90,
+                withBorder=True,
+                visibleFrom='md'
+            )],
+
+            id=APPSHELL,
+            style={'padding': '0',
+                   'background-color': '#f2f2f2',
+                   'overflow': 'hidden',
+                   'height': '96vh'},
+            header={'height': header_height},
+            footer={'height': footer_height},
+            navbar={
+                'breakpoint': 'xl',
+                'collapsed': {'desktop': False, 'mobile': True},
+        },
+            aside={
+                'breakpoint': 'xl',
+                'collapsed': {'desktop': False, 'mobile': True},
+        },
+        )
+    )
---
+++ packages/ecodev-front/ecodev_front-0.0.97-py3-none-any.whl/ecodev_front/tables/data_table.py
@@ -0,0 +1,178 @@
+"""
+Module implementing a generic Dash AG Grid table
+"""
+from typing import Any
+from typing import Dict
+from typing import List
+from typing import Optional
+from typing import Union
+
+import dash_ag_grid as dag
+from ecodev_core import logger_get
+from pydantic_settings import BaseSettings
+from pydantic_settings import SettingsConfigDict
+
+log = logger_get(__name__)
+locale_fr_FR = """d3.formatLocale({
+  "decimal": ",",
+  "thousands": "\u00a0",
+  "grouping": [3],
+  "currency": ["", "\u00a0€"],
+  "percent": "\u202f%",
+  "nan": ""
+})"""
+
+
+class DashAgGridEnterprise(BaseSettings):
+    """
+    Simple authentication configuration class
+    """
+    dag_license_key: str = ''
+    enable_dag_enterprise: bool = True
+    model_config = SettingsConfigDict(env_file='.env')
+
+
+DAG_ENTERPRISE_AUTH = DashAgGridEnterprise()
+
+
+def data_table(id: str | dict,
+               row_data: List[Dict] | Any,
+               column_defs: list[dict[Any, Any]] | None = None,
+               default_col_def: dict | None = None,
+               style: dict | None = None,
+               row_style: dict | None = None,
+               dash_grid_options: dict | None = None,
+               pagination: bool = False,
+               pagination_page_size: int = 5,
+               tree_table: bool = False,
+               floating_filter: bool = False,
+               wrap_text: bool = False,
+               theme: str = 'ag-theme-quartz',
+               side_filter: bool = False,
+               autogenerate_column_defs: bool = True,
+               selected_rows: List[Dict] | Any = None,
+               auto_height: bool = False,
+               hide_empty_cols: bool = False,
+               empty_cols_to_show: list[str] = [],
+               get_row_id: Optional[str] = None,
+               draggable: bool = False,
+               **kwargs,
+               ) -> dag.AgGrid:
+    """
+    Generic Dash AG Grid table
+
+    Args:
+        side_filter (bool): if True, adds a side bar with filtering options. Filters \
+            will be generated for columns according to the config in column_defs. Overwrites
+            the sidebar key in dash_grid_options
+        autogenerate_column_defs (bool): if True, creates column_defs from row_data if \
+            column_defs is not provided. Defaults to True.
+        selected_rows (list): list of rows to select. Applies only to table with selectable \
+            columns
+        auto_height (list): if True, the grid to auto-sizes its height to the number of rows \
+            displayed inside the grid. Overwrites the domLayout key in dash_grid_options and
+            height in style
+        get_row_id (Optional[str]): js function to assign the `RowID` of each row in row_data \
+            from the table's data. Ex : "params.data.id" to assign it to "id" field.
+            See https://dash.plotly.com/dash-ag-grid/row-ids for more information.
+    """
+
+    column_defs = column_defs or _create_default_column_definitions(
+        row_data) if autogenerate_column_defs else []
+    style = style
+    default_col_def = default_col_def or {
+        # enable floating filters by default
+        'floatingFilter': floating_filter,
+        'filter': 'agTextColumnFilter',
+        # 'wrapHeaderText': True,
+        # make row overflow
+        'wrapText': wrap_text,
+    }
+    row_style = row_style
+
+    dash_grid_options = dash_grid_options or {
+        'colResizeDefault': 'shift',
+        'rowSelection': 'single',
+        'headerHeight': 50,
+        'groupHeaderHeight': 30,
+        # Enables pagination
+        'pagination': pagination,
+        'paginationPageSize': pagination_page_size,
+    }
+
+    if side_filter:
+        dash_grid_options['sideBar'] = 'filters'
+
+    if tree_table:
+        dash_grid_options |= {
+            'autoGroupColumnDef': {
+                'cellRendererParams': {
+                    'suppressCount': True,
+                },
+            },
+            'getDataPath': {'function': 'getDataPath(params)'},
+            'treeData': True,
+            'rowSelection': 'single',
+        }
+
+    if auto_height:
+        dash_grid_options['domLayout'] = 'autoHeight'
+        if isinstance(style, dict):
+            style['height'] = None
+        else:
+            style = {'height': None}
+
+    row_data = row_data if isinstance(row_data, list) else row_data.to_dict('records')
+
+    if hide_empty_cols:
+        empty_cols_idx = []
+        for idx, col_def in enumerate(column_defs):
+            if (col := col_def['field']) not in empty_cols_to_show:
+                for row in row_data:
+                    if value := row.get(col, None):
+                        break
+                if value is None:
+                    empty_cols_idx.append(idx)
+
+        column_defs = [col_def for idx, col_def in enumerate(column_defs)
+                       if idx not in empty_cols_idx]
+
+    if draggable:
+        dash_grid_options |= {
+            'rowDragManaged': True,
+            'animateRows': True,
+            'domLayout': 'autoHeight',
+            'autoHeight': True,
+            'rowDragEntireRow': True,
+            'rowDragMultiRow': 'single',
+            'suppressMaintainUnsortedOrder': True,
+            'rowSelection': 'single',
+        }
+
+    return dag.AgGrid(
+        id=id,
+        enableEnterpriseModules=True,
+        licenseKey='',
+        columnDefs=column_defs,
+        rowData=row_data,
+        selectedRows=selected_rows,
+        defaultColDef=default_col_def,
+        style=style,
+        getRowStyle=row_style,
+        columnSize='responsiveSizeToFit',
+        dashGridOptions=dash_grid_options,
+        className=theme,
+        getRowId=get_row_id,
+        **kwargs,
+    )
+
+
+def _create_default_column_definitions(data: Union[List[Dict], Any]) -> List[Dict[str, str]]:
+    """
+    Iterates over the list of column definitions and creates default definitions
+    """
+    if isinstance(data, list):
+        return [{'field': col, 'headerName': col.replace('_', ' ').title()}
+                for col in data[0].keys()]
+
+    return [{'field': col, 'headerName': col.replace('_', ' ').title()} for col in data.columns]
