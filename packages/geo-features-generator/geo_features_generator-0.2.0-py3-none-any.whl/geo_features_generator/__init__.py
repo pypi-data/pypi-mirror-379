d0e76a63e8478cfcd4f5fc3d5ff5e39dd7f7c6bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 04:06:52 +0000

    Add package geo-features-generator/0.2.0/geo_features_generator-0.2.0-py3-none-any.whl

---
+++ packages/geo-features-generator/geo_features_generator-0.2.0-py3-none-any.whl/geo_features_generator/__init__.py
@@ -0,0 +1,10 @@
+from .generator import GeoFeaturesGenerator as Generator
+from .generator import GeoFeaturesGenerator
+
+__version__ = "0.2.0"
+
+__all__ = [
+    "GeoFeaturesGenerator",
+    "Generator",
+    "__version__",
+]
