b1e96b0605d9925be3f798e73c49838b6e5cbb4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 06:06:52 +0000

    Add package DesmosKiller/9.0/desmoskiller-9.0.tar.gz

---
+++ packages/DesmosKiller/desmoskiller-9.0.tar.gz/desmoskiller-9.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="DesmosKiller",
+      version="9.0",
+      packages=["DesmosKiller"],
+      install_requires=["matplotlib"],
+      author="YusufA442",
+      author_email="yusuf365820@gmail.com",
+      license='Creative Commons Attribution-Noncommercial-Share Alike license',
+      long_description=open('readme.txt').read())
