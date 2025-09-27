e786aec01c11b05c66a5ceb92f0523a57b007065
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 03:13:50 +0000

    Add package uniovi-simur-sample/8.0.0/uniovi_simur_sample-8.0.0-py3-none-any.whl

---
+++ packages/uniovi-simur-sample/uniovi_simur_sample-8.0.0-py3-none-any.whl/simur_sample/core/_versions.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib as mp
+
+def numpy_version():
+    return np.__version__
+
+def pandas_version():
+    return pd.__version__
+
+def matplotlib_version():
+    return mp.__version__
