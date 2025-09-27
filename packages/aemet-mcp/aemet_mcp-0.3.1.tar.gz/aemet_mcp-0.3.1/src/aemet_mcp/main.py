64d945ea026c7647b0bac3791fb6133b8c2a82f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 07:05:34 +0000

    Add package aemet-mcp/0.3.1/aemet_mcp-0.3.1.tar.gz

---
+++ packages/aemet-mcp/aemet_mcp-0.3.1.tar.gz/aemet_mcp-0.3.1/src/aemet_mcp/main.py
@@ -0,0 +1,9 @@
+from server import main as server_main
+
+def main():
+    """Ejecutar la función server main."""
+    server_main()
+    print("Hola desde AEMET-mcp!")
+
+if __name__ == "__main__":
+    main()
