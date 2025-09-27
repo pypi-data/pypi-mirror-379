bebccdaff7348a354441bce2310f26f7991a203f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-27 04:06:05 +0000

    Add package hello-library/0.0.0/hello_library-0.0.0.tar.gz

---
+++ packages/hello-library/hello_library-0.0.0.tar.gz/hello_library-0.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='hello_library',
+    packages=['hello_library'],
+    author='Sechaba Maqai',
+    author_email='sechaba.maqai@gmail.com',
+    description='A simple library that prints Lumelang ke Maqai!',
+    url='https://github.com/your_username/hello_library',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
